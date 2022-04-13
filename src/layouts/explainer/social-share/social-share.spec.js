ed5ca49a603cbfa8429920b19e0401fec5ddc5fd
Jordan Hertzler <jordan.hertzler@kc.frb.org>
2022-04-13 15:21:45 -0500

    FDG-4997 update test code

--- src/layouts/explainer/social-share/social-share.spec.js
+++ src/layouts/explainer/social-share/social-share.spec.js
@@ -8,24 +8,24 @@ const smallSampleCopy = `Lorem ipsum dolor sit amet, consectetur adipiscing elit
 const baseUrl = globalConstants.BASE_SITE_URL;
 
 describe('Social Share component', () => {
-  // it('returns social share sections ', () => {
-  //   const { findByTestId } = render(
-  //     <SocialShare quote={smallSampleCopy}
-  //                  title={"Sample Title"}
-  //                  summary={smallSampleCopy}
-  //                  url={baseUrl + "/national-debt/"}/>
-  //   );
-  //
-  //   const facebook = findByTestId('facebook');
-  //   const twitter = findByTestId('twitter');
-  //   const linkedIn = findByTestId('linkedIn');
-  //   const reddit = findByTestId('reddit');
-  //   const email = findByTestId('email');
-  //
-  //   expect(facebook).toBeInTheDocument();
-  //   expect(twitter).toBeInTheDocument();
-  //   expect(linkedIn).toBeInTheDocument();
-  //   expect(reddit).toBeInTheDocument();
-  //   expect(email).toBeInTheDocument();
-  // });
+  it('returns social share sections ', () => {
+    const { findByTestId } = render(
+      <SocialShare quote={smallSampleCopy}
+                   title={"Sample Title"}
+                   summary={smallSampleCopy}
+                   url={baseUrl + "/national-debt/"}/>
+    );
+
+    const facebook = findByTestId('facebook');
+    const twitter = findByTestId('twitter');
+    const linkedIn = findByTestId('linkedIn');
+    const reddit = findByTestId('reddit');
+    const email = findByTestId('email');
+
+    expect(facebook).toBeTruthy();
+    expect(twitter).toBeTruthy();
+    expect(linkedIn).toBeTruthy();
+    expect(reddit).toBeTruthy();
+    expect(email).toBeTruthy();
+  });
 });
