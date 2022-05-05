1f25a74ee21ba25f7502e500342c646ae804e9b4
Jordan Hertzler <jordan.hertzler@kc.frb.org>
2022-05-05 14:07:45 -0500

    FDG-5475 test social share in stg

--- src/layouts/explainer/explainer-helpers/explainer-helpers.js
+++ src/layouts/explainer/explainer-helpers/explainer-helpers.js
@@ -42,7 +42,7 @@ export const explainerSocialShareMap = {
     quote: 'Sample Quote',
     title: 'Sample Title',
     summary: sampleCopy,
-    url: baseUrl + "/national-debt/",
+    url: "stg.fiscaldata.treasury.gov/national-debt/",
     image: nationalDebtImage
   }
 }
