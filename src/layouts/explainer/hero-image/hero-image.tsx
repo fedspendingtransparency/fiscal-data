9957c01f642e485102bd32cc61620367d6fa63ef
Jordan Hertzler <jordan.hertzler@kc.frb.org>
2022-05-10 14:41:03 -0500

    FDG-5470 update svg

--- src/layouts/explainer/hero-image/hero-image.tsx
+++ src/layouts/explainer/hero-image/hero-image.tsx
@@ -7,7 +7,7 @@ import {
   heroImageSubHeading,
   bottomBorder
 } from './hero-image.module.scss';
-import {withWindowSize} from "react-fns";
+import { withWindowSize } from "react-fns";
 
 const HeroImage: FunctionComponent<IHeroImage> = ({
   heading,
@@ -17,23 +17,19 @@ const HeroImage: FunctionComponent<IHeroImage> = ({
   width,
   children
 }) => {
-  const spacing = 34;
-  const h = 8;
-  const pointHeight = 18;
+  const lineHeight = 8;
+  const chevronHeight = 18;
+  const chevronSpacing = 34;
+  const p1 = `L${width/2 - chevronSpacing} 0 `;
+  const p2 = `L${width/2} ${chevronHeight} `;
+  const p3 = `L${width/2 + chevronSpacing} 0 `;
+  const p4 = `L${width} 0 `;
+  const p5 = `L${width} ${lineHeight} `;
+  const p6 = `L${width/2 + chevronSpacing + 4} ${lineHeight} `;
+  const p7 = `L${width/2} ${chevronHeight + 9.24} `;
+  const p8 = `L${width/2 - chevronSpacing - 4} ${lineHeight} `;
+  const p9 = `L0 ${lineHeight}`;
 
-  const p0 = "M0 0"
-  const p1 = `L${width/2 - spacing} 0`;
-  const p2 = `L${width/2} ${pointHeight}`;
-  const p3 = `L${width/2 + spacing} 0`;
-  const p4 = `L${width} 0`;
-  const p5 = `L${width} ${h}`;
-  const p6 = `L${width/2 + spacing + 4} ${h}`;
-  const p7 = `L${width/2} ${pointHeight + 9.24}`;
-  const p8 = `L${width/2 - spacing - 4 } ${h}`;
-  const p9 = `L0 ${h}`;
-
-  const shape= p0 +" "+ p1 +" "+ p2 +" "+ p3 +" "+ p4 +" "+ p5 +" "+ p6 +" "+ p7 +" "+ p8 +" "+ p9 + " Z";
-  console.log(width);
   return (
     <>
       <div
@@ -49,8 +45,11 @@ const HeroImage: FunctionComponent<IHeroImage> = ({
         {children}
         <p className={heroImageSubHeading}>{subHeading}</p>
       </div>
-      <div className={bottomBorder}>
-        <svg height={28} width={width}>
+      <div className={bottomBorder} >
+        <svg height="28"
+             width="100%"
+             preserveAspectRatio="xMidYMid slice"
+             viewBox={`0 0 ${width} 28`}>
           <defs>
             <linearGradient id="Gradient">
               <stop offset="38%" stopColor={primaryColor}/>
@@ -58,7 +57,7 @@ const HeroImage: FunctionComponent<IHeroImage> = ({
               <stop offset="62%" stopColor={primaryColor}/>
             </linearGradient>
           </defs>
-          <path d={shape}
+          <path d={"M0 0 " + p1 +p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + " Z"}
                 fill="url(#Gradient)"
           />
         </svg>
