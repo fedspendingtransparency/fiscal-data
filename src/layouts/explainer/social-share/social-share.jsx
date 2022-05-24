776a9b42d45bb41028465a324abf2d213b5f292f
Jordan Hertzler <jordan.hertzler@kc.frb.org>
2022-05-24 08:49:13 -0500

    FDG-5475 update meta properties, remove height / width

--- src/layouts/explainer/social-share/social-share.jsx
+++ src/layouts/explainer/social-share/social-share.jsx
@@ -109,8 +109,6 @@ const SocialMetaData = ({ image, title, summary, url }) => {
         <meta property="og:image" content={ image } />
         <meta property="og:type" content="website" />
         <meta property="og:url" content={ url } />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={ title } />
         <meta name="twitter:description" content={ summary } />
