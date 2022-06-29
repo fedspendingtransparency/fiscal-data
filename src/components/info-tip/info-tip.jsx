9ad505025d7a05abe7aa5c9a2f99d5133530d481
Jordan Hertzler <jordan.hertzler@kc.frb.org>
2022-06-29 15:21:44 -0500

    FDG-4796 Update glossary.csv

--- src/components/info-tip/info-tip.jsx
+++ src/components/info-tip/info-tip.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Popover from '@material-ui/core/Popover';
 import Button from '@material-ui/core/Button';
