901e73d9961f00f752c8368b9ee8a7e308f31ed0
Jordan Hertzler <jordan.hertzler@kc.frb.org>
2022-06-30 15:29:44 -0500

    FDG-4796 Update unit tests

--- src/components/info-tip/info-tip.spec.js
+++ src/components/info-tip/info-tip.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, act } from '@testing-library/react';
+import  userEvent from '@testing-library/user-event'
 import InfoTip from "./info-tip";
 
 describe('InfoTip', () => {
@@ -8,7 +9,6 @@ describe('InfoTip', () => {
   const clickEvent = jest.fn();
 
   const InfoTipComponent = <InfoTip title={title} clickEvent={clickEvent}>{body}</InfoTip>
-      
 
   it('does not render the popup without user interaction with the infographic button', () => {
     const { queryByTestId } = render(InfoTipComponent);
@@ -16,13 +16,13 @@ describe('InfoTip', () => {
   });
 
   it('provides an infographic button that has 508 accessibility features', () => {
-    const { getByTestId } = render(InfoTipComponent); 
+    const { getByTestId } = render(InfoTipComponent);
     const button = getByTestId('infoTipButton');
     expect(button).toBeInTheDocument();
   });
 
   it('shows the popup and calls the click event passed to it when clicked on', () => {
-    const { getByTestId } = render(InfoTipComponent); 
+    const { getByTestId } = render(InfoTipComponent);
     const button = getByTestId('infoTipButton');
 
     act(() => {
@@ -32,4 +32,57 @@ describe('InfoTip', () => {
     expect(getByTestId('popupContainer')).toBeInTheDocument();
     expect(clickEvent).toHaveBeenCalled();
   })
+});
+
+describe('InfoTip for glossary terms',  () => {
+  const glossaryTitle = 'Term';
+  const glossaryBody = 'Definition';
+  const GlossaryInfoTipComponent = <InfoTip title={glossaryTitle} glossaryText={glossaryTitle}>{glossaryBody}</InfoTip>;
+
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  it('shows the popup after hovering for .5 seconds', () => {
+    jest.spyOn(global,'setTimeout');
+
+    const { getByRole, getByTestId } = render(GlossaryInfoTipComponent);
+    const button = getByRole('button', {name: glossaryTitle});
+    act(() => {
+      userEvent.hover(button);
+    });
+    jest.runOnlyPendingTimers();
+    expect(setTimeout).toHaveBeenCalled();
+    expect(getByTestId('popupContainer')).toBeInTheDocument();
+  });
+
+  it('will not render popup if mouse moves away before .5 seconds', () => {
+    jest.spyOn(global,'setTimeout');
+    jest.spyOn(global,'clearTimeout');
+    const { getByRole, queryByTestId } = render(GlossaryInfoTipComponent);
+    const button = getByRole('button', {name: glossaryTitle});
+    act(() => {
+      userEvent.hover(button);
+      userEvent.unhover(button);
+    });
+    expect(setTimeout).toHaveBeenCalled();
+    expect(clearTimeout).toHaveBeenCalled();
+    expect(queryByTestId('popupContainer')).toBeNull();
+  });
+
+  it('opens the popup when the user tabs into the button and hits enter', () => {
+    const { getByRole, queryByTestId, getByTestId } = render(GlossaryInfoTipComponent);
+    const button = getByRole('button', {name: glossaryTitle});
+
+    userEvent.tab();
+    expect(button).toHaveFocus();
+    act(() => {
+      userEvent.keyboard('[enter]');
+    });
+
+    expect(getByTestId('popupContainer')).toBeInTheDocument();
+  });
 });
