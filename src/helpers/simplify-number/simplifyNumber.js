a56e0da68552d4cdd910f9b2348b7fe27e5713ab
Jordan Hertzler <jordan.hertzler@kc.frb.org>
2022-07-21 15:03:51 -0500

    update regex

--- src/helpers/simplify-number/simplifyNumber.js
+++ src/helpers/simplify-number/simplifyNumber.js
@@ -19,5 +19,5 @@ export default function simplifyNumber(n, currency) {
 }
 
 export const numberWithCommas = (x) => {
-  return x.toString().replace(/\B(?<!\.\d)(?=(\d{3})+(?!\d))/g, ",");
+  return x.toString();
 };
