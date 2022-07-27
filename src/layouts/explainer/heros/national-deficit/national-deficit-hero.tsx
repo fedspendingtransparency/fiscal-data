ccc0ce2f96fc161edc386405d44ec92f8fbe19c5
Wyatt Chandler <wyatt.chandler@kc.frb.org>
2022-07-27 15:05:30 -0500

    FDG-5333 Added in mobile view

--- src/layouts/explainer/heros/national-deficit/national-deficit-hero.tsx
+++ src/layouts/explainer/heros/national-deficit/national-deficit-hero.tsx
@@ -1,10 +1,12 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useLayoutEffect, useState} from "react";
 import CustomLink from "../../../../components/links/custom-link/custom-link";
 import {calloutContainer, counterSourceInfo, heroImageCallout, icon, footNotes, deficitBox, deficitBoxPercent, deficitBoxContainer, deficitArrow} from "../../hero-image/hero-image.module.scss";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faFlagUsa, faDownLong, faUpLong} from "@fortawesome/free-solid-svg-icons";
 import {apiPrefix, basicFetch} from "../../../../utils/api-utils";
 import {numberWithCommas} from "../../../../helpers/simplify-number/simplifyNumber";
+import {pxToNumber} from "../../../../helpers/styles-helper/styles-helper";
+import {breakpointLg} from "../../../../variables.module.scss";
 
 const NationalDeficitHero = (): JSX.Element => {
   const fields: string = 'fields=current_fytd_net_outly_amt,prior_fytd_net_outly_amt,record_date,record_calendar_month,record_calendar_year,record_fiscal_year';
@@ -15,39 +17,46 @@ const NationalDeficitHero = (): JSX.Element => {
     = `v1/accounting/mts/mts_table_5?${fields}&${filter}&${sort}&${pagination}`;
   const deficitUrl: string = `${apiPrefix}${endpointUrl}`;
 
-  const [nationalDeficitValue, setNationalDeficitValue]
-    = useState<string | null>(null);
+  const [displayedDeficitValue, setDisplayedDeficitValue] =
+    useState<string | null>(null);
 
-  const [nationalDeficitValueNoDecimal, setNationalDeficitValueNoDecimal]
-    = useState<string | null>(null);
-
-  const [currentRecordMonth, setCurrentRecordMonth] = useState<string | string>('');
-  const [currentFiscalYear, setCurrentFiscalYear] = useState<string | string>('');
-  const [previousFiscalYear, setPreviousFiscalYear] = useState<string | string>('');
-  const [deficitStatus, setDeficitStatus] = useState<string | string>('');
-  const [deficitDifPercent, setDeficitDifPercent] = useState<string | string>('');
+  const [currentRecordMonth, setCurrentRecordMonth] = useState<string>('');
+  const [previousCalendarYear, setPreviousCalendarYear] = useState<string>('');
+  const [currentFiscalYear, setCurrentFiscalYear] = useState<string>('');
+  const [previousFiscalYear, setPreviousFiscalYear] = useState<string>('');
+  const [deficitStatus, setDeficitStatus] = useState<string>('');
+  const [deficitDifPercent, setDeficitDifPercent] = useState<string>('');
 
   const getCurrentNationalDeficit = (url) => {
     basicFetch(`${url}`)
     .then((res) => {
       if (res.data) {
-        console.log(res.data);
-        setNationalDeficitValue(res.data[0].current_fytd_net_outly_amt);
-        setNationalDeficitValueNoDecimal(numberWithCommas(parseFloat(Math.abs(parseFloat(res.data[0].current_fytd_net_outly_amt)).toFixed())));
+        if(typeof(window) !== 'undefined') {
+          if (window.innerWidth < pxToNumber(breakpointLg)) {
+            setDisplayedDeficitValue(Math.abs(
+              (parseFloat(res.data[0].current_fytd_net_outly_amt) / 1000000000)).toFixed() + 'B');
+          }
+          else {
+            setDisplayedDeficitValue(numberWithCommas(
+              parseFloat(Math.abs(parseFloat(res.data[0].current_fytd_net_outly_amt)).toFixed())));
+          }
+        }
         const date = new Date();
         date.setMonth(parseInt(res.data[0].record_calendar_month) - 1);
         setCurrentRecordMonth(date.toLocaleString('en-US', {
           month: 'short',
         }));
-        setPreviousFiscalYear((parseInt(res.data[0].record_fiscal_year) - 1).toString());
-        setCurrentFiscalYear(res.data[0].record_fiscal_year);
+        setCurrentFiscalYear((parseInt(res.data[0].record_fiscal_year) - 1).toString());
+        setPreviousFiscalYear((parseInt(res.data[0].record_fiscal_year) - 2).toString());
+        setPreviousCalendarYear((parseInt(res.data[0].record_calendar_year) - 1).toString());
         const currentDeficit = Math.abs(parseFloat(res.data[0].current_fytd_net_outly_amt));
         const priorYearDeficit = Math.abs(parseFloat(res.data[0].prior_fytd_net_outly_amt));
-        setDeficitDifPercent((((currentDeficit - priorYearDeficit) / priorYearDeficit)*100).toFixed())
-        if(Math.abs(currentDeficit) > Math.abs(priorYearDeficit)) {
+        setDeficitDifPercent((
+          ((currentDeficit - priorYearDeficit) / priorYearDeficit)*100).toFixed())
+        if(currentDeficit > priorYearDeficit) {
           setDeficitStatus('increased');
         }
-        else if (Math.abs(currentDeficit) < Math.abs(priorYearDeficit)) {
+        else if (currentDeficit < priorYearDeficit) {
           setDeficitStatus('decreased');
         }
         else {
@@ -57,42 +66,56 @@ const NationalDeficitHero = (): JSX.Element => {
     });
   };
 
+  const useWindowSize = () => {
+    const [size, setSize] = useState([0, 0]);
+    useLayoutEffect(() => {
+      const updateSize = () => {
+        setSize([window.innerWidth, window.innerHeight]);
+      }
+      window.addEventListener('resize', updateSize);
+      updateSize();
+      return () => window.removeEventListener('resize', updateSize);
+    }, []);
+    return size;
+  }
+
   useEffect(() => {
     getCurrentNationalDeficit(deficitUrl);
-  }, []);
+  }, [useWindowSize()]);
 
   const mtsLink =
-    <CustomLink url={'https://fiscaldata.treasury.gov/datasets/monthly-treasury-statement/' +
-       'summary-of-receipts-and-outlays-of-the-u-s-government'}>Monthly Treasury Statement (MTS)
+    <CustomLink url={'https://fiscaldata.treasury.gov/datasets/monthly-treasury-statement/summary-of-receipts-and-outlays-of-the-u-s-government'}>
+      Monthly Treasury Statement (MTS)
     </CustomLink>;
   const debt = <CustomLink url={'/national-debt/'}>Debt</CustomLink>;
   return (
     <>
       <div className={counterSourceInfo}>
         <p>
-          Fiscal year-to-date (since October {previousFiscalYear}) total updated monthly using
+          Fiscal year-to-date (since October {currentFiscalYear}) total updated monthly using
           the {mtsLink} dataset.
         </p>
       </div>
       <div className={footNotes}>
         <p>
-          Compared to the same period last year (Oct {previousFiscalYear} - {currentRecordMonth} {previousFiscalYear}),
+          Compared to the same period last year
+          (Oct {previousFiscalYear} - {currentRecordMonth} {previousCalendarYear}),
           our national deficit has {deficitStatus}.
         </p>
       </div>
       <div className={deficitBoxContainer}>
         <div className={deficitBox}>
-          ${nationalDeficitValueNoDecimal}
+          ${displayedDeficitValue}
         </div>
           {
             deficitStatus === 'increased' ? (
               <div className={deficitArrow}>
-                <FontAwesomeIcon icon={faUpLong} title={"up arrow"}/>
+                <FontAwesomeIcon icon={faUpLong} title={"up arrow"} />
               </div>
             )
             : (
               <div className={deficitArrow}>
-                <FontAwesomeIcon icon={faDownLong} title={"down arrow"}/>
+                <FontAwesomeIcon icon={faDownLong} title={"down arrow"} />
               </div>
             )
           }
@@ -102,7 +125,7 @@ const NationalDeficitHero = (): JSX.Element => {
       </div>
       <div className={calloutContainer}>
         <div className={heroImageCallout} data-testid={"nationalDebtCallout"}>
-          <FontAwesomeIcon icon={faFlagUsa} className={icon}/>
+          <FontAwesomeIcon icon={faFlagUsa} className={icon} />
           <p>
             This topic is the second of four U.S. government financial concepts from Your Guide to
             America’s Finances with more being added in the coming months. We’ll help you learn more
