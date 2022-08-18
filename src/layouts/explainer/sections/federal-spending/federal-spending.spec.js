1d5b2bd7489fd36b410704470314d38c1a924726
Jordan Hertzler <jordan.hertzler@kc.frb.org>
2022-08-18 16:00:50 -0500

    FDG-5670 Fix typo

--- src/layouts/explainer/sections/federal-spending/federal-spending.spec.js
+++ src/layouts/explainer/sections/federal-spending/federal-spending.spec.js
@@ -1,6 +1,6 @@
 import federalSpendingSections from "./federal-spending";
 
-describe('Federal spendinf explainer page sections', () => {
+describe('Federal spending explainer page sections', () => {
   it('returns 6 sections with headings and body components', () => {
     expect(federalSpendingSections.length).toBe(5);
     expect(federalSpendingSections[0].title).toBeDefined();
