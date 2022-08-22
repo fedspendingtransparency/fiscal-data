f4f88adfd1ff1d622e86f6fd9163d7e984307dc3
chasls2 <110061741+chasls2@users.noreply.github.com>
2022-08-22 11:32:43 -0500

    FDG-4652 Added Test
    FDG 4652 Added a unit test for the flip card display footer.
    I added it to the previous test  with a getByText 'period last year (Oct 2021

--- src/layouts/explainer/heros/national-deficit/national-deficit-hero.spec.js
+++ src/layouts/explainer/heros/national-deficit/national-deficit-hero.spec.js
@@ -30,7 +30,9 @@ describe('National Deficit Hero', () => {
     expect(await getByText("down arrow", {exact: false})).toBeInTheDocument();
     expect(await getByText("2021, the federal government", {exact: false})).toBeInTheDocument();
     expect(await getByText("government spent $2.77 trillion", {exact: false})).toBeInTheDocument();
+    expect(await getByText('period last year (Oct 2021', {exact: false})).toBeInTheDocument();
     global.fetch.mockRestore();
   });
+
 
 });
