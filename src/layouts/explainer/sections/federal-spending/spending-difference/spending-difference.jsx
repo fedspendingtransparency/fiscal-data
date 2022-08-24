9f76b8191235bb7165b06b391645f8f6c44f8b7b
chasls2 <110061741+chasls2@users.noreply.github.com>
2022-08-24 16:18:29 -0500

    Updated Img to show PNG. PNG are not responsive though.

--- src/layouts/explainer/sections/federal-spending/spending-difference/spending-difference.jsx
+++ src/layouts/explainer/sections/federal-spending/spending-difference/spending-difference.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import {ChartPlaceholder} from
     "../../../explainer-helpers/federal-spending/federal-spending-helper";
 import {
-  spendingDifferenceContent
+  spendingDifferenceContent,
+  mandatorySpendingImgStyle,
+  mandatorySpendingContainer,
 } from "./spending-difference.module.scss";
 import {spendingAccordion} from "../federal-spending.module.scss";
 import Accordion from "../../../../../components/accordion/accordion";
 import {withWindowSize} from "react-fns";
 import {pxToNumber} from "../../../../../helpers/styles-helper/styles-helper";
 import {breakpointLg} from "../../../../../variables.module.scss";
-import {MandatorySpendingImgDesktop} from "../../../../../../static/images/mandatory-spending_desktop.svg";
-import {MandatorySpendingImgMobile} from "../../../../../../static/images/mandatory-spending_desktop.svg";
+import MandatorySpendingImgDesktop from "../../../../../../static/images/mandatory-spending_desktop.png";
+import MandatorySpendingImgMobile from "../../../../../../static/images/mandatory-spending_mobile.png";
+import DeficitDesk from "../../../../../../static/images/deficit-vs-debt_desktop.svg"
+import DeficitMobile from "../../../../../../static/images/deficit-vs-debt_mobile.svg"
 
 export const SpendingDifference = ({width}) => {
   return (
@@ -37,13 +41,15 @@ export const SpendingDifference = ({width}) => {
         amended again. Due to authorization laws, the funding for these programs must be allocated
         for spending each year, hence the term mandatory.
       </p>
-      <img src={width < pxToNumber(breakpointLg) ? MandatorySpendingImgMobile : MandatorySpendingImgDesktop}
-           alt={"Step 1: Existing laws require (mandatory) money for spending each year " +
-           "Step 2: The Treasury issues funds to specific agency spending accounts towards contracts, " +
-           "loans, grants, direct payments, and other financial assistance" +
-           "Step 3: Entitlement program benefits are paid out from these accounts to support " +
-           "people, businesses, and state and local governments "}
-           data-testid={'mandatorySpendingImg'}/>
+      <div className={mandatorySpendingContainer}>
+        <img src={width < pxToNumber(breakpointLg) ? DeficitMobile : DeficitDesk}
+             alt={"Step 1: Existing laws require (mandatory) money for spending each year " +
+             "Step 2: The Treasury issues funds to specific agency spending accounts towards contracts, " +
+             "loans, grants, direct payments, and other financial assistance " +
+             "Step 3: Entitlement program benefits are paid out from these accounts to support " +
+             "people, businesses, and state and local governments "}
+             data-testid={'mandatorySpendingImg'} className={mandatorySpendingImgStyle}/>
+      </div>
       <h6>Discretionary Spending</h6>
       <p>
         Discretionary spending is money formally approved by Congress and the President during the
