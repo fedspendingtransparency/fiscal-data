6e185af38415c5adf15bdb9f02192f7107b354c1
Fareez Ahmed <fareez.ahmed@kc.frb.org>
2022-08-29 11:31:04 -0700

    fix test runner and add tests

---
+++ src/layouts/explainer/sections/federal-spending/how-much-does-the-govt-spend/how-much-does-the-govt-spend.spec.js
@@ -0,0 +1,15 @@
+import HowMuchDoesTheGovtSpend from "./how-much-does-the-govt-spend"
+import { render } from "@testing-library/react"
+import React from "react"
+
+describe("Federal spending explainer page sections", () => {
+  it("renders", () => {
+    const instance = render(<HowMuchDoesTheGovtSpend />)
+    expect(instance).toBeTruthy()
+  })
+  it("has the right sections", () => {
+    const { getByText } = render(<HowMuchDoesTheGovtSpend />)
+    expect(getByText("U.S. Government Spending, FY 2021")).toBeDefined()
+    expect(getByText("Top 10 Spending by Category and Agency")).toBeDefined()
+  })
+})
