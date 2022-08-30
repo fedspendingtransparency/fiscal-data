42be9e56074053468054fffbde2e1e302a185e04
Fareez Ahmed <fareez.ahmed@kc.frb.org>
2022-08-30 09:23:09 -0700

    add back missing file from merge

--- src/layouts/explainer/explainer-components/key-takeaways/key-takeaways-section.jsx
+++ src/layouts/explainer/explainer-components/key-takeaways/key-takeaways-section.jsx
@@ -1,27 +1,55 @@
-import React from "react";
+import React from "react"
 import {
   icon,
   iconBackground,
   keyTakeawaysContent,
-  offsetIcon
-} from "./key-takeaways-section.module.scss";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+  offsetIcon,
+} from "./key-takeaways-section.module.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import GlossaryTerm from "../../../../components/glossary-term/glossary-term"
+import reactStringReplace from "react-string-replace"
 
-const KeyTakeawaysSection = ({takeaways, primaryColor, secondaryColor}) => (
+const getText = (takeaway, glossary) => {
+  const { hasGlossaryTerm, glossaryRegex, text } = takeaway
+  if (hasGlossaryTerm) {
+    return reactStringReplace(text, glossaryRegex, (match, i) => (
+      <GlossaryTerm
+        term={match[0].toUpperCase() + match.substring(1)}
+        page={"Spending Explainer"}
+        glossary={glossary}
+        key={i}
+      >
+        {match}
+      </GlossaryTerm>
+    ))
+  }
+  return text
+}
+
+const KeyTakeawaysSection = ({
+  takeaways,
+  primaryColor,
+  secondaryColor,
+  glossary,
+}) => (
   <>
-    {takeaways.map((takeaway) =>
+    {takeaways.map(takeaway => (
       <div className={keyTakeawaysContent} key={takeaway.text}>
-        <div className={iconBackground} style={{backgroundColor:secondaryColor}}>
+        <div
+          className={iconBackground}
+          style={{ backgroundColor: secondaryColor }}
+        >
           <FontAwesomeIcon icon={takeaway.icon} className={icon} />
-          <FontAwesomeIcon icon={takeaway.icon}
-                           className={offsetIcon}
-                           style={{color:primaryColor}}
+          <FontAwesomeIcon
+            icon={takeaway.icon}
+            className={offsetIcon}
+            style={{ color: primaryColor }}
           />
         </div>
-        <p>{takeaway.text}</p>
+        <p>{getText(takeaway, glossary)}</p>
       </div>
-    )}
+    ))}
   </>
-);
+)
 
-export default KeyTakeawaysSection;
+export default KeyTakeawaysSection
