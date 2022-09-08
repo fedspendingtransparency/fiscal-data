dbc45da207500830be2f87b3d005473965156e0b
Fareez Ahmed <fareez.ahmed@kc.frb.org>
2022-09-08 11:16:55 -0700

    fix: fix

--- src/layouts/explainer/sections/federal-spending/spending-categories/spending-categories.jsx
+++ src/layouts/explainer/sections/federal-spending/spending-categories/spending-categories.jsx
@@ -11,6 +11,12 @@ import {
 import HowMuchDoesTheGovtSpend from "../how-much-does-the-govt-spend/how-much-does-the-govt-spend"
 import CustomLink from "../../../../../components/links/custom-link/custom-link"
 export const SpendingCategories = () => {
+  const usaSpending = (
+    <CustomLink url={"https://www.usaspending.gov/explorer"}>
+      {" "}
+      USAspending.gov.
+    </CustomLink>
+  )
   return (
     <div className={spendingCategoriesContent}>
       <p>
@@ -32,7 +38,7 @@ export const SpendingCategories = () => {
         <VisualizationCallout color={spendingExplainerPrimary}>
           <p>
             For more details on U.S. government spending by category and agency,
-            visit USAspending.gov’s Spending Explorer and Agency Profile pages.
+            visit {usaSpending}
           </p>
         </VisualizationCallout>
       </div>
