d1133df3213766f210548910538529d6d7eea1db
Fortune Vieyra <fortune.vieyra@kc.frb.org>
2022-09-08 09:15:47 -0700

    FDG-5937 updated test cases

--- src/layouts/explainer/sections/government-revenue/sources-of-federal-revenue/sources-of-federal-revenue.spec.js
+++ src/layouts/explainer/sections/government-revenue/sources-of-federal-revenue/sources-of-federal-revenue.spec.js
@@ -1,23 +1,18 @@
-import {SourcesOfFederalRevenue} from "./sources-of-federal-revenue";
+import SourcesOfFederalRevenue from "./sources-of-federal-revenue";
 import {render} from '@testing-library/react';
 import React from "react";
 
 
 describe('Sources of Federal Revenue', () => {
 
-  it('renders the category header', () => {
-    const {getByRole} = render(<SourcesOfFederalRevenue />);
-    expect(getByRole('heading', {name: 'Sources of Federal Revenue'})).toBeInTheDocument();
-  });
-
   it('renders the category sub header', () => {
-    const {getByText} = render(<SourcesOfFederalRevenue />);
-    const subHeader = getByText('Social Security and Medicare Taxes');
-    expect(subHeader).toBeInTheDocument();
+    const {getByRole} = render(<SourcesOfFederalRevenue />);
+    expect(getByRole('heading', {name: 'Social Security and Medicare Taxes'})).toBeInTheDocument();
   });
 
   it('render the quote box', () => {
-    const {getByTestId} = render(<SourcesOfFederalRevenue />);
+    const {getByTestId,getByRole} = render(<SourcesOfFederalRevenue />);
+    expect(getByRole('link', {name: 'irs.gov'})).toBeInTheDocument();
     expect(getByTestId('quote-box')).toBeInTheDocument();
   });
 
