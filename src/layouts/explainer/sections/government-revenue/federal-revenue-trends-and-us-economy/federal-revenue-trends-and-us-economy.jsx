c23bb7c175e12e9bca82b836dbd624e27a78455b
chasls2 <110061741+chasls2@users.noreply.github.com>
2022-09-09 12:27:46 -0500

    FDG-5940
    Adding quote box

--- src/layouts/explainer/sections/government-revenue/federal-revenue-trends-and-us-economy/federal-revenue-trends-and-us-economy.jsx
+++ src/layouts/explainer/sections/government-revenue/federal-revenue-trends-and-us-economy/federal-revenue-trends-and-us-economy.jsx
@@ -1,17 +1,42 @@
 import React from "react";
-
+import {visWithCallout, quoteBoxContent} from "../../../explainer.module.scss";
+import VisualizationCallout
+  from "../../../../../components/visualization-callout/visualization-callout";
+import QuoteBox from "../../../quote-box/quote-box";
+import CustomLink from "../../../../../components/links/custom-link/custom-link";
+import {revenueExplainerPrimary, revenueExplainerLightSecondary} from "../revenue.module.scss";
+import { faMapLocationDot } from "@fortawesome/free-solid-svg-icons";
+import {
+  sourcesContent,
+  sourceURL
+} from "./federal-revenue-trends-and-us-economy.module.scss";
+import {ChartPlaceholder} from
+    "../../../explainer-helpers/federal-spending/federal-spending-helper";
 // This section will be filled out more post-mvp
 
 const FederalRevenueTrendsAndUSEconomy = () => {
 
+  const irsGov = <CustomLink url={'https://www.gps.gov/policy/funding/'}>GPS.gov</CustomLink>;
+
   return(
     <div>
       <div>
-        Coming Soon Flag Placeholder
+        Coming Soon
+        <ChartPlaceholder />
       </div>
-      <div>
-        Coming Soon: A section exploring how revenue trends relate to the U.S economy.
-      </div>
+      <QuoteBox
+        icon={faMapLocationDot}
+        primaryColor={revenueExplainerPrimary}
+        secondaryColor={revenueExplainerLightSecondary}
+      >
+        <p className={quoteBoxContent}>
+          Free GPS (Global Positioning System) service enjoyed throughout the world is funded by general U.S. tax revenues.
+          <br/>
+          <span className={sourceURL}>
+          Source: {irsGov}
+        </span>
+        </p>
+      </QuoteBox>
     </div>
   );
 }
