1bd5e9bdd405b76b5101a2ec2466944a4f7109de
Jordan Hertzler <jordan.hertzler@kc.frb.org>
2022-09-09 12:54:16 -0500

    FDG-5934 Add chart styling

--- src/layouts/explainer/sections/government-revenue/sources-of-federal-revenue/sources-of-revenue-circle-chart/sources-of-revenue-circle-chart.jsx
+++ src/layouts/explainer/sections/government-revenue/sources-of-federal-revenue/sources-of-revenue-circle-chart/sources-of-revenue-circle-chart.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import ChartContainer from "../../../../explainer-components/chart-container/chart-container";
 import { CirclePacking, ResponsiveCirclePacking } from '@nivo/circle-packing';
 import CustomLink from "../../../../../../components/links/custom-link/custom-link";
-import { dataHeaderContainer, header } from "./sources-of-revenue-circle-chart.module.scss";
+import {
+  dataHeaderContainer,
+  header,
+  dataLabels,
+  headerTitle,
+  subHeader,
+  category
+} from "./sources-of-revenue-circle-chart.module.scss";
 
 const SourcesOfRevenueCircleChart = () => {
+  const [categoryName, setCategoryName] = useState("Individual Income Taxes");
+  const [revenueAmount, setRevenueAmount] = useState(2);
+  const [totalRevenuePercent, setTotalRevenuePercent] = useState(51);
+
+
   const title = 'Sources of Revenue for the U.S. Federal Government';
   const subTitle = 'Revenue by Source Categories';
   const date = new Date();
@@ -26,54 +38,78 @@ const SourcesOfRevenueCircleChart = () => {
 
   const dataHeader = (
     <div className={dataHeaderContainer}>
-      <div className={header}>Social Security and Medicare Taxes</div>
-      <div>Category</div>
-
+      <div className={header}>{categoryName}</div>
+      <div className={category}>Category</div>
+      <div className={dataLabels}>
+        <div>
+          <div className={headerTitle}>${revenueAmount} T</div>
+          <span className={subHeader}>Revenue Amount</span>
+        </div>
+        <div>
+          <div className={headerTitle}>{totalRevenuePercent}%</div>
+          <span className={subHeader}>% of Total Revenue</span>
+        </div>
+      </div>
     </div>
   )
+  const individualIncomeTaxes = "#0A2F5A";
+  const SocialSecurityAndMedicareTaxes = "#EB5160";
+  const CorporateIncomeTaxes = "#C13F77";
+  const MiscellaneousIncome = "#FF773E";
+  const ExciseTaxes = "#883C7F";
+  const CustomsDuties = "#FFA600";
+  const EstateAndGiftTaxes = "#4B3974";
+
+  const colors = [
+    individualIncomeTaxes,
+    SocialSecurityAndMedicareTaxes,
+    CorporateIncomeTaxes,
+    MiscellaneousIncome,
+    ExciseTaxes,
+    CustomsDuties,
+    EstateAndGiftTaxes
+  ];
+
+  const theme = {
+    textColor: '#FFFFFF',
+    fontSize: '14px',
+  }
 
   const data = {
     id: "tester",
     children: [
       {
-        id: "IndividualIncomeTaxes",
-        name: "Individual Income Taxes",
+        id: "Individual Income Taxes",
         color: "#0A2F5A",
         value: 75,
       },
       {
-        id: "SocialSecurityAndMedicareTaxes",
-        name: "Social Security and Medicare Taxes",
+        id: "Social Security and Medicare Taxes",
         color: "#EB5160",
         value: 20,
       },
       {
-        id: "CorporateIncomeTaxes",
-        name: "Corporate Income Taxes",
+        id: "Corporate Income Taxes",
         color: "#C13F77",
         value: 15,
       },
       {
-        id: "MiscellaneousIncome",
-        name: "Miscellaneous Income",
+        id: "Miscellaneous Income",
         color: "#FF773E",
         value: 5,
       },
       {
-        id: "ExciseTaxes",
-        name: "Excise Taxes",
+        id: "Excise Taxes",
         color: "#883C7F",
         value: 4,
       },
       {
-        id: "CustomsDuties",
-        name: "Customs Duties",
+        id: "Customs Duties",
         color: "#FFA600",
         value: 3,
       },
       {
-        id: "Estate&GiftTaxes",
-        name: "Estate & Gift Taxes",
+        id: "Estate & Gift Taxes",
         color: "#4B3974",
         value: 2,
       }
@@ -94,14 +130,13 @@ const SourcesOfRevenueCircleChart = () => {
           <CirclePacking
           data={data}
           margin={{top: 20, right: 20, bottom:20, left:20}}
-          id={"name"}
-          childColor={{from: 'color'}}
-          inheritColorFromParent={true}
+          colors={colors}
+          colorBy={'id'}
           height={500}
           width={500}
           enableLabels={true}
           leavesOnly={true}
-          colorBy="id"
+          theme={theme}
           />
         </div>
       </ChartContainer>
