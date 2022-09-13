8c4792859b917af536a98bd58df04f4f520d8d65
Jordan Hertzler <jordan.hertzler@kc.frb.org>
2022-09-13 16:29:49 -0500

    FDG-5934 Add mobile chart labels

--- src/layouts/explainer/sections/government-revenue/sources-of-federal-revenue/sources-of-revenue-circle-chart/sources-of-revenue-circle-chart.jsx
+++ src/layouts/explainer/sections/government-revenue/sources-of-federal-revenue/sources-of-revenue-circle-chart/sources-of-revenue-circle-chart.jsx
@@ -9,11 +9,11 @@ import {
   headerTitle,
   subHeader,
   category,
-  label,
+  footer,
   totalRevenueDataPill
 } from "./sources-of-revenue-circle-chart.module.scss";
 import { withWindowSize } from "react-fns";
-import { breakpointLg } from "../../../../../../variables.module.scss";
+import {breakpointLg, semiBoldWeight} from "../../../../../../variables.module.scss";
 import {pxToNumber} from "../../../../../../helpers/styles-helper/styles-helper";
 
 const SourcesOfRevenueCircleChart = ({width}) => {
@@ -30,7 +30,7 @@ const SourcesOfRevenueCircleChart = ({width}) => {
   const slug = `https://fiscaldata.treasury.gov/datasets/monthly-treasury-statement/summary-of-
   receipts-and-outlays-of-the-u-s-government`;
   const footer =
-    <>
+    <div className={footer}>
       <p>
         <i>
           To explore this visual, hover or tap over any category bubble to discover its data.
@@ -40,7 +40,7 @@ const SourcesOfRevenueCircleChart = ({width}) => {
         Visit the <CustomLink url={slug}>{name}</CustomLink> dataset to explore and
         download this data.
       </p>
-    </>
+    </div>
 
   const dataHeader = (
     <div className={dataHeaderContainer}>
@@ -61,52 +61,89 @@ const SourcesOfRevenueCircleChart = ({width}) => {
 
   const labelFormatTable = {
     'Individual Income Taxes': {
-      desktop: ['Individual Income Taxes'],
-      mobile: ['Individual Income','Taxes']
+      desktop: {
+        lines: ['Individual Income Taxes'],
+      },
+      mobile: {
+        lines: ['Individual Income','Taxes'],
+      },
     },
     'Corporate Income Taxes': {
-      desktop: ['Corporate', 'Income Taxes'],
-      mobile: ['Corporate ','Income', 'Taxes']
+      desktop: {
+        lines: ['Corporate', 'Income Taxes'],
+      },
+      mobile: {
+        lines: ['Corporate ','Income', 'Taxes'],
+      },
     },
     'Social Security and Medicare Taxes': {
-      desktop: ['Social Security', 'and Medicare Taxes'],
-      mobile: ['Social Security', 'and', 'Medicare Taxes']
+      desktop: {
+        lines: ['Social Security', 'and Medicare Taxes'],
+      },
+      mobile: {
+        lines: ['Social Security', 'and', 'Medicare Taxes'],
+      },
     },
     'Miscellaneous Income': {
-      desktop: ['Miscellaneous', 'Income'],
-      mobile: ['Miscellaneous', 'Income'],
-      external: true,
-      verticalOffset: -18,
-      horizontalOffset: 6 + 43.75
+      desktop: {
+        lines: ['Miscellaneous', 'Income'],
+        verticalOffset: -18,
+        horizontalOffset: 6 + 43.75
+      },
+      mobile: {
+        lines: ['Miscellaneous', 'Income'],
+        verticalOffset: -15,
+        horizontalOffset: 30.5
+      },
+      external: true
     },
     'Customs Duties': {
-      desktop: ['Customs Duties'],
-      mobile: ['Customs Duties'],
-      external: true,
-      verticalOffset: -15,
-      horizontalOffset: 6
+      desktop: {
+        lines: ['Customs Duties'],
+        verticalOffset: -15,
+        horizontalOffset: 6
+      },
+      mobile: {
+        lines: ['Customs Duties'],
+        verticalOffset: -16,
+        horizontalOffset: -2
+      },
+      external: true
     },
     'Estate & Gift Taxes': {
-      desktop: ['Estate & Gift Taxes'],
-      mobile: ['Estate & Gift Taxes'],
-      external: true,
-      verticalOffset: -24,
-      horizontalOffset: -10
+      desktop: {
+        lines: ['Estate & Gift Taxes'],
+        verticalOffset: -24,
+        horizontalOffset: -10
+      },
+      mobile: {
+        lines: ['Estate & Gift Taxes'],
+        verticalOffset: -22,
+        horizontalOffset: -10
+      },
+      external: true
     },
-
     'Excise Taxes': {
-      desktop: ['Excise Taxes'],
-      mobile: ['Excise Taxes'],
+      desktop: {
+        lines: ['Excise Taxes'],
+        verticalOffset: 28,
+        horizontalOffset: -95
+      },
+      mobile: {
+        lines: ['Excise Taxes'],
+        verticalOffset: 15,
+        horizontalOffset: -72
+      },
       external: true,
-      verticalOffset: 28,
-      horizontalOffset: -80
     },
   }
 
   const LabelComponent = ({node, style, label}) => {
-    console.log(node)
-    const lines = labelFormatTable[label].desktop;
-    const yStartPoint = node.y - ((lines.length / 2) * 16.5) + 9;
+    const labelFormat = width < pxToNumber(breakpointLg) ?
+      labelFormatTable[label].mobile : labelFormatTable[label].desktop;
+    const lines = labelFormat.lines; //labelFormatTable[label].desktop;
+    const lineSpaceOffset = width < pxToNumber(breakpointLg) ? 12.5 : 16.5;
+    const yStartPoint = node.y - ((lines.length / 2) * lineSpaceOffset) + 9;
       return (
         <>
           {lines.map((line, index) => (
@@ -116,12 +153,12 @@ const SourcesOfRevenueCircleChart = ({width}) => {
                     <text
                       textAnchor={lines.length > 1 ? "middle" : "start"}
                       dominantBaseline="central"
-                      x={node.radius + node.x + 6 + labelFormatTable[label].horizontalOffset}
-                      y={yStartPoint + 16.5 * index + labelFormatTable[label].verticalOffset}
+                      x={node.radius + node.x + 6 + labelFormat.horizontalOffset}
+                      y={yStartPoint + lineSpaceOffset * index + labelFormat.verticalOffset}
                       fill={'#666666'}
                       style={{
-                        fontSize: 14,
-                        fontWeight: 600
+                        fontSize: width < pxToNumber(breakpointLg) ? 10 : 14,
+                        fontWeight: semiBoldWeight
                       }}
                     >
                       {line}
@@ -132,12 +169,12 @@ const SourcesOfRevenueCircleChart = ({width}) => {
                     textAnchor="middle"
                     dominantBaseline="central"
                     x={node.x}
-                    y={yStartPoint + 16.5 * index}
+                    y={yStartPoint + lineSpaceOffset * index}
                     fill={'#FFFFFF'}
                     style={{
-                      fontSize: 14,
-                      fontWeight: 600,
-                      maxWidth: 90
+                      fontSize: width < pxToNumber(breakpointLg) ? 10 : 14,
+                      fontWeight: semiBoldWeight,
+                      fontColor: '#FFFFFF'
                     }}
                   >
                     {line}
@@ -150,22 +187,22 @@ const SourcesOfRevenueCircleChart = ({width}) => {
     )
   }
 
-  const individualIncomeTaxes = "#0A2F5A";
-  const SocialSecurityAndMedicareTaxes = "#EB5160";
-  const CorporateIncomeTaxes = "#C13F77";
-  const MiscellaneousIncome = "#FF773E";
-  const ExciseTaxes = "#883C7F";
-  const CustomsDuties = "#FFA600";
-  const EstateAndGiftTaxes = "#4B3974";
+  const individualIncomeTaxes = "rgb(10,47,90, 0.8)" //"#0A2F5A";
+  const SocialSecurityAndMedicareTaxes = "rgb(235,81,96, 0.8)" //"#EB5160";
+  const CorporateIncomeTaxes = "rgb(193, 63, 119, 0.8)" //"#C13F77";
+  const MiscellaneousIncome = "rgb(255, 119, 62, 0.8)" //"#FF773E";
+  const ExciseTaxes = "rgb(136, 60, 127, 0.8)" //"#883C7F";
+  const CustomsDuties = "rgb(255, 166, 0, 0.8)" //"#FFA600";
+  const EstateAndGiftTaxes = "rgb(75, 57, 116, 0.8)" //"#4B3974";
 
   const colors = [
     individualIncomeTaxes,
     CorporateIncomeTaxes,
     SocialSecurityAndMedicareTaxes,
     MiscellaneousIncome,
-    ExciseTaxes,
     CustomsDuties,
     EstateAndGiftTaxes,
+    ExciseTaxes,
   ];
 
   const theme = {
@@ -179,57 +216,39 @@ const SourcesOfRevenueCircleChart = ({width}) => {
     },
   }
 
-
-
   const data = {
-    id: "parent",
-    // color: 'rgb(10,47,90)',
-
     children: [
       {
         id: "Individual Income Taxes",
         value: 85,
-        // color: 'rgb(10,47,90)'
       },
       {
         id: "Corporate Income Taxes",
         value: 15,
-        // color: 'rgb(10,47,90)'
       },
       {
         id: "Social Security and Medicare Taxes",
         value: 20,
-        // color: 'rgb(10,47,90)'
       },
       {
         id: "Miscellaneous Income",
         value: 1.75,
-        // color: 'rgb(10,47,90)'
       },
       {
         id: "Customs Duties",
         value: 1,
-        // color: 'rgb(10,47,90)'
       },
       {
         id: "Estate & Gift Taxes",
         value: .25,
-        // color: 'rgb(10,47,90)'
       },
       {
         id: "Excise Taxes",
         value: 1,
-        // color: 'rgb(10,47,90)'
       },
     ]
 };
 
-  const customStyle =
-  {
-    width: '10px',
-    height: '10px'
-  }
-  //overflow hidden
   return (
     <>
       <ChartContainer
@@ -241,33 +260,20 @@ const SourcesOfRevenueCircleChart = ({width}) => {
         date={date}
       >
         <div style={{display:'flex', justifyContent:'center', flexDirection: 'column'}} >
-          <div style={{height: '465px', overflow: 'hidden'}}>
+          <div style={{height: width < pxToNumber(breakpointLg) ? '325px' : '450px', overflow: 'hidden'}}>
             <CirclePacking
               data={data}
-              margin={{top: 10, right: 10, bottom:0, left:10}}
-              height={ width < pxToNumber(breakpointLg) ? 300 : 500 }
-              width={ width < pxToNumber(breakpointLg) ? 300 : 500 }
+              margin={{top: 0, right: 10, bottom:0, left:10}}
+              height={ width < pxToNumber(breakpointLg) ? 350 : 500 }
+              width={ width < pxToNumber(breakpointLg) ? 350 : 500 }
               colors={colors}
               colorBy={'id'}
-              // inheritColorFromParent={false}
-              theme={theme}
+              // theme={theme}
               leavesOnly
               enableLabels={true}
-              labelTextColor="#FFFFFF"
               labelsSkipRadius={0}
-              // labelsFilter={label => label.node.depth === 1}
-              labelComponent={({node, label, style}) =>
-                <LabelComponent node={node} style={style} label={label} />}
-              borderWidth={0}
-              borderColor={{
-                from: 'color',
-                modifiers: [
-                  [
-                    'darker',
-                    '0'
-                  ]
-                ]
-              }}
+              labelComponent={({node, label}) =>
+                <LabelComponent node={node}  label={label} />}
               animate={false}
             />
           </div>
