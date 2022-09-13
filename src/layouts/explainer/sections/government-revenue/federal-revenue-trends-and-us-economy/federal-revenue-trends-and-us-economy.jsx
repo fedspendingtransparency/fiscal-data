d30a9acb054ffd23e1fb15b5552b9f95caf8a541
chasls2 <110061741+chasls2@users.noreply.github.com>
2022-09-13 08:34:50 -0500

    FDG-5952
    Updated the text Coming Soon.

--- src/layouts/explainer/sections/government-revenue/federal-revenue-trends-and-us-economy/federal-revenue-trends-and-us-economy.jsx
+++ src/layouts/explainer/sections/government-revenue/federal-revenue-trends-and-us-economy/federal-revenue-trends-and-us-economy.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-
+import {comingSoon} from './federal-revenue-trends-and-us-economy.module.scss';
 // This section will be filled out more post-mvp
 
-const FederalRevenueTrendsAndUSEconomy = () => {
+export const FederalRevenueTrendsAndUSEconomy = () => {
 
-  return(
+  return (
     <div>
-      <div>
-        Coming Soon Flag Placeholder
-      </div>
-      <div>
-        Coming Soon: A section exploring how revenue trends relate to the U.S economy.
-      </div>
+      <p className={comingSoon}> Coming Soon: A section exploring
+       how revenue trends relate to the U.S. economy. </p>
     </div>
+
   );
 }
 
