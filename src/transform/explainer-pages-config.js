3a9878d3476b0029d79b3cdd69cf27e32287f221
Joel Mendoza <joel.mendoza@kc.frb.org>
2022-09-19 09:49:53 -0500

    FDG-5949 CC: 89.73 Make the Revenue Explainer page available in production

--- src/transform/explainer-pages-config.js
+++ src/transform/explainer-pages-config.js
@@ -67,7 +67,7 @@ const explainerPagesSource = {
       description: `Learn how much the U.S. government collects in revenue, and what types of
       revenue it brings in.`,
     },
-    prodReady: false,
+    prodReady: true,
     heroImage: {
       heading: "How much revenue has the U.S. government collected this year? ",
       subHeading: "",
