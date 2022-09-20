fa941008ab9b35185bf3bcca564eff7ef4ace738
Fareez Ahmed <fareez.ahmed@kc.frb.org>
2022-09-19 20:22:28 -0700

    initial changes

--- src/components/topics-section/explainer-tile/explainer-tile-helper.js
+++ src/components/topics-section/explainer-tile/explainer-tile-helper.js
@@ -1,70 +1,100 @@
-import React, {useEffect, useState} from "react";
-import {apiPrefix, basicFetch} from "../../../utils/api-utils";
-import {getShortForm} from "../../../layouts/explainer/heros/hero-helper";
+import React, { useEffect, useState } from "react"
+import { apiPrefix, basicFetch } from "../../../utils/api-utils"
+import { getShortForm } from "../../../layouts/explainer/heros/hero-helper"
 
 export const SpendingBodyGenerator = () => {
-  const fields = 'fields=current_fytd_net_outly_amt,record_fiscal_year,record_date';
-  const filter = 'filter=line_code_nbr:eq:5691';
-  const sort = 'sort=-record_date';
-  const pagination = 'page[size]=1';
-  const endpointUrl
-    = `v1/accounting/mts/mts_table_5?${fields}&${filter}&${sort}&${pagination}`;
-  const spendingUrl = `${apiPrefix}${endpointUrl}`;
-  const [amount, setAmount] = useState('0');
-  const [year, setYear] = useState('null');
+  const fields =
+    "fields=current_fytd_net_outly_amt,record_fiscal_year,record_date"
+  const filter = "filter=line_code_nbr:eq:5691"
+  const sort = "sort=-record_date"
+  const pagination = "page[size]=1"
+  const endpointUrl = `v1/accounting/mts/mts_table_5?${fields}&${filter}&${sort}&${pagination}`
+  const spendingUrl = `${apiPrefix}${endpointUrl}`
+  const [amount, setAmount] = useState("0")
+  const [year, setYear] = useState("null")
 
   useEffect(() => {
-    basicFetch(`${spendingUrl}`)
-      .then((res) => {
-        if (res.data) {
-          const data = res.data[0];
-          setAmount(data.current_fytd_net_outly_amt);
-          setYear(data.record_fiscal_year);
-        }
-      })
-  }, []);
+    basicFetch(`${spendingUrl}`).then(res => {
+      if (res.data) {
+        const data = res.data[0]
+        setAmount(data.current_fytd_net_outly_amt)
+        setYear(data.record_fiscal_year)
+      }
+    })
+  }, [])
 
-  return (<>The U.S. government has spent ${getShortForm(amount, 2, false)}{' '}
-    in fiscal year {year} to ensure the well-being of the people of the United States.
-    Learn more about spending categories, types of spending, and spending trends over time.</>);
-};
-
+  return (
+    <>
+      The U.S. government has spent ${getShortForm(amount, 2, false)} in fiscal
+      year {year} to ensure the well-being of the people of the United States.
+      Learn more about spending categories, types of spending, and spending
+      trends over time.
+    </>
+  )
+}
 
 export const pageTileMap = {
-
-  'debt': {
-    title: 'What is the national debt?',
-    body: 'The national debt enables the federal government to pay for important programs and ' +
-      'services for the American public. Explore debt concepts, the latest values, and trends ' +
-      'over time.',
-    altText: 'Hands raised in the air holding various objects, including a calculator, a pencil, ' +
-      'money, and magnifying glass',
-    desktopImage: 'homepage_debt_1200x630',
-    mobileImage: 'homepage_debt_square',
-    path: '/americas-finance-guide/national-debt/'
+  debt: {
+    title: "What is the national debt?",
+    body:
+      "The national debt enables the federal government to pay for important programs and " +
+      "services for the American public. Explore debt concepts, the latest values, and trends " +
+      "over time.",
+    altText:
+      "Hands raised in the air holding various objects, including a calculator, a pencil, " +
+      "money, and magnifying glass",
+    desktopImage: "homepage_debt_1200x630",
+    mobileImage: "homepage_debt_square",
+    path: "/americas-finance-guide/national-debt/",
   },
-  'spending': {
-    title: 'How much has the U.S. government spent this year?',
-    body: 'The U.S. government has spent {$XX.X trillion (total spending)} in fiscal ' +
-      'year {YYYY (current fiscal year)} to ensure the well-being of the people of the ' +
-      'United States. Learn more about spending categories, types of spending, and ' +
-      'spending trends over time.',
+  spending: {
+    title: "How much has the U.S. government spent this year?",
+    body:
+      "The U.S. government has spent {$XX.X trillion (total spending)} in fiscal " +
+      "year {YYYY (current fiscal year)} to ensure the well-being of the people of the " +
+      "United States. Learn more about spending categories, types of spending, and " +
+      "spending trends over time.",
     bodyGenerator: SpendingBodyGenerator,
-    altText: 'The US Treasury building is placed next to a row of homes. A pair of hands ' +
-      'exchange money in the foreground.',
-    desktopImage: 'homepage_spending_1200x630',
-    mobileImage: 'homepage_spending_square',
+    altText:
+      "The US Treasury building is placed next to a row of homes. A pair of hands " +
+      "exchange money in the foreground.",
+    desktopImage: "homepage_spending_1200x630",
+    mobileImage: "homepage_spending_square",
     mainFeature: true,
-    path: '/americas-finance-guide/federal-spending/'
+    path: "/americas-finance-guide/federal-spending/",
   },
-  'deficit': {
-    title: 'What is the national deficit?',
-    body: 'A national deficit occurs when the money going out exceeds the money coming in for a ' +
-      'given period of time. Learn more about the U.S. deficit and how it has changed over time.',
-    altText: 'A hand holding a gold coin beside a variety of symbols, including a pie chart, bar ' +
-      'graph, and lit lightbulb.',
-    desktopImage: 'homepage_deficit_1200x630',
-    mobileImage: 'homepage_deficit_square',
-    path: '/americas-finance-guide/national-deficit/'
-  }
-};
+  deficit: {
+    title: "What is the national deficit?",
+    body:
+      "A national deficit occurs when the money going out exceeds the money coming in for a " +
+      "given period of time. Learn more about the U.S. deficit and how it has changed over time.",
+    altText:
+      "A hand holding a gold coin beside a variety of symbols, including a pie chart, bar " +
+      "graph, and lit lightbulb.",
+    desktopImage: "homepage_deficit_1200x630",
+    mobileImage: "homepage_deficit_square",
+    path: "/americas-finance-guide/national-deficit/",
+  },
+  revenue: {
+    title: "How much revenue has the U.S. government collected this year? ",
+    body:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    altText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    desktopImage: "homepage_revenue_1200x630",
+    // TODO:
+    mobileImage: "homepage_revenue_square",
+    path: "americas-finance-guide/government-revenue",
+  },
+  "americas-finance-guide": {
+    title: "Your Guide to America’s Finances",
+    body:
+      "Here you’ll find information on spending, revenue, the deficit, and debt. The Guide represents a series of interactive visualizations exploring each category and how it has changed over time.",
+    altText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    desktopImage: "AFG-Overview_1200x630",
+    // TODO:
+    mobileImage: "homepage_revenue_square",
+    path: "americas-finance-guide",
+  },
+}
--- src/components/topics-section/topics-section.jsx
+++ src/components/topics-section/topics-section.jsx
@@ -1,78 +1,86 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBookOpen} from "@fortawesome/free-solid-svg-icons";
-import React from "react";
-import {createTheme, Grid, ThemeProvider} from "@material-ui/core"
-import ExplainerTile from "./explainer-tile/explainer-tile";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBookOpen } from "@fortawesome/free-solid-svg-icons"
+import React from "react"
+import { createTheme, Grid, ThemeProvider } from "@material-ui/core"
+import ExplainerTile from "./explainer-tile/explainer-tile"
 import {
   tileContainer,
   sectionHeader,
   topicsSectionHeader,
   topicsSectionContainer,
-  line
-} from "./topics-section.module.scss";
-import {withWindowSize} from "react-fns";
-import {breakpointLg} from "../../variables.module.scss";
-import {pxToNumber} from "../../helpers/styles-helper/styles-helper";
-import {pageTileMap} from "./explainer-tile/explainer-tile-helper";
+  line,
+} from "./topics-section.module.scss"
+import { withWindowSize } from "react-fns"
+import { breakpointLg } from "../../variables.module.scss"
+import { pxToNumber } from "../../helpers/styles-helper/styles-helper"
+import { pageTileMap } from "./explainer-tile/explainer-tile-helper"
 
-
-export const TopicsSection = ({images, width}) => {
-  const mainWidth = 8;
-  const secondaryWidth = 4;
+export const TopicsSection = ({ images, width }) => {
+  const mainWidth = 8
+  const secondaryWidth = 4
 
   const theme = createTheme({
     breakpoints: {
       values: {
-        lg: 992
-      }
-    }
+        lg: 992,
+      },
+    },
   })
 
   return (
     <div className={topicsSectionContainer}>
-      <div className={sectionHeader}>
-        TOPICS
-      </div>
+      <div className={sectionHeader}>TOPICS</div>
       <h5 className={topicsSectionHeader}>
-        <FontAwesomeIcon icon={faBookOpen}/>
-        <div>
-          Your Guide to America’s Finances
-        </div>
+        <FontAwesomeIcon icon={faBookOpen} />
+        <div>Your Guide to America’s Finances</div>
       </h5>
       <i>
-        Fiscal Data presents the third of four concepts from Your Guide
-        to America’s Finances, which will be added in the coming months.
+        Fiscal Data presents the third of four concepts from Your Guide to
+        America’s Finances, which will be added in the coming months.
       </i>
       <div className={tileContainer}>
         <ThemeProvider theme={theme}>
           <Grid container spacing={4}>
             <Grid item lg={mainWidth}>
-              <ExplainerTile content={pageTileMap['spending']}
-                             images={images}
-                             width={width}
+              <ExplainerTile
+                content={pageTileMap["americas-finance-guide"]}
+                images={images}
+                width={width}
+              />
+              <ExplainerTile
+                content={pageTileMap["revenue"]}
+                images={images}
+                width={width}
               />
             </Grid>
-            {width < pxToNumber(breakpointLg) ? <div className={line}/> : undefined}
+            {width < pxToNumber(breakpointLg) ? (
+              <div className={line} />
+            ) : (
+              undefined
+            )}
             <Grid item lg={secondaryWidth}>
-
-              <ExplainerTile content={pageTileMap['deficit']}
-                             images={images}
-                             width={width}
-
+              <ExplainerTile
+                content={pageTileMap["spending"]}
+                images={images}
+                width={width}
               />
-              <div className={line}/>
-              <ExplainerTile  content={pageTileMap['debt']}
-                             images={images}
-                             width={width}
+              <ExplainerTile
+                content={pageTileMap["deficit"]}
+                images={images}
+                width={width}
               />
-
+              <div className={line} />
+              <ExplainerTile
+                content={pageTileMap["debt"]}
+                images={images}
+                width={width}
+              />
             </Grid>
           </Grid>
         </ThemeProvider>
       </div>
     </div>
   )
-};
+}
 
-export default withWindowSize(TopicsSection);
-
+export default withWindowSize(TopicsSection)
