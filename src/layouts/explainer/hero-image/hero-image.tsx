8245173265965cb26badda1bc5c96bfc7aafcb36
Fareez Ahmed <fareez.ahmed@kc.frb.org>
2022-10-03 14:37:33 -0700

    fix: add amount to national debt

--- src/layouts/explainer/hero-image/hero-image.tsx
+++ src/layouts/explainer/hero-image/hero-image.tsx
@@ -20,11 +20,11 @@ const HeroImage: FunctionComponent<IHeroImage> = ({
   children,
   pageName,
 }) => {
-  console.log(pageName, "THE PAGE NAME");
   const [debtAmount, setDebtAmount] = useState("");
   useEffect(() => {
     const debtUrl = `https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/debt_to_penny?sort=-record_date&page[size]=1`;
     basicFetch(`${debtUrl}`).then(res => {
+      if (!res?.data) return;
       setDebtAmount(res?.data[0]?.tot_pub_debt_out_amt);
     });
   }, []);
