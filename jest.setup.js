ff85315e1c9f0fb8180d90f74bdcbcccf4b8db42
Wyatt Chandler <wyatt.chandler@kc.frb.org>
2022-10-31 16:24:21 -0500

    FDG-5479 Have jest overlook untestable intersection observer

--- jest.setup.js
+++ jest.setup.js
@@ -26,4 +26,24 @@ jest.mock('rxjs/webSocket',() => ({
   }
 }));
 
+global.IntersectionObserver = class IntersectionObserver {
+  constructor() {}
+
+  disconnect() {
+    return null;
+  }
+
+  observe() {
+    return null;
+  }
+
+  takeRecords() {
+    return null;
+  }
+
+  unobserve() {
+    return null;
+  }
+};
+
 jest.setTimeout(750000);
