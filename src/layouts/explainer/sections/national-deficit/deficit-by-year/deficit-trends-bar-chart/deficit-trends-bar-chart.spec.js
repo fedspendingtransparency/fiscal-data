561263bf527b217b508e493aa349884112a8fe13
Wyatt Chandler <wyatt.chandler@kc.frb.org>
2022-11-22 16:59:07 -0600

    FDG-6260 Removed unused import

--- src/layouts/explainer/sections/national-deficit/deficit-by-year/deficit-trends-bar-chart/deficit-trends-bar-chart.spec.js
+++ src/layouts/explainer/sections/national-deficit/deficit-by-year/deficit-trends-bar-chart/deficit-trends-bar-chart.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render, waitFor, screen} from '@testing-library/react';
+import {render, waitFor} from '@testing-library/react';
 import {setGlobalFetchResponse} from "../../../../../../utils/mock-utils";
 import {mockDeficitTrendsData} from "../../../../explainer-test-helper";
 import {DeficitTrendsBarChart} from "./deficit-trends-bar-chart";
