8cc753ea171f7fb1b15454486e0dc10976cce470
Wyatt Chandler <wyatt.chandler@kc.frb.org>
2022-12-07 13:51:00 -0600

    Revert "Merge pull request #391 from fedspendingtransparency/FDG-5314"
    This reverts commit dd7e10dbc53567f7973e7308a14b1ead55ef7ef7, reversing
    changes made to fdc6668d4c638d2dd3fafd33eb91b49549d21593.

--- src/components/anchor-text/anchor-text.tsx
+++ src/components/anchor-text/anchor-text.tsx
@@ -1,23 +1,13 @@
-import React from 'react';
+
+import React from "react";
 
 type IAnchorText = {
-  link: string;
-  text: string;
-};
+    link: string,
+    text: string
+  }
 
-const AnchorText = ({ link, text }: IAnchorText): JSX.Element => {
-  return (
-    <sup>
-      <a
-        href="#footnote"
-        id={link}
-        data-testid="anchor-text"
-        className="primary"
-      >
-        {text}
-      </a>
-    </sup>
-  );
-};
+const AnchorText = ({link, text}: IAnchorText): JSX.Element => {
+    return (<a href="#footnote" id={link} data-testid="anchor-text" className="primary">{text}</a>)
+}
 
-export default AnchorText;
+export default AnchorText
