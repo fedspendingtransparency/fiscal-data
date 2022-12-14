50b9f1e6f97c4c93f1f00c10f51a4b652093f42d
Joel Mendoza <joel.mendoza@kc.frb.org>
2022-12-14 09:19:15 -0600

    FDG-6408 fixed roman numerals

--- src/helpers/footnotes-helper/footnotes-helper.jsx
+++ src/helpers/footnotes-helper/footnotes-helper.jsx
@@ -1,17 +1,17 @@
 export const getAFGFootnotes = currentFiscalYear => {
     return [
       {
-        text: 'i',
+        text: '1',
         link: 'CurrentFiscalYear',
         body: `The current fiscal year began on October 1, ${currentFiscalYear-1} and ends September 30, ${currentFiscalYear}.`,
       },
       {
-        text: 'ii',
+        text: '2',
         link: 'LatestFiscalYear',
         body: `The latest complete fiscal year began on October 1, ${currentFiscalYear-2} and ended September 30, ${currentFiscalYear-1}.`,
       },
       {
-        text: 'iii',
+        text: '3',
         link: 'PreviousFiscalYear',
         body: `The previous fiscal year began on October 1, ${currentFiscalYear-3} and ended September 30, ${currentFiscalYear-2}.`,
       },
