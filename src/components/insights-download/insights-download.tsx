c0929bbbf14567a80b7e5e5d247997eb2702da24
Joel Mendoza <joel.mendoza@kc.frb.org>
2022-12-20 10:01:35 -0600

    FDG-6559 changed button to anchor for hover hint

--- src/components/insights-download/insights-download.tsx
+++ src/components/insights-download/insights-download.tsx
@@ -8,46 +8,15 @@ type InsightsDownloadProps = {
   dataDate: string
 }
 
-
-
 const InsightsDownload = ({downloadLink, dataDate}: InsightsDownloadProps): JSX.Element => {
-
-    const fileName = downloadLink.split('/').pop();
-
-    const getFile = (downloadLink) => {
-      fetch(downloadLink, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'text/csv'
-        },
-      })
-      .then((res) => res.blob())
-      .then((blob) => {
-        const url = window.URL.createObjectURL(
-          new Blob([blob]),
-        );
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute(
-          'download',
-          fileName,
-        );
-
-        document.body.appendChild(link);
-        link.click();
-        link.parentNode.removeChild(link);
-
-      })
-    }
-
     return (
       <div className={downloadContainer} data-testid={'insights-download'}>
-        <button onClick={() => getFile(downloadLink)} className={downloadButton} data-testid={'download-button'}>
-          <div className={downloadIcon}>
+          <a href={downloadLink} className={downloadButton} data-testid={'download-button'} download>
+            <div className={downloadIcon}>
             <FontAwesomeIcon icon={faFileDownload} title={'download'} />
-          </div>
-          Download the data (.csv) as of {dataDate}
-        </button>
+            </div>
+            Download the data (.csv) as of {dataDate}
+          </a>
       </div>
   )
 }
