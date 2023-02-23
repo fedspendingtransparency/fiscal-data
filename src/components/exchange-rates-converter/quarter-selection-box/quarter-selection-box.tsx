ef72650994dd57dc461f534facdb67bdec8072a8
Wyatt Chandler <wyatt.chandler@kc.frb.org>
2023-02-23 16:50:05 -0600

    FDG-6631 removed unused code

--- src/components/exchange-rates-converter/quarter-selection-box/quarter-selection-box.tsx
+++ src/components/exchange-rates-converter/quarter-selection-box/quarter-selection-box.tsx
@@ -2,9 +2,6 @@ import {dateBox} from './quarter-selection-box.module.scss';
 import React, {FunctionComponent} from 'react';
 
 const QuarterSelectionBox:FunctionComponent = () => {
-
-  const quarters = ['1','2','3','4'];
-
   return (
     <div className={dateBox}>
     </div>
