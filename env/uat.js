36006a31c1b6c4adfade946c7d64ea6d7646e569
Wyatt Chandler <wyatt.chandler@kc.frb.org>
2023-03-03 11:04:44 -0600

    FDG-6510 Moved TMA to prod

--- env/uat.js
+++ env/uat.js
@@ -6,24 +6,6 @@ module.exports = {
   WEB_SOCKET_BASE_URL: 'wss://downloads.uat.fiscaldata.treasury.gov/main',
   EXPERIMENTAL_WHITELIST: ['chartingConfigurationTool', 'experimental-page'],
   ADDITIONAL_DATASETS: {
-    "015-BFS-2014Q3-040": {
-      "seoConfig": {
-        "pageTitle": "Treasury Managed Accounts",
-        "description": "Balances of Contract Disputes Receivables, "
-          + "No FEAR Act Receivables, and Unclaimed Money for Treasury Managed Accounts.",
-        "keywords": "Financial Summaries"
-      },
-      "topics": [
-        "financial-summaries"
-      ],
-      "relatedDatasets": [
-        "015-BFS-2014Q3-105",
-        "015-BFS-2014Q1-07",
-        "015-BFS-2014Q1-13"
-      ],
-      "slug": "/treasury-managed-accounts/",
-      "currentDateButton": "byMonth"
-    }
   },
   ADDITIONAL_ENDPOINTS: {
     '27': {
