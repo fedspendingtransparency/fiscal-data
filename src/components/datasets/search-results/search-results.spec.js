92f8a103ba3e80f73064f925f19971f4f4aa346e
Wyatt Chandler <wyatt.chandler@kc.frb.org>
2023-07-05 12:55:20 -0500

    FDG-3894 Adjusted test

--- src/components/datasets/search-results/search-results.spec.js
+++ src/components/datasets/search-results/search-results.spec.js
@@ -67,7 +67,7 @@ describe('Search Results', () => {
       />
     );
 
-    expect(getAllByTestId('cardPlacement')).toHaveLength(mockAllDatasets.length);
+    expect(getAllByTestId('cardPlacement')).toHaveLength(mockFilteredDatasets.length);
   });
 
   it('places the sort control', () => {
@@ -120,7 +120,7 @@ describe('Search Results', () => {
     );
 
     expect(getByText(SortOptions[0].label)).toBeInTheDocument();
-    
+
     rerender(
       <SearchResults
         allDatasets={mockAllDatasets}
