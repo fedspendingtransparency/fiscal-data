d028c4741ae6fa70f277efb6ac2b8a3bc83fd2b6
j1jsh04 <jordan.hertzler@kc.frb.org>
2023-08-02 14:02:46 -0500

    FDG-7581 Add banner to DTS DDP

--- src/components/banner-callout/banner-callout.jsx
+++ src/components/banner-callout/banner-callout.jsx
@@ -1,31 +1,38 @@
 import React from 'react';
 import CustomLink  from '../links/custom-link/custom-link';
 import { banner, sideTab, calloutText, icon } from './banner-callout.module.scss';
-import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'; 
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const BannerCallout = ({bannerCallout}) => {
-    const callouts = {
-        "XRCallout": <>To calculate foreign currency exchange rates for tax reporting, visit the{" "}
-        <CustomLink url={"/currency-exchange-rates-converter/"}>
-            Currency Exchange Rates Converter</CustomLink>
-            {" "}page.</>
+    const calloutCopy = {
+        "XRCallout":
+            <>
+              To calculate foreign currency exchange rates for tax reporting, visit the{" "}
+              <CustomLink url={"/currency-exchange-rates-converter/"}>Currency Exchange Rates Converter</CustomLink>
+              {" "}page.
+            </>,
+        "DTSAPIUpdate":
+            <>
+              NOTICE: UPDATES COMING SOON! The Daily Treasury Statement (DTS) dataset will be updated to match the published DTS.
+              All of the DTS API endpoints will be renamed to reflect the DTS table names. The Federal Tax Deposits and Short-Term
+              Cash Investments tables will contain historical data only (through Feb. 13, 2023). There will be a new API endpoint
+              for the Inter-Agency Tax Transfers table, which started on Feb. 14, 2023.
+            </>
     }
 
-    const currentCallout = callouts[bannerCallout];
+    const currentCallout = calloutCopy[bannerCallout];
 
-    if(currentCallout){
+    if (currentCallout) {
         return (
             <div className={banner} data-testid="banner">
-                <div className={sideTab}></div>
-                
+                <div className={sideTab} />
                 <span className={calloutText}>
-                    <FontAwesomeIcon className={icon} icon={faCircleInfo}/>
+                    <FontAwesomeIcon className={icon} icon={faCircleInfo} />
                     <div>
-                        {currentCallout}
+                      {currentCallout}
                     </div>
                 </span>
-                
             </div>
         );
     } else {
