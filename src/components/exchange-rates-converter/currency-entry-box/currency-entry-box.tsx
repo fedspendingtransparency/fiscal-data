bb53b829b8100b16d842e0851332cb07419c2f96
jhertzler <jordan.hertzler@kc.frb.org>
2023-09-11 11:08:23 -0500

    FDG-7441 Move content

--- src/components/exchange-rates-converter/currency-entry-box/currency-entry-box.tsx
+++ src/components/exchange-rates-converter/currency-entry-box/currency-entry-box.tsx
@@ -1,4 +1,4 @@
-import React, {FunctionComponent} from "react";
+import React, { FunctionComponent } from 'react';
 import {
   currencyBody,
   currencyBox,
@@ -6,40 +6,66 @@ import {
   currencyText,
 } from './currency-entry-box.module.scss';
 import ComboCurrencySelect from '../../combo-select/combo-currency-select/combo-currency-select';
+import { faDollarSign, faGlobe } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
-interface ICurrencyEntryBox  {
-  defaultCurrency: string,
-  currencyValue: string,
-  dropdown ? : boolean,
-  selectedCurrency ?,
-  onCurrencyChange ?,
-  onCurrencyValueChange,
-  options ? : [],
-  resetFilterCount: number,
-  testId: string,
+interface ICurrencyEntryBox {
+  defaultCurrency: string;
+  currencyValue: string;
+  dropdown?: boolean;
+  selectedCurrency?;
+  onCurrencyChange?;
+  onCurrencyValueChange;
+  options?: [];
+  testId: string;
 }
 
-const noNonNumericChar = (event) => {
+const noNonNumericChar = event => {
   // Prevents users from typing 'e', 'E', or '-'
-  return (event.key === 'e' || event.key === 'E' || event.key === '-') && event.preventDefault();
-}
+  return (
+    (event.key === 'e' || event.key === 'E' || event.key === '-') &&
+    event.preventDefault()
+  );
+};
 
-const CurrencyEntryBox:FunctionComponent<ICurrencyEntryBox> = (
-  {
-    defaultCurrency,
-    dropdown=false,
-    currencyValue,
-    onCurrencyValueChange,
-    onCurrencyChange,
-    options,
-    selectedCurrency,
-    resetFilterCount,
-    testId
-  }) => {
+const CurrencyEntryBox: FunctionComponent<ICurrencyEntryBox> = ({
+  defaultCurrency,
+  dropdown = false,
+  currencyValue,
+  onCurrencyValueChange,
+  onCurrencyChange,
+  options,
+  selectedCurrency,
+  testId,
+}) => {
   return (
     <>
-      {dropdown ?
+      {dropdown ? (
         <div className={currencyBox} data-testid={testId}>
+          <div>
+            <FontAwesomeIcon icon={faGlobe as IconProp} />
+            FOREIGN CURRENCY
+          </div>
+          <div>Amount</div>
+          <div className={currencyBody}>
+            <div className={currencyText}>
+              {currencyValue === '--' ? (
+                <div>{currencyValue}</div>
+              ) : (
+                <input
+                  type="number"
+                  inputMode="numeric"
+                  step="any"
+                  onKeyDown={noNonNumericChar}
+                  onChange={onCurrencyValueChange}
+                  value={currencyValue}
+                  data-testid="input-dropdown"
+                />
+              )}
+            </div>
+          </div>
+          <span>Country-Currency</span>
           {options && (
             <ComboCurrencySelect
               selectedOption={selectedCurrency}
@@ -51,42 +77,39 @@ const CurrencyEntryBox:FunctionComponent<ICurrencyEntryBox> = (
               disabledMessage="This option has no data for the selected quarter."
             />
           )}
+        </div>
+      ) : (
+        <div className={currencyBox}>
+          <div>
+            <FontAwesomeIcon icon={faDollarSign as IconProp} />
+            U.S. DOLLAR
+          </div>
+          <div>Amount</div>
           <div className={currencyBody}>
             <div className={currencyText}>
               {currencyValue === '--' ? (
-                <div>
-                  {currencyValue}
-                </div>
+                <div>{currencyValue}</div>
               ) : (
-              <input type="number" inputMode="numeric" step="any" onKeyDown={noNonNumericChar}
-                     onChange={onCurrencyValueChange} value={currencyValue} data-testid="input-dropdown"
-              />
-              )}
-            </div>
-          </div>
-        </div> :
-        <div className={currencyBox} >
-          <div className={currencyHeader}>
-            <span>{defaultCurrency}</span>
-          </div>
-          <div className={currencyBody}>
-            <div className={currencyText}>
-              {currencyValue === '--' ? (
-                <div>
-                  {currencyValue}
-                </div>
-              ) : (
-                <input type="number" inputMode="numeric" step="any" onKeyDown={noNonNumericChar}
-                       onChange={onCurrencyValueChange}
-                       value={currencyValue} data-testid="input"
+                <input
+                  type="number"
+                  inputMode="numeric"
+                  step="any"
+                  onKeyDown={noNonNumericChar}
+                  onChange={onCurrencyValueChange}
+                  value={currencyValue}
+                  data-testid="input"
                 />
               )}
             </div>
           </div>
+          <span>Country-Currency</span>
+          <div className={currencyHeader}>
+            <span>{defaultCurrency}</span>
+          </div>
         </div>
-      }
+      )}
     </>
   );
-}
+};
 
 export default CurrencyEntryBox;
