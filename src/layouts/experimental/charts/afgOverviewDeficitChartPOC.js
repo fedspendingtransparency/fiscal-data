32d46ebbf4deb220a931f785c832a9c3b606f94b
Hallie Martin <hallie.martin@kc.frb.org>
2023-09-13 10:27:38 -0500

    FDG-7676 add midpoint dot animation

--- src/layouts/experimental/charts/afgOverviewDeficitChartPOC.js
+++ src/layouts/experimental/charts/afgOverviewDeficitChartPOC.js
@@ -2,12 +2,13 @@
 
 // still don't know how to make content custom but when figure out then use points to animate 
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import {
   Line,
+  Dot,
   XAxis,
   YAxis,
-  LineChart, CartesianGrid,
+  LineChart, CartesianGrid, ReferenceDot,
 } from 'recharts';
 import { deficitExplainerPrimary } from "../../explainer/sections/national-deficit/national-deficit.module.scss";
 import {
@@ -16,6 +17,8 @@ import {
 import {
    revenueExplainerPrimary
 } from "../../explainer/sections/government-revenue/revenue.module.scss";
+import { spendingExplainerPrimaryDot, revenueExplainerPrimaryDot } from '../experimental.module.scss';
+import { green } from '@material-ui/core/colors';
 
 
 const AFGDeficitPOC = () => {
@@ -26,41 +29,44 @@ const AFGDeficitPOC = () => {
   const testData = [
     {
       data: [
-        {year: '2016', value: 0.5, type: 'spending'},
-        {year: '2016', value: 2, type: 'revenue'},
+        {year: '2016', value: 0.5, type: 'revenue'},
+        {year: '2016', value: 2, type: 'spending'},
       ]
     },
     {
       data: [
-        {year: '2017', value: 1, type: 'spending'},
-        {year: '2017', value: 2.5, type: 'revenue'},
+        {year: '2017', value: 1, type: 'revenue'},
+        {year: '2017', value: 2.5, type: 'spending'},
       ]
     },
     {
       data: [
-        {year: '2018', value: 2.4, type: 'spending'},
-        {year: '2018', value: 4, type: 'revenue'},
+        {year: '2018', value: 2.4, type: 'revenue'},
+        {year: '2018', value: 4, type: 'spending'},
       ]
     },
     {
       data: [
-        {year: '2019', value: 3.5, type: 'spending'},
-        {year: '2019', value: 6, type: 'revenue'},
+        {year: '2019', value: 3.5, type: 'revenue'},
+        {year: '2019', value: 6, type: 'spending'},
       ]
     },
     {
       data: [
-        {year: '2020', value: 5.5, type: 'spending'},
-        {year: '2020', value: 7, type: 'revenue'},
+        {year: '2020', value: 5.5, type: 'revenue'},
+        {year: '2020', value: 7, type: 'spending'},
       ]
     },
     {
       data: [
-        {year: '2021', value: 6, type: 'spending', latest: true},
-        {year: '2021', value: 8, type: 'revenue', latest: true},
+        {year: '2021', value: 6, type: 'revenue', latest: true},
+        {year: '2021', value: 8, type: 'spending', latest: true},
       ]
     }
   ]
+
+  const animationDots = [];
+  let yAxisCx = 0;
 
   const chartWidth = 730;
   const midPointArray = [];
@@ -71,38 +77,41 @@ const AFGDeficitPOC = () => {
         value: ((curData.data[0]?.value + curData.data[1]?.value) / 2)
       }
       midPointArray.push(midPoint);
-    console.log("ARRAY: ", midPointArray);
     });
-
-  const getCurrentX = () => {
-    const curX = chartRef.current?.container.offsetLeft;
-    console.log("LOOK HERE REF", chartRef);
-    console.log("LOOK HERE", curX);
-    setX(x);
-  }
-
-
-  // call it when ref
-  useEffect(() => {
-    getCurrentX();
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener("resize", getCurrentX);
-  }, []);
 
   const CustomDot = (props) => {
     const {cx, cy, payload, strokeWidth, r} = props;
+
+    const currentLine = testData.find(line => {
+      return line.data[0]?.year === payload?.year;
+    });
+
+    const curDot = {
+      year: payload.year,
+      color: payload.type === 'spending' ? spendingExplainerPrimary : revenueExplainerPrimary,
+      cx: cx,
+      cy: cy
+    }
+
+    animationDots.push(curDot);
+
+
+
+    if (payload.type === 'spending') {
+      currentLine.data[0].spending_cx = cx;
+      currentLine.data[1].spending_cx = cx;
+    }
+    else {
+      currentLine.data[0].revenue_cx = cx;
+      currentLine.data[1].revenue_cx = cx;
+    }
+
+    let pixelMidPoint = null; 
+
     const color = payload?.type === 'spending' ? spendingExplainerPrimary : revenueExplainerPrimary;
-   // const curStyle = payload?.type === 'spending' ? "z-index: 1;" : '';
     const fill =  payload?.latest ? null : color;
-    const startValue = midPointArray.find(currentPoint => currentPoint.year === payload.year).value;
-    const endValue = payload.value; 
-    const startXPoint = (((cx-110) / endValue) * startValue) + 110;
-    const endXPoint = cx;
-    console.log("START VALUE ", startValue)
-    console.log("START X -- WANT TO BE THE SAME ", startXPoint)
-    console.log("END ",  endValue)
+    const midPointValue = midPointArray.find(currentPoint => currentPoint.year === payload.year).value;
+    pixelMidPoint = ((midPointValue * (cx - yAxisCx)) / payload.value) + 80;
 
     return (
       <>
@@ -117,7 +126,7 @@ const AFGDeficitPOC = () => {
         >
           <animate 
             attributeName='cx'
-            from={startXPoint}
+            from={pixelMidPoint}
             to={cx}
             dur='1s'
           />
@@ -130,11 +139,10 @@ const AFGDeficitPOC = () => {
           fillOpacity={1}
           cx={cx}
           cy={cy}
-          //style={curStyle} // how to specify style
         >
           <animate 
             attributeName='cx'
-            from={startXPoint}
+            from={pixelMidPoint}
             to={cx}
             dur='1s'
           />
@@ -143,19 +151,13 @@ const AFGDeficitPOC = () => {
       </>
     )
   }
-// how to make it take my version of the line
-// stroke probs isnt the right answer
-// make sure my line style is fine
-  const lineStyle = {
-    stroke: spendingExplainerPrimary, 
-    'stroke-width':2
-  };
 
-  const CustomLine = (props) => {
+  const CustomShape = (props) => {
+    console.log(props)
+    yAxisCx = props.cx
+    console.log(yAxisCx)
     return (
       <>
-        <line x1="0" y1="0" x2="200" y2="200" style={lineStyle} />
-          
       </>
     )
   }
@@ -163,7 +165,7 @@ const AFGDeficitPOC = () => {
   return (
     <>
       <LineChart
-        key={Math.random()}
+        //key={Math.random()}
         width={chartWidth}
         height={250}
         margin={{
@@ -175,20 +177,56 @@ const AFGDeficitPOC = () => {
         <CartesianGrid horizontal={false} />
         <YAxis dataKey="year" type="category" allowDuplicatedCategory={false} axisLine={false} tickLine={false} />
         <XAxis ticks={[0,2,4,6,8]} type="number" tickFormatter={(value) => `$${value}`} axisLine={false} tickLine={false} />
+        <ReferenceDot x={0} y='2016' shape={<CustomShape />} />
         {testData.map((s) =>
-
+        // maybe not use built in animate
+        // custom dots/points?
+        // do some math to know where the different dots should be 
+            // startingPoint = (spending + revenue) / 2;
+            // totalLineLengthInDollars = spending > revenue ? spending - revenue : revenue - spending;
+            // howMuchDollarPerMillisecond = totalLength / 1000;
+            // howMuchInEachDirection = howMuchDollarPerMillisecond / 2;
+        // make it a smaller line and data changes getting bigger and bigger line as animation goes on 
+          // starts with just spending point
+          // change every milisecond 
           <Line dataKey="value"
                 data={s.data} stroke={deficitExplainerPrimary} strokeWidth={6} strokeOpacity={1}
                 dot={<CustomDot />}
                 isAnimationActive={false}
                 // one second delay
-                //animationBegin={1000}
+                // animationBegin={1500}
                 // one second animation
                 //animationDuration={1000}
           />
+          
         )}
+
+        {
+          testData.map((s) => {
+console.log(JSON.stringify(s))
+            return (
+            <circle  cx={s.data[0]?.spending_cx} cy={s.data[0]?.spending_cy} r={10} fillOpacity={1} strokeWidth={10} />
+
+            )
+          }
+            
+          )
+        }
       </LineChart>
     </>
   )
 }
 export default AFGDeficitPOC;
+
+// custom line component
+  // css transitions
+  // delay adding points to chart?
+
+// look at car charts using spring and bar chart on Spending using css transitions
+
+// component called customized -- catch all -- look at docs
+// animation easing
+
+
+// seeing if I can determine how many pixels to use then I can math to make it start in the middle
+//     and the animation/transition can include positioning left or something
