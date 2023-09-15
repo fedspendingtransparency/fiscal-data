4d9b41f9877df43b8a0c1b8fcba88a8271384fa8
wyattch <wyatt.chandler@kc.frb.org>
2023-09-15 10:31:52 -0500

    FDG-7840 React table data formatting

--- src/components/data-table/data-table.tsx
+++ src/components/data-table/data-table.tsx
@@ -8,6 +8,7 @@ import {
   getFilteredRowModel,
   SortingState,
 } from '@tanstack/react-table';
+import { currencyFormatter, numberFormatter } from '../../helpers/text-format/text-format';
 import DataTableFooter from './data-table-footer/data-table-footer';
 
 import StickyTable from 'react-sticky-table-thead';
@@ -42,6 +43,16 @@ type DataTableProps = {
   setFiltersActive: (value: boolean) => void;
 };
 
+const customFormat = (stringValue, decimalPlaces) => {
+  // if block is to show "-$123,123.23" instead of "$-123,123.23"
+  const absVal = Math.abs(stringValue);
+  let returnString = '$' + absVal.toFixed(decimalPlaces).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+  if (Number(stringValue) < 0) {
+    returnString = '-' + returnString;
+  }
+  return returnString;
+};
+
 const DataTable: FunctionComponent<DataTableProps> = ({
   rawData,
   defaultSelectedColumns,
@@ -58,20 +69,82 @@ const DataTable: FunctionComponent<DataTableProps> = ({
   pageSize,
   setFiltersActive,
 }) => {
+  console.log(rawData);
   const allColumns = rawData.meta
     ? Object.entries(rawData.meta.labels).map(([field, label]) => {
-      if (field === 'record_date') {
-        return {
-          accessorKey: field,
-          header: label,
-          filterFn: 'equalsString',
-          cell: ({ getValue }) => {
-            return moment(getValue()).format('MM/DD/YYYY');
-          },
-        } as ColumnDef<any, any>;
-      }
-      return { accessorKey: field, header: label } as ColumnDef<any, any>;
-    })
+        if (field === 'record_date') {
+          return {
+            accessorKey: field,
+            header: label,
+            filterFn: 'equalsString',
+            cell: ({ getValue }) => {
+              return moment(getValue()).format('M/D/YYYY');
+            },
+          } as ColumnDef<any, any>;
+        } else if (rawData.meta.dataTypes[field] === 'DATE') {
+          return {
+            accessorKey: field,
+            header: label,
+            cell: ({ getValue }) => {
+              return moment(getValue()).format('M/D/YYYY');
+            },
+          } as ColumnDef<any, any>;
+        } else if (rawData.meta.dataTypes[field] === 'NUMBER') {
+          return {
+            accessorKey: field,
+            header: label,
+            cell: ({ getValue }) => {
+              return numberFormatter.format(getValue());
+            },
+          } as ColumnDef<any, any>;
+        } else if (rawData.meta.dataTypes[field] === 'PERCENTAGE') {
+          return {
+            accessorKey: field,
+            header: label,
+            cell: ({ getValue }) => {
+              return `${getValue()}%`;
+            },
+          } as ColumnDef<any, any>;
+        } else if (rawData.meta.dataTypes[field] === 'SMALL_FRACTION') {
+          return {
+            accessorKey: field,
+            header: label,
+            cell: ({ getValue }) => {
+              return new Intl.NumberFormat('en-US', { maximumSignificantDigits: 5 }).format(getValue());
+            },
+          } as ColumnDef<any, any>;
+        } else if (rawData.meta.dataTypes[field] === 'CURRENCY') {
+          return {
+            accessorKey: field,
+            header: label,
+            cell: ({ getValue }) => {
+              return currencyFormatter.format(getValue());
+            },
+          } as ColumnDef<any, any>;
+        } else if (rawData.meta.dataTypes[field].includes('CURRENCY') && /\d/.test(rawData.meta.dataTypes[field].split('CURRENCY')[1])) {
+          const decimalPlaces = parseInt(rawData.meta.dataTypes[field].split('CURRENCY')[1]);
+          return {
+            accessorKey: field,
+            header: label,
+            cell: ({ getValue }) => {
+              return customFormat(getValue(), decimalPlaces);
+            },
+          } as ColumnDef<any, any>;
+        } else if (rawData.meta.dataTypes[field] === 'STRING') {
+          return {
+            accessorKey: field,
+            header: label,
+            cell: ({ getValue }) => {
+              if (getValue().includes('%')) {
+                return getValue().replace(/-/g, '\u2011');
+              } else {
+                return getValue();
+              }
+            },
+          } as ColumnDef<any, any>;
+        }
+        return { accessorKey: field, header: label } as ColumnDef<any, any>;
+      })
     : [];
   const data = rawData.data;
 
