5b9311217699a8092c64b0ee8ddfabb795c0769e
jhertzler <jordan.hertzler@kc.frb.org>
2023-09-19 13:57:16 -0500

    FDG-7796 Date range picker dropdown

--- src/components/data-table/data-table-header/date-range-filter/date-range-filter.tsx
+++ src/components/data-table/data-table-header/date-range-filter/date-range-filter.tsx
@@ -5,9 +5,17 @@ import { DesktopDatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
 import { DateRangePicker } from 'react-date-range';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
 
 const DateRangeFilter: FunctionComponent<any> = ({ column }: { column: Column<any, any> }) => {
   const [date, setDate] = useState(null);
+  const [visibile, setVisible] = useState(false);
+  const [selectedRange, setSelectedRange] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection',
+  });
 
   useEffect(() => {
     if (date) {
@@ -21,7 +29,36 @@ const DateRangeFilter: FunctionComponent<any> = ({ column }: { column: Column<an
     }
   }, [date]);
 
-  return <></>;
+  const handleChange = ranges => {
+    const newDateRange = {
+      startDate: ranges.selection.startDate,
+      endDate: ranges.selection.endDate,
+      key: 'selection',
+    };
+    setSelectedRange(newDateRange);
+  };
+
+  return (
+    <>
+      <button onClick={() => setVisible(!visibile)}>Toggle Date Picker</button>
+      {visibile && (
+        <div style={{ width: '355px', height: '275px', position: 'absolute', overflow: 'hidden' }}>
+          <div style={{ position: 'absolute', left: '-227px' }}>
+            <DateRangePicker
+              ranges={[selectedRange]}
+              onChange={handleChange}
+              showSelectionPreview={false}
+              shownDate={false}
+              showDateDisplay={false}
+              showMonthAndYearPicker={false}
+              staticRanges={[]}
+              inputRanges={[]}
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default DateRangeFilter;
