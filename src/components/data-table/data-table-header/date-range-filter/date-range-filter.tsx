c0e3eb52baaef2564668ecad2b421c3897184b4d
jhertzler <jordan.hertzler@kc.frb.org>
2023-09-21 15:01:34 -0500

    FDG-7796 WIP

--- src/components/data-table/data-table-header/date-range-filter/date-range-filter.tsx
+++ src/components/data-table/data-table-header/date-range-filter/date-range-filter.tsx
@@ -1,59 +1,81 @@
 import React, { FunctionComponent, useEffect, useState } from 'react';
 import { Column } from '@tanstack/react-table';
 import moment from 'moment/moment';
-import { DesktopDatePicker, LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import dayjs from 'dayjs';
 import { DateRangePicker } from 'react-date-range';
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
-
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { faCalendarDay } from '@fortawesome/free-solid-svg-icons';
+import {
+  dateEntryBox,
+  dropdown,
+  datePickerContainer,
+  calendarIcon,
+  dateText,
+  buttonContainer,
+  datePickerButton,
+  datePickerSelected,
+  datePickerHover,
+} from './date-range-filter.module.scss';
 const DateRangeFilter: FunctionComponent<any> = ({ column }: { column: Column<any, any> }) => {
-  const [date, setDate] = useState(null);
-  const [visibile, setVisible] = useState(false);
-  const [selectedRange, setSelectedRange] = useState({
-    startDate: new Date(),
-    endDate: new Date(),
-    key: 'selection',
+  const [visible, setVisible] = useState(false);
+  const [selectedText, setSelectedText] = useState('');
+  const [selected, setSelected] = useState({
+    from: undefined,
+    to: undefined,
   });
 
-  useEffect(() => {
-    if (date) {
-      if (!isNaN(date.toDate().getTime())) {
-        column.setFilterValue(moment(date.toDate()).format('YYYY-MM-DD'));
-      } else {
-        column.setFilterValue('');
-      }
-    } else {
-      column.setFilterValue('');
-    }
-  }, [date]);
-
-  const handleChange = ranges => {
+  const todayOnClick = () => {
     const newDateRange = {
-      startDate: ranges.selection.startDate,
-      endDate: ranges.selection.endDate,
-      key: 'selection',
+      from: new Date(),
+      to: new Date(),
     };
-    setSelectedRange(newDateRange);
+    setSelected(newDateRange);
+    const start = moment(new Date()).format('MM/DD/YYYY');
+    const end = moment(new Date()).format('MM/DD/YYYY');
+    console.log(`${start} - ${end}`);
+    setSelectedText(`${start} - ${end}`);
   };
+  const clearOnClick = () => {
+    setSelected(undefined);
+    setSelectedText(``);
+  };
+  useEffect(() => {
+    if (selected?.from && selected?.to) {
+      const start = moment(selected?.from).format('MM/DD/YYYY');
+      const end = moment(selected?.to).format('MM/DD/YYYY');
+      setSelectedText(`${start} - ${end}`);
+    } else {
+      setSelectedText('');
+    }
+  }, [selected]);
 
   return (
     <>
-      <button onClick={() => setVisible(!visibile)}>Toggle Date Picker</button>
-      {visibile && (
-        <div style={{ width: '355px', height: '275px', position: 'absolute', overflow: 'hidden' }}>
-          <div style={{ position: 'absolute', left: '-227px' }}>
-            <DateRangePicker
-              ranges={[selectedRange]}
-              onChange={handleChange}
-              showSelectionPreview={false}
-              shownDate={false}
-              showDateDisplay={false}
-              showMonthAndYearPicker={false}
-              staticRanges={[]}
-              inputRanges={[]}
+      <div className={dateEntryBox}>
+        <div className={dateText}>{selectedText}</div>
+        <FontAwesomeIcon icon={faCalendarDay as IconProp} onClick={() => setVisible(!visible)} className={calendarIcon} />
+      </div>
+      {visible && (
+        <div className={dropdown}>
+          <div className={datePickerContainer}>
+            <DayPicker
+              mode="range"
+              selected={selected}
+              onSelect={setSelected}
+              modifiersClassNames={{ selected: datePickerSelected, focus: datePickerHover }}
             />
+          </div>
+          <div className={buttonContainer}>
+            <div role="button" onClick={todayOnClick} onKeyDown={todayOnClick} tabIndex={0} className={datePickerButton}>
+              Today
+            </div>
+            <div role="button" onClick={clearOnClick} onKeyDown={clearOnClick} tabIndex={0} className={datePickerButton}>
+              Clear
+            </div>
           </div>
         </div>
       )}
