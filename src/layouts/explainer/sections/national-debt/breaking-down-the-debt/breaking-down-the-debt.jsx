052c9bc0685aa1427507e42e7a6b4392c0d630c6
chasls2 <110061741+chasls2@users.noreply.github.com>
2023-10-16 10:08:45 -0500

    fixed issue

--- src/layouts/explainer/sections/national-debt/breaking-down-the-debt/breaking-down-the-debt.jsx
+++ src/layouts/explainer/sections/national-debt/breaking-down-the-debt/breaking-down-the-debt.jsx
@@ -395,8 +395,7 @@ const BreakingDownTheDebt = ({ sectionId, glossary, glossaryClickHandler, width 
           depends on the total national debt and the various securities’{" "}
           {glossaryTerms.interestRates}.
         </p>
-        <QuoteBox
-        icon={faDollarSign} primaryColor={debtExplainerPrimary} secondaryColor={debtExplainerLightSecondary} customTopMargin={'-16'}>
+        <QuoteBox icon={faDollarSign} primaryColor={debtExplainerPrimary} secondaryColor={debtExplainerLightSecondary} customTopMargin={'-16'}>
           <p>
           As of {interestExpenseEndMonth} {interestExpenseEndYear} it costs $
           {shortenedDebtExpense} billion to maintain the debt, which is{" "}
