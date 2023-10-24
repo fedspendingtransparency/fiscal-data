463c68f1a7b297278b43fa7d44fe3efb3f6a9988
jhertzler <jordan.hertzler@kc.frb.org>
2023-10-24 14:16:20 -0500

    FDG-8178 Mobile dot spacing

--- src/layouts/explainer/sections/overview/chart-components/chart-legend.jsx
+++ src/layouts/explainer/sections/overview/chart-components/chart-legend.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { dot, legend, legendItem } from '../deficit-chart/deficit-chart.module.scss';
+import { dot, legend, legendItem, dotMobile } from './chart-legend.module.scss';
+import classnames from 'classnames';
 
-const ChartLegend = ({ legendItems }) => {
+const ChartLegend = ({ legendItems, mobileDotSpacing }) => {
   return (
     <div className={legend}>
       {legendItems?.map((row, index) => {
         return (
           <div className={legendItem} key={index}>
-            <span className={dot} style={{ backgroundColor: row.color }}></span>
+            <span className={classnames([dot, mobileDotSpacing ? dotMobile : undefined])} style={{ backgroundColor: row.color }}></span>
             {row.title}
           </div>
         );
