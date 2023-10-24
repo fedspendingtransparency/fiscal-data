cf974d4f9b46e7ca37b75ed487135532ed4a62f9
chasls2 <110061741+chasls2@users.noreply.github.com>
2023-10-23 20:03:14 -0500

    change test

--- src/layouts/explainer/sections/overview/spending-chart/custom-tooltip/custom-tooltip.spec.js
+++ src/layouts/explainer/sections/overview/spending-chart/custom-tooltip/custom-tooltip.spec.js
@@ -1,26 +1,29 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import { render } from '@testing-library/react'; 
-import CustomTooltip from './custom-tooltip1';
-
-describe('AFG Spending Tooltip', () => {
-const mockPayload = [
-  {
-    payload: {
-      tooltip: [{label: 'Oct', value: 12000000000000, name: 2021}]
+import CustomTooltip from './custom-tooltip';
+describe('AFG Deficit Tooltip', () => {
+  const mockPayload = [
+    {
+      name: '2021 FYTD',
+      color: 'red',
+      value: 2.12345678,
+      payload: {
+        currentFY: 2021,
+        currentFYValue: 2.12345678,
+      },
     },
-  },
-];
+  ];
 
+  it('renders the tooltip', () => {
+    const { getByTestId, getByText } = render(<CustomTooltip payload={mockPayload} label={'Oct'} />);
+    expect(getByTestId('CustomTooltip')).toBeInTheDocument();
+    expect(getByText('2021 FYTD')).toBeInTheDocument();
+    expect(getByText('October')).toBeInTheDocument();
+    expect(getByText(`$2.12T`)).toBeInTheDocument();
+  });
 
-it('renders the tooltip', () => {
-  const { getByTestId, getByText } = render(<CustomTooltip payload={mockPayload} label={'Oct'} />);
-  expect(getByTestId('CustomTooltip')).toBeInTheDocument();
-  expect(getByText('October')).toBeInTheDocument();
-});
-
-it('does not render the tooltip when it is inactive', () => {
-  const { queryByTestId } = render(<CustomTooltip />);
-  expect(queryByTestId('CustomTooltip')).not.toBeInTheDocument();
-});
-
+  it('does not render the tooltip when it is inactive', () => {
+    const { queryByTestId } = render(<CustomTooltip />);
+    expect(queryByTestId('CustomTooltip')).not.toBeInTheDocument();
+  });
 });
