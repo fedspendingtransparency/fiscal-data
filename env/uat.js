c76ad8267afe93e8aee6f7aeaa39321b57879197
chasls2 <110061741+chasls2@users.noreply.github.com>
2023-10-30 09:39:32 -0500

    removed saving-bonds

--- env/uat.js
+++ env/uat.js
@@ -13,7 +13,7 @@ module.exports = {
           'The Treasury Securities Upcoming Auctions Data dataset provides information on auction announcements. Each announcement includes what securities are being auctioned, the announcement date, the auction date and issue date. This data provides a notification of what Treasury Marketable securities will be announced and or auctioned in the upcoming week.',
         keywords: 'Treasury Securities, Debt, Savings Bonds, Auctions',
       },
-      topics: ['auctions', 'debt', 'savings-bonds'],
+      topics: ['auctions', 'debt'],
       relatedDatasets: ['015-BFS-2014Q1-14', '015-BFS-2014Q3-045'],
       slug: '/upcoming_auctions/',
       currentDateButton: 'byDay',
@@ -36,7 +36,7 @@ module.exports = {
           'The FRN Daily Indexes dataset provides data on Floating Rate Notes. FRNs are relatively short-term investments which mature in two years, pay interest four times each year, and have an interest rate that may change or "float" over time. A person is able to hold an FRN until it matures or sell it before it matures. The FRN Daily Indexes provide information for specific CUSIPs, accrual periods, daily indexes, daily interest accrual rates, spread, and interest payment periods.',
         keywords: 'Debt, Savings Bonds, Auctions',
       },
-      topics: ['auctions', 'debt', 'savings-bonds'],
+      topics: ['auctions', 'debt'],
       relatedDatasets: ['015-BFS-2014Q1-14', '015-BFS-2014Q3-045', '015-BFS-2014Q3-048'],
       currentDateButton: 'byDay',
       hideColumns: [
