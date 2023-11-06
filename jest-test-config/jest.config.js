73e1a6aabdf0a4a243d07bca134a55296cfda6ac
chasls2 <110061741+chasls2@users.noreply.github.com>
2023-11-06 13:12:37 -0600

    up

--- jest-test-config/jest.config.js
+++ jest-test-config/jest.config.js
@@ -8,10 +8,15 @@ module.exports = {
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': `<rootDir>/jest-test-config/identity-obj-proxy-esm.js`,
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
+    '^gatsby-core-utils/(.*)$': `gatsby-core-utils/dist/$1`,
+    '^gatsby-plugin-utils/(.*)$': [
+      `gatsby-plugin-utils/dist/$1`,
+      `gatsby-plugin-utils/$1`,
+    ],
   },
   testMatch: ['<rootDir>/**/__tests__/*.{js,jsx}', '<rootDir>/**/**.spec.{js,jsx}'],
   testPathIgnorePatterns: [`node_modules`, `.cache`, `public`],
-  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  transformIgnorePatterns: [`node_modules/(?!(gatsby|gatsby-script|gatsby-link)/)`],
   globals: {
     __PATH_PREFIX__: ``,
   },
