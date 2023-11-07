17e53cd43cd8bb1dfc3b4bb2e4d01cd8873a6137
chasls2 <110061741+chasls2@users.noreply.github.com>
2023-11-06 20:12:39 -0600

    update

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,7 @@ module.exports = {
     '.+\\.(css|styl|less|sass|scss)$': `<rootDir>/jest-test-config/identity-obj-proxy-esm.js`,
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
     '^gatsby-core-utils/(.*)$': `gatsby-core-utils/dist/$1`,
+    '^gatsby-page-utils/(.*)$': `gatsby-page-utils/dist/$1`,
     '^gatsby-plugin-utils/(.*)$': [
       `gatsby-plugin-utils/dist/$1`,
       `gatsby-plugin-utils/$1`,
@@ -17,7 +18,7 @@ module.exports = {
   },
   testMatch: ['<rootDir>/**/**.spec.{js,jsx,ts,tsx}'],
   testPathIgnorePatterns: [`node_modules`, `.cache`, `public`],
-  transformIgnorePatterns: [`node_modules/(?!(gatsby|gatsby-script|gatsby-link)/)`],
+  transformIgnorePatterns: [`node_modules/(?!(gatsby|gatsby-script|gatsby-link|jest-fetch-mock|jest-dom)/)`],
   globals: {
     __PATH_PREFIX__: ``,
     'ts-jest': {
