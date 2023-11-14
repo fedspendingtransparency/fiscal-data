05e118f62ea189741cd64e53ece4053c0cf67f36
jhertzler <jordan.hertzler@kc.frb.org>
2023-11-14 15:49:42 -0600

    FDG-7891 hover style

--- src/components/dataset-detail-nav/dataset-detail-nav.jsx
+++ src/components/dataset-detail-nav/dataset-detail-nav.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import { container, menu, mobile, activeMenu, disabledLink, desktopLinks, content } from './dataset-detail-nav.module.scss';
+import React, { useState } from 'react';
+import { container, menu, activeMenu, desktopLinks, content, hoverMenu, linkContainer } from './dataset-detail-nav.module.scss';
 import { Link } from 'react-scroll';
 import { updateAddressPath } from '../../helpers/address-bar/address-bar';
 import globalConstants from '../../helpers/constants';
-
 const scrollDelay = globalConstants.config.smooth_scroll.delay;
 const scrollDuration = globalConstants.config.smooth_scroll.duration;
 
 const DDNav = () => {
+  const [hover, setHover] = useState(null);
   const linksArr = [
     {
       title: 'Introduction',
-      href: 'introduction',
+      id: 'introduction',
     },
     {
       title: 'About This Dataset',
-      href: 'about-this-dataset',
+      id: 'about-this-dataset',
     },
     {
       title: 'Preview & Download',
-      href: 'preview-and-download',
+      id: 'preview-and-download',
     },
     {
       title: 'API Quick Guide',
-      href: 'api-quick-guide',
+      id: 'api-quick-guide',
     },
     {
       title: 'Related Datasets',
-      href: 'related-datasets',
+      id: 'related-datasets',
     },
   ];
 
@@ -39,6 +39,7 @@ const DDNav = () => {
 
     if (id) {
       updateAddressPath(id, window.location);
+      setHover(null);
     }
   };
 
@@ -48,23 +49,33 @@ const DDNav = () => {
         <div data-testid="DDNavMenu" className={menu}>
           {linksArr.map((d, i) => {
             return (
+              // <div
+              //   role="button"
+              //   tabIndex={-1}
+              //   onMouseEnter={() => setHover(d.id)}
+              //   onMouseLeave={() => setHover(null)}
+              //   className={`${linkContainer} ${hover === d.id ? hoverMenu : ''}`}
+              // >
               <Link
-                className={desktopLinks}
+                className={`${desktopLinks} ${hover === d.id ? hoverMenu : ''}`}
                 key={`DDNavDesktopLink${i}`}
                 data-testid={`DDNavDesktopLink${i}`}
                 aria-label={`Jump to ${d.title} section`}
-                to={d.href}
+                to={d.id}
                 activeClass={activeMenu}
                 smooth
                 spy
                 duration={scrollDuration}
                 delay={scrollDelay}
-                onClick={() => handleInteraction(null, d.href)}
+                onClick={() => handleInteraction(null, d.id)}
                 tabIndex={0}
                 offset={-36}
+                onMouseEnter={() => setHover(d.id)}
+                onMouseLeave={() => setHover(null)}
               >
                 {d.title}
               </Link>
+              // </div>
             );
           })}
         </div>
