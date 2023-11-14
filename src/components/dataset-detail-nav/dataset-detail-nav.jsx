e109f8ec384eac28bd7b96115b6f57a6945f5207
jhertzler <jordan.hertzler@kc.frb.org>
2023-11-14 11:57:22 -0600

    FDG-7891 Update to react-scroll Link

--- src/components/dataset-detail-nav/dataset-detail-nav.jsx
+++ src/components/dataset-detail-nav/dataset-detail-nav.jsx
@@ -1,15 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { SmoothScroll } from '../smooth-scroll/smooth-scroll';
-import * as styles from './dataset-detail-nav.module.scss';
+import { container, mobileIcon, menu, mobile, disabledLink, desktopLinks, content } from './dataset-detail-nav.module.scss';
 import { select, selectAll } from 'd3-selection';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import * as Scroll from 'react-scroll';
+import { Link } from 'react-scroll';
+import { updateAddressPath } from '../../helpers/address-bar/address-bar';
+import globalConstants from '../../helpers/constants';
+import { sectionLink } from '../secondary-nav/secondary-nav.module.scss';
 
 const d3 = { select, selectAll };
 
 const breakpoint = {
   desktop: 992,
   tablet: 600,
+};
+const scrollDelay = globalConstants.config.smooth_scroll.delay;
+const scrollDuration = globalConstants.config.smooth_scroll.duration;
+
+const scrollOptions = {
+  smooth: false,
+  spy: false,
+  duration: 0,
+  delay: 0,
 };
 
 const DDNav = ({ title }) => {
@@ -18,24 +32,28 @@ const DDNav = ({ title }) => {
   const [scrollInstance] = useState(new SmoothScroll());
   const [mobileIdx, setMobileIdx] = useState(0);
   const [offsetHeight, setOffsetHeight] = useState(0);
+  const [scrollToId, setScrollToId] = useState(null);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [lastScrollPosition, setLastScrollPosition] = useState(0);
+
   let debounce, previousWidth;
 
   const linksArr = [
     {
       title: 'About This Dataset',
-      href: '#about-this-dataset',
+      href: 'about-this-dataset',
     },
     {
       title: 'Preview & Download',
-      href: '#preview-and-download',
+      href: 'preview-and-download',
     },
     {
       title: 'API Quick Guide',
-      href: '#api-quick-guide',
+      href: 'api-quick-guide',
     },
     {
       title: 'Related Datasets',
-      href: '#related-datasets',
+      href: 'related-datasets',
     },
   ];
 
@@ -68,14 +86,41 @@ const DDNav = ({ title }) => {
     setMobileLinks(newIdx);
   };
 
+  const handleInteraction = (e, id) => {
+    // only proceed on mouse click or Enter key press
+    // if (e?.key && e.key !== 'Enter') {
+    //   return;
+    // }
+
+    if (id) {
+      // navigate to page section
+      // setTocIsOpen(false);
+      updateAddressPath(id, window.location);
+      setScrollToId(id);
+    }
+    setLastScrollPosition(scrollPosition);
+  };
+
   useEffect(() => {
-    d3.select('.' + styles.menu)
-      .selectAll('a')
-      .each(function() {
-        const el = d3.select(this).node();
-        scrollInstance.attachClickHandlers(el, offsetHeight);
-      });
-  }, [links]);
+    const handleSelectLink = e => {
+      const { target, key, className } = e.target;
+      if (target && key === 'Enter' && className.includes(sectionLink)) {
+        target.click();
+      }
+    };
+
+    const handleScroll = () => {
+      setScrollPosition(window.pageYOffset);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('keyup', handleSelectLink);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('keyup', handleSelectLink);
+    };
+  }, []);
 
   useEffect(() => {
     if (isMobile) {
@@ -93,53 +138,54 @@ const DDNav = ({ title }) => {
     }
   };
 
-  useEffect(() => {
-    const header = d3.select(`#${styles.container}`);
-    let headerHeight = 0;
-
-    if (!header.empty()) {
-      headerHeight = header.node().getBoundingClientRect().height;
-    }
-    setOffsetHeight(headerHeight, 10);
-
-    window.addEventListener('resize', function() {
-      if (previousWidth === window.innerWidth) {
-        return;
-      }
-
-      if (debounce) {
-        clearTimeout(debounce);
-      }
-
-      previousWidth = window.innerWidth;
-
-      debounce = setTimeout(responsiveLinks, 50);
-    });
-  }, []);
+  // useEffect(() => {
+  //   const header = d3.select(`#${container}`);
+  //   let headerHeight = 0;
+  //
+  //   if (!header.empty()) {
+  //     headerHeight = header.node().getBoundingClientRect().height;
+  //   }
+  //   setOffsetHeight(headerHeight, 10);
+  //
+  //   window.addEventListener('resize', function() {
+  //     if (previousWidth === window.innerWidth) {
+  //       return;
+  //     }
+  //
+  //     if (debounce) {
+  //       clearTimeout(debounce);
+  //     }
+  //
+  //     previousWidth = window.innerWidth;
+  //
+  //     debounce = setTimeout(responsiveLinks, 50);
+  //   });
+  // }, []);
 
   useEffect(() => {
     responsiveLinks();
   });
 
   return (
-    <section id={styles.container}>
-      <div className={styles.content}>
-        <div data-testid="DDNavTitle" className={styles.title} title={title}>
-          {title}
-        </div>
-        <div data-testid="DDNavMenu" className={`${styles.menu} ${isMobile ? styles.mobile : ''}`}>
+    <section id={container}>
+      <div className={content}>
+        <div data-testid="DDNavMenu" className={`${menu} ${isMobile ? mobile : ''}`}>
           {!isMobile
             ? links.map((d, i) => {
                 return (
-                  <a
-                    className={styles.desktopLinks}
+                  <Link
+                    className={desktopLinks}
                     key={`DDNavDesktopLink${i}`}
                     data-testid={`DDNavDesktopLink${i}`}
                     aria-label={`Jump to ${d.title} section`}
-                    href={d.href}
+                    to={d.href}
+                    smooth
+                    spy
+                    onClick={() => handleInteraction(null, d.href)}
+                    tabIndex={0}
                   >
                     {d.title}
-                  </a>
+                  </Link>
                 );
               })
             : links.map((d, i) => {
@@ -147,7 +193,7 @@ const DDNav = ({ title }) => {
                   <FontAwesomeIcon
                     key={`DDDisabledNavMobileLink${i}`}
                     icon={i === 1 ? faArrowDown : faArrowUp}
-                    className={`${styles.mobileIcon} ${styles.disabledLink}`}
+                    className={`${mobileIcon} ${disabledLink}`}
                   />
                 ) : (
                   <a
@@ -160,7 +206,7 @@ const DDNav = ({ title }) => {
                     }}
                     href={d.href}
                   >
-                    <FontAwesomeIcon className={styles.mobileIcon} icon={i === 1 ? faArrowDown : faArrowUp} />
+                    <FontAwesomeIcon className={mobileIcon} icon={i === 1 ? faArrowDown : faArrowUp} />
                   </a>
                 );
               })}
