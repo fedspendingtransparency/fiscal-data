a8d38e89bd33e38c3f87836dff438300623d5428
Hallie Martin <hallie.martin@kc.frb.org>
2023-12-11 21:06:14 -0600

    FDG-6478 add api mocks to test

--- src/helpers/hook-helpers/useBeaGDP-helper.spec.js
+++ src/helpers/hook-helpers/useBeaGDP-helper.spec.js
@@ -47,33 +47,51 @@ const mockDebtOutstandingNotCurrentData = {
   ],
 };
 
+const mts5 = apiPrefix + '/v1/accounting/mts/mts_table_5?sort=-record_date';
+const mts4 = apiPrefix + '/v1/accounting/mts/mts_table_4?sort=-record_date';
+const debtOutstanding = apiPrefix + 'v2/accounting/od/debt_outstanding?sort=-record_date';
+
+fetchMock.get(`begin:${apiPrefix}${mts4}`, mockMtsSeptemberData, { overwriteRoutes: true }, { repeat: 1 });
+
 describe('isOtherDataUpdated', () => {
   it('will return true when debtOutstanding flagged and in with 2020 data as the most recent', () => {
+    fetchMock.get(`begin:${apiPrefix}${debtOutstanding}`, mockDebtOutstandingCurrentData, { overwriteRoutes: true }, { repeat: 1 });
+
     const result = isOtherDataUpdated('debtOutstanding', 2020);
     expect(result).toEqual(true);
   });
 
   it('will return false when debtOutstanding flagged and in with 2019 data as the most recent', () => {
+    fetchMock.get(`begin:${apiPrefix}${debtOutstanding}`, mockDebtOutstandingNotCurrentData, { overwriteRoutes: true }, { repeat: 1 });
+
     const result = isOtherDataUpdated('debtOutstanding', 2020);
     expect(result).toEqual(false);
   });
 
   it('will return true when mts5 flagged and in with September 2020 data as the most recent', () => {
+    fetchMock.get(`begin:${apiPrefix}${mts5}`, mockMtsSeptemberData, { overwriteRoutes: true }, { repeat: 1 });
+
     const result = isOtherDataUpdated('mts5', 2020);
     expect(result).toEqual(true);
   });
 
   it('will return false when mts5 flagged and in with August 2019 data as the most recent', () => {
+    fetchMock.get(`begin:${apiPrefix}${mts5}`, mockMtsAugustData, { overwriteRoutes: true }, { repeat: 1 });
+
     const result = isOtherDataUpdated('mts5', 2020);
     expect(result).toEqual(false);
   });
 
   it('will return true when mts4 flagged and in with September 2020 data as the most recent', () => {
+    fetchMock.get(`begin:${apiPrefix}${mts5}`, mockMtsSeptemberData, { overwriteRoutes: true }, { repeat: 1 });
+
     const result = isOtherDataUpdated('mts4', 2020);
     expect(result).toEqual(true);
   });
 
   it('will return false when mts4 flagged and in with August 2019 data as the most recent', () => {
+    fetchMock.get(`begin:${apiPrefix}${mts5}`, mockMtsAugustData, { overwriteRoutes: true }, { repeat: 1 });
+
     const result = isOtherDataUpdated('mts4', 2020);
     expect(result).toEqual(false);
   });
--- src/helpers/hook-helpers/useBeaGDP-helper.ts
+++ src/helpers/hook-helpers/useBeaGDP-helper.ts
@@ -1,4 +1,12 @@
+import { apiPrefix, basicFetch } from '../../utils/api-utils';
+
+// data[0].fiscal_data_year or whatever
+const mts5 = apiPrefix + '/v1/accounting/mts/mts_table_5?sort=-record_date';
+const mts4 = apiPrefix + '/v1/accounting/mts/mts_table_4?sort=-record_date';
+const debtOutstanding = apiPrefix + 'v2/accounting/od/debt_outstanding?sort=-record_date';
+
 export const isOtherDataUpdated = (otherDataToCheck: string, currentYear: number) => {
+  const currentYearString = currentYear.toString();
   let isOtherDataCurrent = false;
   return isOtherDataCurrent;
 };
