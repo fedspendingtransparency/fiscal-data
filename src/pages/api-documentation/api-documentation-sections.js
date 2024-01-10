37b52ecef1a49c6eed63edbb81fa3c4b757d9a41
Wyatt Chandler <wyatt.chandler@kc.frb.org>
2024-01-10 17:19:58 -0600

    FDG-8450 refactored api doc sections

---
+++ src/pages/api-documentation/api-documentation-sections.js
@@ -0,0 +1,188 @@
+import { headingLevel2, headingLevel3, headingLevel4 } from './api.module.scss';
+export const tocList = [
+  {
+    id: 'getting-started',
+    headingLevel: headingLevel2,
+    title: 'Getting Started',
+  },
+  {
+    id: 'what-is-an-api',
+    headingLevel: headingLevel3,
+    title: 'What is an API?',
+  },
+  {
+    id: 'what-is-a-dataset',
+    headingLevel: headingLevel3,
+    title: 'What is a dataset?',
+  },
+  {
+    id: 'api-endpoint-url-structure',
+    headingLevel: headingLevel3,
+    title: 'API Endpoint URL structure',
+  },
+  {
+    id: 'how-to-access-our-api',
+    headingLevel: headingLevel3,
+    title: 'How to Access our API',
+  },
+  {
+    id: 'license-and-authorization',
+    headingLevel: headingLevel3,
+    title: 'License & Authorization',
+  },
+  {
+    id: 'change-log',
+    headingLevel: headingLevel3,
+    title: 'Change Log',
+  },
+  {
+    id: 'endpoints',
+    headingLevel: headingLevel2,
+    title: 'Endpoints',
+  },
+  {
+    id: 'list-of-endpoints',
+    headingLevel: headingLevel3,
+    title: 'List of Endpoints',
+  },
+  {
+    id: 'fields-by-endpoint',
+    headingLevel: headingLevel3,
+    title: 'Fields by Endpoint',
+  },
+  {
+    id: 'data-registry',
+    headingLevel: headingLevel2,
+    title: 'Fiscal Service Data Registry',
+  },
+  {
+    id: 'methods',
+    headingLevel: headingLevel2,
+    title: 'Methods',
+  },
+  {
+    id: 'parameters',
+    headingLevel: headingLevel2,
+    title: 'Parameters',
+  },
+  {
+    id: 'fields',
+    headingLevel: headingLevel3,
+    title: 'Fields',
+  },
+  {
+    id: 'data-types',
+    headingLevel: headingLevel4,
+    title: 'Data Types',
+  },
+  {
+    id: 'fields-fields-by-endpoint',
+    headingLevel: headingLevel4,
+    title: 'Fields by Endpoint',
+  },
+  {
+    id: 'filters',
+    headingLevel: headingLevel3,
+    title: 'Filters',
+  },
+  {
+    id: 'parameters-sorting',
+    headingLevel: headingLevel3,
+    title: 'Sorting',
+  },
+  {
+    id: 'parameters-format',
+    headingLevel: headingLevel3,
+    title: 'Format',
+  },
+  {
+    id: 'parameters-pagination',
+    headingLevel: headingLevel3,
+    title: 'Pagination',
+  },
+  {
+    id: 'responses-response-objects',
+    headingLevel: headingLevel2,
+    title: 'Responses & Response Objects',
+  },
+  {
+    id: 'responses-response-codes',
+    headingLevel: headingLevel3,
+    title: 'Response Codes',
+  },
+  {
+    id: 'responses-meta-object',
+    headingLevel: headingLevel3,
+    title: 'Meta Object',
+  },
+  {
+    id: 'responses-links-object',
+    headingLevel: headingLevel3,
+    title: 'Links Object',
+  },
+  {
+    id: 'responses-data-object',
+    headingLevel: headingLevel3,
+    title: 'Data Object',
+  },
+  {
+    id: 'responses-error-object',
+    headingLevel: headingLevel3,
+    title: 'Error Object',
+  },
+  {
+    id: 'responses-pagination-header',
+    headingLevel: headingLevel3,
+    title: 'Pagination Header',
+  },
+  {
+    id: 'aggregation-sums',
+    headingLevel: headingLevel2,
+    title: 'Aggregation & Sums',
+  },
+  {
+    id: 'examples-code-snippets',
+    headingLevel: headingLevel2,
+    title: 'Examples and Code Snippets',
+  },
+  {
+    id: 'examples-fields',
+    headingLevel: headingLevel3,
+    title: 'Fields',
+  },
+  {
+    id: 'examples-filters',
+    headingLevel: headingLevel3,
+    title: 'Filters',
+  },
+  {
+    id: 'examples-sorting',
+    headingLevel: headingLevel3,
+    title: 'Sorting',
+  },
+  {
+    id: 'examples-format',
+    headingLevel: headingLevel3,
+    title: 'Format',
+  },
+  {
+    id: 'examples-pagination',
+    headingLevel: headingLevel3,
+    title: 'Pagination',
+  },
+  {
+    id: 'examples-aggregation',
+    headingLevel: headingLevel3,
+    title: 'Aggregation',
+  },
+  {
+    id: 'examples-pivoting',
+    headingLevel: headingLevel3,
+    title: 'Pivoting',
+  },
+  {
+    id: 'examples-multi-dimension-datasets',
+    headingLevel: headingLevel3,
+    title: 'Multi-dimension Datasets',
+  },
+];
--- src/pages/api-documentation/index.jsx
+++ src/pages/api-documentation/index.jsx
@@ -15,200 +15,14 @@ import Aggregation from '../../components/api-documentation/aggregation/aggregat
 import Examples from '../../components/api-documentation/examples/examples';
 import TOCButton from '../../components/table-of-contents/toc-button/toc-button';
 import { tocCont, tocHeader, tocWrapper } from '../../components/table-of-contents/toc.module.scss';
-import { toc, content, headingLevel2, headingLevel3, headingLevel4, link, activeLink, tocOpen, tocClosed, apiPageWrapper } from './api.module.scss';
+import { toc, content, link, activeLink, tocOpen, tocClosed, apiPageWrapper } from './api.module.scss';
 import DataRegistry from '../../components/api-documentation/data-registry/data-registry';
 import { updateAddressPath } from '../../helpers/address-bar/address-bar';
 import { scrollOptionsSmooth } from '../../utils/scroll-config';
+import { SecondaryNav } from '../../components/secondary-nav/secondary-nav';
+import { tocList } from './api-documentation-sections';
 
 const ApiDocumentationPage = ({ location }) => {
-  const tocList = [
-    {
-      id: 'getting-started',
-      headingLevel: headingLevel2,
-      title: 'Getting Started',
-    },
-    {
-      id: 'what-is-an-api',
-      headingLevel: headingLevel3,
-      title: 'What is an API?',
-    },
-    {
-      id: 'what-is-a-dataset',
-      headingLevel: headingLevel3,
-      title: 'What is a dataset?',
-    },
-    {
-      id: 'api-endpoint-url-structure',
-      headingLevel: headingLevel3,
-      title: 'API Endpoint URL structure',
-    },
-    {
-      id: 'how-to-access-our-api',
-      headingLevel: headingLevel3,
-      title: 'How to Access our API',
-    },
-    {
-      id: 'license-and-authorization',
-      headingLevel: headingLevel3,
-      title: 'License & Authorization',
-    },
-    {
-      id: 'change-log',
-      headingLevel: headingLevel3,
-      title: 'Change Log',
-    },
-    {
-      id: 'endpoints',
-      headingLevel: headingLevel2,
-      title: 'Endpoints',
-    },
-    {
-      id: 'list-of-endpoints',
-      headingLevel: headingLevel3,
-      title: 'List of Endpoints',
-    },
-    {
-      id: 'fields-by-endpoint',
-      headingLevel: headingLevel3,
-      title: 'Fields by Endpoint',
-    },
-    {
-      id: 'data-registry',
-      headingLevel: headingLevel2,
-      title: 'Fiscal Service Data Registry',
-    },
-    {
-      id: 'methods',
-      headingLevel: headingLevel2,
-      title: 'Methods',
-    },
-    {
-      id: 'parameters',
-      headingLevel: headingLevel2,
-      title: 'Parameters',
-    },
-    {
-      id: 'fields',
-      headingLevel: headingLevel3,
-      title: 'Fields',
-    },
-    {
-      id: 'data-types',
-      headingLevel: headingLevel4,
-      title: 'Data Types',
-    },
-    {
-      id: 'fields-fields-by-endpoint',
-      headingLevel: headingLevel4,
-      title: 'Fields by Endpoint',
-    },
-    {
-      id: 'filters',
-      headingLevel: headingLevel3,
-      title: 'Filters',
-    },
-    {
-      id: 'parameters-sorting',
-      headingLevel: headingLevel3,
-      title: 'Sorting',
-    },
-    {
-      id: 'parameters-format',
-      headingLevel: headingLevel3,
-      title: 'Format',
-    },
-    {
-      id: 'parameters-pagination',
-      headingLevel: headingLevel3,
-      title: 'Pagination',
-    },
-    {
-      id: 'responses-response-objects',
-      headingLevel: headingLevel2,
-      title: 'Responses & Response Objects',
-    },
-    {
-      id: 'responses-response-codes',
-      headingLevel: headingLevel3,
-      title: 'Response Codes',
-    },
-    {
-      id: 'responses-meta-object',
-      headingLevel: headingLevel3,
-      title: 'Meta Object',
-    },
-    {
-      id: 'responses-links-object',
-      headingLevel: headingLevel3,
-      title: 'Links Object',
-    },
-    {
-      id: 'responses-data-object',
-      headingLevel: headingLevel3,
-      title: 'Data Object',
-    },
-    {
-      id: 'responses-error-object',
-      headingLevel: headingLevel3,
-      title: 'Error Object',
-    },
-    {
-      id: 'responses-pagination-header',
-      headingLevel: headingLevel3,
-      title: 'Pagination Header',
-    },
-    {
-      id: 'aggregation-sums',
-      headingLevel: headingLevel2,
-      title: 'Aggregation & Sums',
-    },
-    {
-      id: 'examples-code-snippets',
-      headingLevel: headingLevel2,
-      title: 'Examples and Code Snippets',
-    },
-    {
-      id: 'examples-fields',
-      headingLevel: headingLevel3,
-      title: 'Fields',
-    },
-    {
-      id: 'examples-filters',
-      headingLevel: headingLevel3,
-      title: 'Filters',
-    },
-    {
-      id: 'examples-sorting',
-      headingLevel: headingLevel3,
-      title: 'Sorting',
-    },
-    {
-      id: 'examples-format',
-      headingLevel: headingLevel3,
-      title: 'Format',
-    },
-    {
-      id: 'examples-pagination',
-      headingLevel: headingLevel3,
-      title: 'Pagination',
-    },
-    {
-      id: 'examples-aggregation',
-      headingLevel: headingLevel3,
-      title: 'Aggregation',
-    },
-    {
-      id: 'examples-pivoting',
-      headingLevel: headingLevel3,
-      title: 'Pivoting',
-    },
-    {
-      id: 'examples-multi-dimension-datasets',
-      headingLevel: headingLevel3,
-      title: 'Multi-dimension Datasets',
-    },
-  ];
-
   const breadCrumbLinks = [
     {
       name: 'API Documentation',
@@ -319,6 +133,7 @@ const ApiDocumentationPage = ({ location }) => {
                 </div>
               );
             })}
+            {/*<SecondaryNav sections={tocList} activeClass={activeLink} />*/}
           </div>
         </div>
         <div id={content} className={toggleStyles}>
