c81d4e976aec268b58a80bbc28ac8e549d3cb589
jhertzler <jordan.hertzler@kc.frb.org>
2024-01-30 11:55:27 -0600

    FDG-8152 Invalid date range

--- src/components/data-table/data-table-header/date-range-filter/date-input.jsx
+++ src/components/data-table/data-table-header/date-range-filter/date-input.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { inputAddOn, inputValidation } from './date-range-helper';
-import { format, isValid } from 'date-fns';
+import { format, isValid, isAfter } from 'date-fns';
 import moment from 'moment';
 
 const DateRangeTextInput = ({ setStartDate, setEndDate, selected, setSelected, setText, text, setInvalidDate, invalidDate }) => {
@@ -13,6 +13,7 @@ const DateRangeTextInput = ({ setStartDate, setEndDate, selected, setSelected, s
 
     if (length <= 23 && inputValidation[length]?.test(input)) {
       if (length === 10 && length > prevLength) {
+        //Validate the first date
         const from = new Date(input);
         if (isValid(from)) {
           setStartDate(moment(from));
@@ -20,12 +21,16 @@ const DateRangeTextInput = ({ setStartDate, setEndDate, selected, setSelected, s
         } else {
           setInvalidDate(true);
         }
-      } else if ((length === 9 || length === 22) && prevLength > length) {
+      } else if ((length < 10 || (length < 23 && length > 13)) && prevLength > length) {
+        // reset invalid date flag when corrections have been made
         setInvalidDate(false);
       } else if (length === 23) {
-        const from = new Date(input.substring(0, 10));
-        const to = new Date(input.substring(13));
-        if (isValid(to)) {
+        //Validate the second date
+        const fromInput = input.substring(0, 10);
+        const toInput = input.substring(13);
+        const from = new Date(fromInput);
+        const to = new Date(toInput);
+        if (isValid(to) && (isAfter(to, from) || toInput === fromInput)) {
           setEndDate(moment(to));
           setSelected({ from: from, to: to });
         } else {
@@ -54,6 +59,7 @@ const DateRangeTextInput = ({ setStartDate, setEndDate, selected, setSelected, s
   useEffect(() => {
     if (text.length === 0) {
       setInputText('');
+      setInvalidDate(false);
     }
   }, [text]);
 
