bdebe400c70225472e462e74f3678103ac18ab27
jhertzler <jordan.hertzler@kc.frb.org>
2024-02-05 10:08:47 -0600

    FDG-8540 delete unused import

--- src/components/dataset-data/chart-table-toggle/chart-table-toggle.jsx
+++ src/components/dataset-data/chart-table-toggle/chart-table-toggle.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
