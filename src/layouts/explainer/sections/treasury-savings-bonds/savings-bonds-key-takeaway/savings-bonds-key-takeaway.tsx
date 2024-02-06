10745497d76afe60feaf678be191bcf33eeecf97
chasls2 <110061741+chasls2@users.noreply.github.com>
2024-02-06 16:50:44 -0600

    updated FC import

--- src/layouts/explainer/sections/treasury-savings-bonds/savings-bonds-key-takeaway/savings-bonds-key-takeaway.tsx
+++ src/layouts/explainer/sections/treasury-savings-bonds/savings-bonds-key-takeaway/savings-bonds-key-takeaway.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import KeyTakeawaysSection from '../../../explainer-components/key-takeaways/key-takeaways-section';
 
 import { faHandshakeAlt, faHourglassHalf, faLineChart } from '@fortawesome/free-solid-svg-icons';
 import { treasurySavingsBondsExplainerSecondary, treasurySavingsBondsExplainerLightSecondary } from '../treasury-savings-bonds.module.scss';
 
-const SavingBondsKeyTakeaway: React.FC = () => {
+const SavingBondsKeyTakeaway: FunctionComponent = () => {
 
   const savingBondsKeyTakeaways = [
     {
