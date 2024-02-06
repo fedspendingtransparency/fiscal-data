7751b5eeadf3e924b9c82984632d0d839e149024
Wyatt Chandler <wyatt.chandler@kc.frb.org>
2024-02-06 12:31:11 -0600

    FDG-8633 added millions to getShortForm test

--- src/utils/rounding-utils.spec.js
+++ src/utils/rounding-utils.spec.js
@@ -4,11 +4,14 @@ describe('Rounding Utils', () => {
   it('getShortForm', () => {
     const valueBillions = '100000000000';
     const valueTrillions = '1000000000000';
+    const valueMillions = '100000000';
     const fractionDigitsBillion = 0;
+    const fractionDigitsMillion = 0;
     const fractionDigitsTrillion = 2;
     expect(getShortForm(valueBillions, false, false, fractionDigitsBillion)).toEqual('100 billion');
     expect(getShortForm(valueTrillions, false, false, fractionDigitsTrillion)).toEqual('1.00 trillion');
     expect(getShortForm(valueTrillions, true, false, fractionDigitsTrillion)).toEqual('1.00 T');
+    expect(getShortForm(valueMillions, false, false, fractionDigitsMillion)).toEqual('100 million');
   });
 
   it('getShortForm rounding', () => {
