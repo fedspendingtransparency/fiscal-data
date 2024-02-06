c306f5b8e26098e71388867c0537f01d12e89eb5
chasls2 <110061741+chasls2@users.noreply.github.com>
2024-02-06 16:45:39 -0600

    FDG-5878 Key Takeaways Section

---
+++ src/layouts/explainer/sections/treasury-savings-bonds/savings-bonds-key-takeaway/savings-bonds-key-takeaway.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import KeyTakeawaysSection from '../../../explainer-components/key-takeaways/key-takeaways-section';
+
+import { faHandshakeAlt, faHourglassHalf, faLineChart } from '@fortawesome/free-solid-svg-icons';
+import { treasurySavingsBondsExplainerSecondary, treasurySavingsBondsExplainerLightSecondary } from '../treasury-savings-bonds.module.scss';
+
+const SavingBondsKeyTakeaway: React.FC = () => {
+
+  const savingBondsKeyTakeaways = [
+    {
+      text: `Savings bonds are simple, safe, and affordable loans to the federal government 
+      that can be purchased by individuals and investors. These loans help the government 
+      finance its operations and offer benefits to the purchaser. `,
+      icon: faHandshakeAlt,
+    },
+    {
+      text: `Savings bond sales have varied over time. Occasionally, the government develops 
+      special campaigns to help fund specific financial activities such as U.S. participation 
+      in World War II. At other times, sales have increased or decreased alongside changes in 
+      interest rates or inflation. `,
+      icon: faLineChart,
+    },
+    {
+      text: ` Savings bonds earn interest until they reach "maturity," which is generally 
+      20-30 years, depending on the type purchased. If a bond is held past its maturity, 
+      the federal government remains responsible for this debt. However, savings bonds 
+      that are held past their maturity date and no longer earn interest may lose value 
+      due to inflation.`,
+      icon: faHourglassHalf,
+    },
+  ];
+
+  return (
+    <KeyTakeawaysSection takeaways={savingBondsKeyTakeaways} primaryColor={treasurySavingsBondsExplainerSecondary} secondaryColor={treasurySavingsBondsExplainerLightSecondary} />
+  );
+};
+
+export default SavingBondsKeyTakeaway;
