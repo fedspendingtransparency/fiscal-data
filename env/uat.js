5fe34ed3d2aaa475ef5bf9103523176867826fa3
Wyatt Chandler <wyatt.chandler@kc.frb.org>
2024-02-13 14:18:32 -0600

    FDG-8005 changed field name

--- env/uat.js
+++ env/uat.js
@@ -43,7 +43,7 @@ module.exports = {
       topics: ['financial-summaries'],
       relatedDatasets: ['015-BFS-2014Q3-098'],
       currentDateButton: 'byMonth',
-      customPublishedReportsFilter: 'account_name',
+      customPublishedReportsFilter: 'account_number_tas',
     },
   },
   ADDITIONAL_ENDPOINTS: {
