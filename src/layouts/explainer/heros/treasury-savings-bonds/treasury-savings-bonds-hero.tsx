3a29239aa27346ebc25b58dc2bddff6ad171b108
Wyatt Chandler <wyatt.chandler@kc.frb.org>
2024-02-14 16:51:31 -0600

    FDG-8576 started plugging in data

--- src/layouts/explainer/heros/treasury-savings-bonds/treasury-savings-bonds-hero.tsx
+++ src/layouts/explainer/heros/treasury-savings-bonds/treasury-savings-bonds-hero.tsx
@@ -1,22 +1,49 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useState, useEffect } from 'react';
 import CustomLink from '../../../../components/links/custom-link/custom-link';
 import { footNotes, heroImageSubHeading, footNotesPillData, flapWrapper } from '../../hero-image/hero-image.module.scss';
 import { getFootNotesDateRange, getPillData } from '../hero-helper';
 import SplitFlapDisplay from '../../../../components/split-flap-display/split-flap-display';
+import { apiPrefix, basicFetch } from '../../../../utils/api-utils';
+import { getShortForm } from '../../../../utils/rounding-utils';
 
 const TreasurySavingsBondsHero = (): ReactElement => {
   // appending 40 to a 6 digit hex color is equivalent to specifying 25% opacity
   const pillColorWithTransparency = `#e2bee440`;
 
-  const [totalSavingsBondsInvested, setTotalSavingsBondsInvested] = useState('323000000');
-  const [priorFiscalYear, setPriorFiscalYear] = useState('2023');
-  const [priorCalendarYear, setPriorCalendarYear] = useState('2022');
-  const [recordCalendarMonth, setRecordCalendarMonth] = useState('12');
+  const filter = 'filter=security_type_desc:eq:Savings%20Bond';
+  const sort = 'sort=-record_date';
+  const pagination = 'page[size]=1';
+  const endpointUrl = `v1/accounting/od/securities_sales?${filter}&${sort}&${pagination}`;
+  const securitiesSalesUrl = `${apiPrefix}${endpointUrl}`;
+
+  const [totalSavingsBondsInvested, setTotalSavingsBondsInvested] = useState('');
+  const [priorFiscalYear, setPriorFiscalYear] = useState('');
+  const [priorCalendarYear, setPriorCalendarYear] = useState('');
+  const [recordCalendarMonth, setRecordCalendarMonth] = useState('');
   const [savingsBondChangeLabel, setSavingsBondChangeLabel] = useState('increased');
   const [savingsBondChange, setSavingsBondChange] = useState(100000000);
   const [savingsBondPercentChange, setSavingsBondPercentChange] = useState(30);
 
   const numberFormat = new Intl.NumberFormat('en-US');
+
+  const getHeroData = () => {
+    basicFetch(`${securitiesSalesUrl}`).then(res => {
+      if (res.data) {
+        console.log(res.data[0]);
+        setPriorFiscalYear((parseInt(res.data[0].record_fiscal_year) - 1).toString());
+        setPriorCalendarYear((parseInt(res.data[0].record_calendar_year) - 1).toString());
+        setRecordCalendarMonth(res.data[0].record_calendar_month);
+        const filterCurrentFY = `filter=security_type_desc:eq:Savings%20Bond,record_fiscal_year:eq:${res.data[0].record_fiscal_year}`;
+        const currentFYEndPoint = `v1/accounting/od/securities_sales?${filterCurrentFY}`;
+        const currentFYReqUrl = `${apiPrefix}${currentFYEndPoint}`;
+        basicFetch(`${currentFYReqUrl}`).then(res2 => {
+          if (res2.data) {
+            setTotalSavingsBondsInvested(res2.data[0].net_sales_amt);
+          }
+        });
+      }
+    });
+  };
 
   const electronicSecurities = (
     <CustomLink url="/datasets/electronic-securities-transactions/" id="Electronic Securities Transactions">
@@ -27,11 +54,15 @@ const TreasurySavingsBondsHero = (): ReactElement => {
   const rightTooltip = 'The percentage change in savings bonds investments compared to the same period last year.';
   const leftTooltip = change => `The total amount of investment in savings bonds has ${savingsBondChangeLabel} compared to the same period last year`;
 
+  useEffect(() => {
+    getHeroData();
+  }, []);
+
   return (
     <>
       <p className={heroImageSubHeading}>
-        The American public has invested ${parseInt(totalSavingsBondsInvested) / 1000000} million in savings bonds this fiscal year to finance the
-        federal government's operations.
+        The American public has invested ${getShortForm(totalSavingsBondsInvested, false)} in savings bonds this fiscal year to finance the federal
+        government's operations.
       </p>
       <div className={flapWrapper}>
         <SplitFlapDisplay
