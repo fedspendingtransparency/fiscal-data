34f9db366711f3f953614e9abf137df7dd383d1a
Wyatt Chandler <wyatt.chandler@kc.frb.org>
2024-02-20 14:33:48 -0600

    FDG-8762 building atom

--- src/components/dataset-data/table-section-container/table-section-container.jsx
+++ src/components/dataset-data/table-section-container/table-section-container.jsx
@@ -30,6 +30,7 @@ import {
   detailViewIcon,
 } from './table-section-container.module.scss';
 import SummaryTable from './summary-table/summary-table';
+import { useSetRecoilState } from 'recoil';
 
 const TableSectionContainer = ({
   config,
@@ -76,6 +77,10 @@ const TableSectionContainer = ({
   const [manualPagination, setManualPagination] = useState(false);
   const [apiErrorState, setApiError] = useState(apiError || false);
   const [chartData, setChartData] = useState(null);
+
+  useEffect(() => {
+    console.log(userFilterUnmatchedForDateRange);
+  }, [userFilterUnmatchedForDateRange]);
 
   const getDepaginatedData = async () => {
     const from = formatDateForApi(dateRange.from);
