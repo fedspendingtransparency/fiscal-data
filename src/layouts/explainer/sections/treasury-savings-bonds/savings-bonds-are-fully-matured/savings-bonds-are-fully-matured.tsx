a1a3f91e1233d63f8f5226de6a61bea080104fd7
Nicolle Weidner <nicolle.weidner@kc.frb.org>
2024-02-29 09:23:01 -0600

    FDG-8593 add quote box to savings-bonds-are-fully-matured

--- src/layouts/explainer/sections/treasury-savings-bonds/savings-bonds-are-fully-matured/savings-bonds-are-fully-matured.tsx
+++ src/layouts/explainer/sections/treasury-savings-bonds/savings-bonds-are-fully-matured/savings-bonds-are-fully-matured.tsx
@@ -1,6 +1,23 @@
 import React, { FunctionComponent } from 'react';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons/faMagnifyingGlass';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { fontBodyCopy } from '../../../explainer.module.scss';
+import { treasurySavingsBondsExplainerSecondary } from '../treasury-savings-bonds.module.scss';
+import QuoteBox from '../../../quote-box/quote-box';
+import CustomLink from '../../../../../components/links/custom-link/custom-link';
+import ReduceMaturedUnredeemedDebtAccordion
+  from './reduce-matured-unredeemed-debt-accordion/reduce-matured-unredeemed-debt-accordion';
+import GlossaryPopoverDefinition from '../../../../../components/glossary/glossary-term/glossary-popover-definition';
 
 const SavingsBondsAreFullyMatured: FunctionComponent = () => {
+   const glossaryTerms = {
+     maturedUnredeemedDebt: (
+      <GlossaryPopoverDefinition term="Matured Unredeemed Debt (MUD)" page="Savings Bonds Explainer">
+        Matured Unredeemed Debt (MUD)
+      </GlossaryPopoverDefinition>
+    )
+  };
+
   return (
     <>
       <p>A savings bond can be redeemed any time after at least one year; however, the longer a bond is held
@@ -10,7 +27,7 @@ const SavingsBondsAreFullyMatured: FunctionComponent = () => {
       </p>
       <p>
         Occasionally, bond owners hold onto bonds after they have reached maturity and are no longer earning interest.
-        These outstanding but unredeemed bonds are called Matured Unredeemed Debt (MUD). The government continues to be
+        These outstanding but unredeemed bonds are called {glossaryTerms.maturedUnredeemedDebt}. The government continues to be
         responsible for this debt, as it may be redeemed at any time. Therefore, the Treasury has increased efforts to
         encourage bondholders to redeem their matured savings bonds. As of Month YYYY, there were XXX million
         matured unredeemed savings bonds held by investors.
@@ -19,6 +36,24 @@ const SavingsBondsAreFullyMatured: FunctionComponent = () => {
         If bonds are held past their maturity date, the bonds can lose value due to inflation. To understand how this
         value is lost, see the illustration below.
       </p>
+
+      <div>
+        **************** insert pictures here ****************
+      </div>
+
+      <QuoteBox
+        icon={faMagnifyingGlass as IconProp}
+        primaryColor={fontBodyCopy}
+        secondaryColor={treasurySavingsBondsExplainerSecondary}
+        customTopMargin={'0'}
+      >
+        <p>
+          Could there be a savings bond in your name that you might not know about? Go on a{' '}
+          <CustomLink url="https://treasurydirect.gov/savings-bonds/treasury-hunt/">Treasure Hunt</CustomLink> and see
+          what bonds might be waiting for you to cash in!
+        </p>
+      </QuoteBox>
+      <ReduceMaturedUnredeemedDebtAccordion/>
     </>
   )
 }
