61f02fb0ce0e5ca5da1bf75a82e99099a0fa3365
jhertzler <jordan.hertzler@kc.frb.org>
2024-03-01 11:10:23 -0600

    FDG-8588 Add tests

--- src/layouts/explainer/sections/treasury-savings-bonds/purchase-of-savings-bonds/savings-bonds-sold-by-type-chart/savings-bonds-sold-by-type-chart.spec.js
+++ src/layouts/explainer/sections/treasury-savings-bonds/purchase-of-savings-bonds/savings-bonds-sold-by-type-chart/savings-bonds-sold-by-type-chart.spec.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import SavingsBondsSoldByTypeChart from './savings-bonds-sold-by-type-chart';
-import { fireEvent, render, act } from '@testing-library/react';
+import { fireEvent, render, act, waitFor } from '@testing-library/react';
+import { mockData } from './savings-bonds-sold-by-type-chart-helper';
+import fetchMock from 'fetch-mock';
+import { fyEndpoint } from './savings-bonds-sold-by-type-chart-helper';
 
 describe('Savings Bonds by Type Over Time Chart', () => {
   class ResizeObserver {
@@ -10,19 +13,43 @@ describe('Savings Bonds by Type Over Time Chart', () => {
   }
   window.ResizeObserver = ResizeObserver;
 
+  const mockFYData = { data: [{ record_fiscal_year: 2024, record_date: '2024-12-12' }] };
+
+  beforeEach(() => {
+    fetchMock.get(
+      `https://www.transparency.treasury.gov/services/api/fiscal_service/${fyEndpoint}`,
+      mockFYData,
+      { overwriteRoutes: true },
+      { repeat: 0 }
+    );
+    jest.spyOn(console, 'warn').mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    jest.resetModules();
+  });
+
   it('renders the chart', () => {
-    const { container, getByText } = render(<SavingsBondsSoldByTypeChart />);
+    const { container, getByText } = render(<SavingsBondsSoldByTypeChart chartData={mockData} />);
     expect(container).toBeInTheDocument();
     expect(getByText('Adjust for Inflation')).toBeInTheDocument();
   });
 
   it('renders the chart toggle', () => {
-    const { getByTestId, queryByTestId, getByRole } = render(<SavingsBondsSoldByTypeChart />);
+    const { getByTestId, queryByTestId, getByRole } = render(<SavingsBondsSoldByTypeChart chartData={mockData} />);
     const descriptionsToggle = getByRole('button', { name: 'Description' });
     expect(getByTestId('chartParent')).toBeInTheDocument();
     act(() => {
       fireEvent.click(descriptionsToggle);
     });
     expect(queryByTestId('chartParent')).not.toBeInTheDocument();
+  });
+
+  it('renders chart evergreen copy values', async () => {
+    const fetchSpy = jest.spyOn(global, 'fetch');
+    const { getByText } = render(<SavingsBondsSoldByTypeChart chartData={mockData} />);
+    await waitFor(() => expect(fetchSpy).toBeCalled());
+    expect(getByText('FY 1935 – FTYD 2024', { exact: false })).toBeInTheDocument();
+    expect(getByText('December 12, 2024', { exact: false })).toBeInTheDocument();
   });
 });
