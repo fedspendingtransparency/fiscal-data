b58106f0310a7680405fae7069013b7a8e5ae1c3
jhertzler <jordan.hertzler@kc.frb.org>
2024-03-05 08:24:46 -0600

    FDG-8591 Add data header

--- src/layouts/explainer/sections/treasury-savings-bonds/purchase-of-savings-bonds/i-bond-sales-chart/i-bond-sales-chart-helper.js
+++ src/layouts/explainer/sections/treasury-savings-bonds/purchase-of-savings-bonds/i-bond-sales-chart/i-bond-sales-chart-helper.js
@@ -5,10 +5,10 @@ export const chartCopy = {
   title: 'Correlation Between Inflation and I Bond Sales, 2003 – FYTD 2023',
   altText: 'Inflation and I bond sales shown over time. ',
   footer: (
-    <>
+    <p>
       Visit the <CustomLink url="datasets/electronic-securities-transactions/">Electronic Securities Transactions</CustomLink> dataset to explore and
       download this data. Inflation data is from the{' '}
       <CustomLink url="https://data.bls.gov/cgi-bin/surveymost?bls">Bureau of Labor Statistics</CustomLink>.
-    </>
+    </p>
   ),
 };
--- src/layouts/explainer/sections/treasury-savings-bonds/purchase-of-savings-bonds/i-bond-sales-chart/i-bond-sales-chart.tsx
+++ src/layouts/explainer/sections/treasury-savings-bonds/purchase-of-savings-bonds/i-bond-sales-chart/i-bond-sales-chart.tsx
@@ -3,12 +3,15 @@ import { chartCopy } from './i-bond-sales-chart-helper';
 import ChartContainer from '../../../../explainer-components/chart-container/chart-container';
 import { LineChart, ResponsiveContainer, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 import { getShortForm } from '../../../../../../utils/rounding-utils';
+import { chartStyle } from '../savings-bonds-sold-by-type-chart/savings-bonds-sold-by-type-chart.module.scss';
+import ChartDataHeader from '../../../../explainer-components/chart-data-header/chart-data-header';
 
 const IBondSalesChart: FunctionComponent = () => {
   const lastUpdated = new Date();
-  const [curInflation, setCurInflation] = useState();
-  const [curSales, setCurSales] = useState();
-  const [curYear, setYear] = useState();
+  const [curInflation, setCurInflation] = useState(7);
+  const [curSales, setCurSales] = useState('17 B');
+  const [curYear, setCurYear] = useState(2023);
+  const latestYear = 2023;
 
   const IBondMockData = [
     { year: 2003, sales: 0, inflation: 0 },
@@ -21,43 +24,79 @@ const IBondSalesChart: FunctionComponent = () => {
     { year: 2023, sales: 17000000000, inflation: 7 },
   ];
 
-  const CustomTooltip = ({ payload }) => {
-    console.log(payload);
+  const CustomTooltip = ({ payload, setYear }) => {
+    if (payload.length > 0) {
+      setYear(payload[0]?.payload.year);
+      const inflation = payload.find(x => x.dataKey === 'inflation');
+      setCurInflation(inflation.payload.inflation);
+      const sales = payload.find(x => x.dataKey === 'sales');
+      setCurSales(getShortForm(sales.payload.sales));
+    }
     return <></>;
+  };
+
+  const header = (
+    <ChartDataHeader
+      fiscalYear={curYear}
+      right={{ label: 'I Bonds Sales', value: `$${curSales}` }}
+      left={{ label: 'Inflation', value: `${curInflation.toFixed(1)}%` }}
+    />
+  );
+
+  const Legend = () => (
+    <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
+      <div style={{ display: 'flex' }}>
+        <div style={{ color: '#B04ABD', fontSize: '14px', fontWeight: '600' }}>I Bond Sales</div>
+        <div style={{ width: '24px', borderBottom: '0.5px solid #B04ABD', height: '8px', marginLeft: '0.5rem' }} />
+      </div>
+      <div>Inflation</div>
+    </div>
+  );
+  const resetDataHeader = () => {
+    setCurYear(latestYear);
+    setCurSales('17 B');
+    setCurInflation(7);
   };
 
   return (
     <>
-      <ChartContainer title={chartCopy.title} altText={chartCopy.altText} date={lastUpdated} footer={chartCopy.footer} header={chartCopy.header}>
-        <ResponsiveContainer height={377} width="99%">
-          <LineChart data={IBondMockData} margin={{ top: 16, bottom: 0, left: -18, right: 16 }}>
-            <CartesianGrid vertical={false} stroke="#d9d9d9" />
-            <XAxis dataKey="year" type="number" domain={[2003, 2023]} ticks={[2003, 2008, 2013, 2018, 2023]} minTickGap={3} />
-            <YAxis dataKey="sales" axisLine={false} tickLine={false} domain={[0, 20000000000]} tickFormatter={value => `$${getShortForm(value)}`} />
-            <YAxis
-              yAxisId={1}
-              dataKey="inflation"
-              type="number"
-              axisLine={false}
-              tickLine={false}
-              domain={[0, 8]}
-              tickFormatter={value => `${value}%`}
-              orientation="right"
-            />
-            <Line dataKey="sales" stroke="#B04ABD" dot={false} strokeWidth={1} activeDot={false} isAnimationActive={false} />
-            <Line
-              dataKey="inflation"
-              stroke="#666"
-              strokeDasharray="2 2"
-              dot={false}
-              strokeWidth={2}
-              activeDot={false}
-              isAnimationActive={false}
-              yAxisId={1}
-            />
-            <Tooltip content={<CustomTooltip />} cursor={{ strokeDasharray: '4 4', stroke: '#555', strokeWidth: '2px' }} isAnimationActive={false} />
-          </LineChart>
-        </ResponsiveContainer>
+      <ChartContainer title={chartCopy.title} altText={chartCopy.altText} date={lastUpdated} footer={chartCopy.footer} header={header}>
+        <div className={chartStyle} data-testid="chartParent">
+          <Legend />
+          <ResponsiveContainer height={352} width="99%">
+            <LineChart data={IBondMockData} margin={{ top: 16, bottom: 0, left: -16, right: -22 }} onMouseLeave={resetDataHeader}>
+              <CartesianGrid vertical={false} stroke="#d9d9d9" />
+              <XAxis dataKey="year" type="number" domain={[2003, latestYear]} ticks={[2003, 2008, 2013, 2018, 2023]} minTickGap={3} />
+              <YAxis dataKey="sales" axisLine={false} tickLine={false} domain={[0, 20000000000]} tickFormatter={value => `$${getShortForm(value)}`} />
+              <YAxis
+                yAxisId={1}
+                dataKey="inflation"
+                type="number"
+                axisLine={false}
+                tickLine={false}
+                domain={[0, 8]}
+                tickFormatter={value => `${value.toFixed(1)}%`}
+                orientation="right"
+              />
+              <Line dataKey="sales" stroke="#B04ABD" dot={false} strokeWidth={1} activeDot={false} isAnimationActive={false} />
+              <Line
+                dataKey="inflation"
+                stroke="#666"
+                strokeDasharray="2 2"
+                dot={false}
+                strokeWidth={2}
+                activeDot={false}
+                isAnimationActive={false}
+                yAxisId={1}
+              />
+              <Tooltip
+                content={<CustomTooltip setYear={setCurYear} />}
+                cursor={{ strokeDasharray: '4 4', stroke: '#555', strokeWidth: '2px' }}
+                isAnimationActive={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
       </ChartContainer>
     </>
   );
