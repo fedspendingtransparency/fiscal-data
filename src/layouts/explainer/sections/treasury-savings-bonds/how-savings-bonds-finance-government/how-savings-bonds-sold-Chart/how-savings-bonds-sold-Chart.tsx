5bedeeb240bf375e620d0d46e148564f9d7850b3
chasls2 <110061741+chasls2@users.noreply.github.com>
2024-03-05 17:05:50 -0600

    changes updated

--- src/layouts/explainer/sections/treasury-savings-bonds/how-savings-bonds-finance-government/how-savings-bonds-sold-Chart/how-savings-bonds-sold-Chart.tsx
+++ src/layouts/explainer/sections/treasury-savings-bonds/how-savings-bonds-finance-government/how-savings-bonds-sold-Chart/how-savings-bonds-sold-Chart.tsx
@@ -14,43 +14,27 @@ interface DataItem {
   value: number;
   security: boolean;
 }
-interface CustomTooltipProps extends TooltipProps<number, string> {
- }
-
-const CustomTooltip: React.FC<CustomTooltipProps> = ({ active, payload }) => {
-  if (active && payload && payload.length) {
-    const data = payload[0].payload as DataItem; 
-    return (
-      <div style={{ backgroundColor: '#fff', padding: '5px', border: '1px solid #ccc' }}>
-        <p>{data.name}</p>
-        <p>{`Value: ${data.value}`}</p>
-
-      </div>
-    );
-  }
-  return null;
-};
 
 
 const data01 = [
-  { name: 'Group A', value: 391.2, security: false  },
-  { name: 'Group B', value: 8.8, security: true  },
+  { name: 'Marketable Securities', value: 391.2, security: false, securityType: 'Marketable' },
+  { name: 'Non-Marketable Securities', value: 8.8, security: true, securityType: 'Non-Marketable' },
 
 ];
 const data02 = [
-  { name: 'B3', value: 5, security: false },
-  { name: 'B2', value: 30, security: false },
-  { name: 'B1', value: 65, security: false },
-  { name: 'A2', value: 100, security: false },
-  { name: 'A1', value: 191.2, security: false },
-  { name: 'B5', value: 3.8, security: true  },
-  { name: 'B4', value: 3, security: true  },
-  { name: 'C1', value: 2, security: true  },
+  { name: 'Bonds 1', value: 5, security: false, securityType: 'Marketable' },
+  { name: 'Bonds 2', value: 30, security: false, securityType: 'Marketable' },
+  { name: 'Bonds 3', value: 65, security: false, securityType: 'Marketable' },
+  { name: 'Bonds 4', value: 100, security: false, securityType: 'Marketable' },
+  { name: 'Bonds 5', value: 191.2, security: false, securityType: 'Marketable' },
+  { name: 'Savings Bonds 1', value: 3.8, security: true, securityType: 'Non-Marketable' },
+  { name: 'Savings Bonds 2', value: 3, security: true, securityType: 'Non-Marketable' },
+  { name: 'Other', value: 2, security: true, securityType: 'Non-Marketable' },
 ];
 
 const color = '#4A0072';
 const color2 = '#B04ABD';
-const colors = { true: '#4A0072', false: '#B04ABD' };
+
 
 const HowSavingsBondsSoldChart: FunctionComponent = ({ glossary, glossaryClickHandler }) => {
   const [activeIndex, setActiveIndex] = useState<string | null>(null);
@@ -68,10 +52,42 @@ const HowSavingsBondsSoldChart: FunctionComponent = ({ glossary, glossaryClickHa
     </GlossaryPopoverDefinition>
   );
 
-  const customLegend = [
-    {value: 'Marketable Security', type: 'rect', id: 'secuirtyTrue', color: color},
-    {value: 'Non-Marketable Secuirty', type: 'rect', id: 'secuirtyFalse', color: color2}
-  ]
+  const calculatePercentage = (data) => {
+    if(!Array.isArray(data)){
+      console.error('Invalide data, ',data);
+      return[];
+    }
+    const total = data.reduce((acc, curr) => acc + curr.value, 
+    0);
+    return data.map(item => ({
+      ...item,
+      percent: Number(((item.value / total) * 100).toFixed(2))
+    }));
+  };
+
+  const data1WidthPercentage = calculatePercentage(data01);
+  const data2WidthPercentage = calculatePercentage(data02);
+
+  const CustomTooltip: React.FC<CustomTooltipProps> = ({ active, payload }) => {
+    if (active && payload && payload.length) {
+      const data = payload[0].payload as DataItem;
+      return (
+        <div style={{ backgroundColor: '#fff', padding: '12px 16px', border: '1px solid #ccc', display: 'flex' }}>
+          <span>
+          <div style={{ backgroundColor: color2, width: '18px', height:'18px', marginRight: '8px' }}></div>
+          </span>
+          <span>
+          <div style={{ fontWeight: '600', fontSize: '14px'}}>{data.name}</div>
+          <div style={{ fontWeight: '400', fontSize: '12px', fontStyle: 'italic'}}>{`${data.securityType}`}</div>
+          <div style={{ fontWeight: '400', fontSize: '14px'}}>{`${data.percent}% of National Debt`}</div> 
+          </span>
+
+        </div>
+      );
+    }
+    return null;
+  };
+  
 
   const lastUpdated = new Date();
   const footer = (
@@ -91,12 +107,12 @@ const HowSavingsBondsSoldChart: FunctionComponent = ({ glossary, glossaryClickHa
   };
 
   const CustomLegend = () => (
-    <div style={{ display: 'flex', justifyContent: 'center', marginTop: 20 }}>
-      <div role='presentation' onMouseEnter={() => onLegendEnter(true)} onMouseLeave={onChartLeave} style={{ cursor: 'pointer', marginRight: 10 }}>
-        <span style={{ color: color }}>&#9632;</span> Marketable Security
+    <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: 20 }}>
+      <div role='presentation' onMouseEnter={() => onLegendEnter(true)} onMouseLeave={onChartLeave} style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
+        <div style={{ backgroundColor: color, width: '24px', height:'18px', marginRight: '8px' }}></div> Marketable Security
       </div>
-      <div role='presentation' onMouseEnter={() => onLegendEnter(false)} onMouseLeave={onChartLeave} style={{ cursor: 'pointer' }}>
-        <span style={{ color: color2 }}>&#9632;</span> Non-Marketable Security
+      <div role='presentation' onMouseEnter={() => onLegendEnter(false)} onMouseLeave={onChartLeave} style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
+        <div style={{ backgroundColor: color2, width: '24px', height:'18px', marginRight: '8px' }}></div> Non-Marketable Security
       </div>
     </div>
   );
@@ -110,10 +126,6 @@ const HowSavingsBondsSoldChart: FunctionComponent = ({ glossary, glossaryClickHa
   const getOpacity = (dataset: string, index: number, entry: DataItem) => {
     return activeIndex === `${dataset}-${index}` || activeIndex === null ? entry.security === activeSecurity ? .4 : 1 : .4;
   }
-  // const getOpacity = (entry: DataItem) => {
-  //   return activeSecurity === null || entry.security === activeSecurity ? 1 : 0.4;
-  // };
-
 
   return (
     <>
@@ -122,8 +134,8 @@ const HowSavingsBondsSoldChart: FunctionComponent = ({ glossary, glossaryClickHa
             <ResponsiveContainer height={485} width="99%">
               <PieChart width={400} height={400} onMouseLeave={onPieLeave}>
                 <Pie 
-                  data={data01} 
-                  dataKey="value" 
+                  data={data1WidthPercentage} 
+                  dataKey="percent" 
                   cx="50%" 
                   cy="50%" 
                   innerRadius={100} 
@@ -139,8 +151,8 @@ const HowSavingsBondsSoldChart: FunctionComponent = ({ glossary, glossaryClickHa
                   }
                 </Pie>
                 <Pie 
-                  data={data02} 
-                  dataKey="value" 
+                  data={data2WidthPercentage} 
+                  dataKey="percent" 
                   cx="50%" 
                   cy="50%" 
                   innerRadius={170} 
