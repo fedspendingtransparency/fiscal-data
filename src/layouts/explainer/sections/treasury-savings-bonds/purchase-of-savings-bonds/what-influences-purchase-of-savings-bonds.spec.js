f34076d3a9a24236a2389aa2491d0bf4e196dc31
chasls2 <110061741+chasls2@users.noreply.github.com>
2024-03-06 12:14:56 -0600

    Update what-influences-purchase-of-savings-bonds.spec.js

--- src/layouts/explainer/sections/treasury-savings-bonds/purchase-of-savings-bonds/what-influences-purchase-of-savings-bonds.spec.js
+++ src/layouts/explainer/sections/treasury-savings-bonds/purchase-of-savings-bonds/what-influences-purchase-of-savings-bonds.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, waitFor } from '@testing-library/react'; import '@testing-library/jest-dom/extend-expect';
+import { render, waitFor } from '@testing-library/react'; 
 import WhatInfluencesPurchaseOfSavingsBonds from './what-influences-purchase-of-savings-bonds';
 import mockSavingsBondFetchResponses from './../../../explainer-test-helper';
 import { useStaticQuery } from 'gatsby';
