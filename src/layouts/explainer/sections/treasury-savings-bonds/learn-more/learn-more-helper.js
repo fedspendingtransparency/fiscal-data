d0ed1fa33fdbc2b2fe44aa12d62a33dde8fc3136
chasls2 <110061741+chasls2@users.noreply.github.com>
2024-03-13 10:43:22 -0500

    FDG-8599 add Footer

---
+++ src/layouts/explainer/sections/treasury-savings-bonds/learn-more/learn-more-helper.js
@@ -0,0 +1,12 @@
+export const getSaleBondsFootNotes = () => {
+  return [
+    {
+      anchors: [{ text: ['1'], link: ['https://www.treasurydirect.gov/files/research-center/history-of-savings-bond/history-sb.pdf'] }],
+      definition: `A History of the United States Savings Bonds Program, page 5`,
+    },
+    {
+      anchors: [{ text: ['2'], link: ['https://www.treasurydirect.gov/files/research-center/history-of-savings-bond/history-sb.pdf'] }],
+      definition: `A History of the United States Savings Bonds Program, page 3`,
+    },
+  ];
+};
