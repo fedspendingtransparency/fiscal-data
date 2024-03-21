06c9dbc49bbbe1334baf8d48c6f7fcd59beb854c
Nicolle Weidner <nicolle.weidner@kc.frb.org>
2024-03-21 15:57:51 -0500

    FDG-8826 move spinner to SiteLayout logic

--- src/components/siteLayout/siteLayout.jsx
+++ src/components/siteLayout/siteLayout.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
 import SiteHeader from '../site-header/site-header';
 import SiteFooter from '../site-footer/site-footer';
 import { ErrorBoundary } from 'react-error-boundary';
 import NotFound from '../../pages/404/index';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { loadingIcon } from './site-layout.module.scss';
 
 export const preProdEnvMsg = 'Loading metadata and data from endpoints in pre-production environment.';
 
-const SiteLayout = ({ children, isPreProd }) => {
+const SiteLayout = ({ children, isPreProd, loading }) => {
+  console.log('loading::: ', loading);
   let lowerEnvMsg;
 
   if (isPreProd) {
@@ -16,7 +19,12 @@ const SiteLayout = ({ children, isPreProd }) => {
   return (
     <div>
       <SiteHeader lowerEnvMsg={lowerEnvMsg} />
-      <ErrorBoundary FallbackComponent={() => <NotFound fallback="true" />}>{children}</ErrorBoundary>
+      {loading && (
+        <div className={loadingIcon}>
+          <FontAwesomeIcon icon={faSpinner} spin pulse /> Loading...
+        </div>
+      )}
+      {!loading && <ErrorBoundary FallbackComponent={() => <NotFound fallback="true" />}>{children}</ErrorBoundary>}
       <SiteFooter />
     </div>
   );
