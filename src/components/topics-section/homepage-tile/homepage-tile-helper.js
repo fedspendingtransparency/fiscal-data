cafe791184ad3fa0085acb38b3c99050f89a246a
Nicolle Weidner <nicolle.weidner@kc.frb.org>
2024-03-22 10:00:12 -0500

    FDG-8440 update sbUrl

--- src/components/topics-section/homepage-tile/homepage-tile-helper.js
+++ src/components/topics-section/homepage-tile/homepage-tile-helper.js
@@ -57,12 +57,13 @@ export const SavingsBondsBodyGenerator = () => {
   const [savingsBondsAmount, setSavingsBondsAmount] = useState(null);
   const [recordFiscalYear, setRecordFiscalYear] = useState(null);
   // eslint-disable-next-line max-len
-  const sbUrl = `v1/accounting/od/securities_sales?filter=security_type_desc:eq:Savings%20Bond,record_fiscal_year:eq:2023`;
+  const sbUrl = `v1/accounting/od/securities_sales?filter=security_type_desc:eq:Savings%20Bond&sort=-record_date`;
 
   useEffect(() => {
     basicFetch(`${apiPrefix}${sbUrl}`).then(res => {
       if (res.data) {
         const data = res.data;
+
         let savingsBondsTotal = 0;
         data.map(index => {
           savingsBondsTotal = savingsBondsTotal + parseInt(index.gross_sales_amt);
