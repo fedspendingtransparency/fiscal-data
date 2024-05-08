189901415a88bb3f80d63a97217721dadfcc5126
Wyatt Chandler <wyatt.chandler@kc.frb.org>
2024-05-08 15:47:06 -0500

    FDG-9117 added support in banner content for markdown, adjusted styling, adjusted link parser

--- src/components/site-header/banner-content/banner-content.jsx
+++ src/components/site-header/banner-content/banner-content.jsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import CustomLink from '../../links/custom-link/custom-link';
-import {bannerContent} from './../site-header.module.scss';
-import reactStringReplace from "react-string-replace";
+import { bannerContent } from './../site-header.module.scss';
+import reactStringReplace from 'react-string-replace';
+import reactElementToJSXString from 'react-element-to-jsx-string';
+import ReactMarkdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
 
-const BannerContent = ({content}) => {
+const BannerContent = ({ content }) => {
+  const replaceLinkTextWithLink = string => {
+    const regex = /(https?:\/\/[^\s]+)/g;
+    return reactStringReplace(string, regex, (match, i) => {
+      return reactElementToJSXString(<a href={match}>{match}</a>);
+    });
+  };
 
-const replaceLinkTextWithLink = () => {
-  const regex = /(https?:\/\/[^\s]+)/g;
-  return reactStringReplace(content, regex, (match, i) => {
-    return <CustomLink url={match}>{match}</CustomLink>;
-  });
-}
-
-
-
-return (
-      <>
-        <div className={bannerContent} data-testid={'announcement-banner'}>
-          {replaceLinkTextWithLink()}
-        </div>
-      </>
+  return (
+    <>
+      <div className={bannerContent} data-testid={'announcement-banner'}>
+        <ReactMarkdown
+          children={replaceLinkTextWithLink(content)
+            .toString()
+            .replaceAll(/,/g, '')}
+          components={{
+            a: ({ children, href }) => {
+              return <CustomLink url={href}>{children}</CustomLink>;
+            },
+          }}
+          rehypePlugins={[rehypeRaw]}
+        />
+      </div>
+    </>
   );
 };
 export default BannerContent;
--- src/components/site-header/banner-content/banner-content.spec.js
+++ src/components/site-header/banner-content/banner-content.spec.js
@@ -3,16 +3,20 @@ import { render, screen } from '@testing-library/react';
 import BannerContent from './banner-content';
 
 describe('ContentUnavailable', () => {
-
   it('renders content with link', () => {
-    render(<BannerContent content={'We\'re working to correct an issue with this dataset. Please find the static data at https://fiscaldata.treasury.gov/static-data/published-reports/debt_to_penny.pdf'} />);
-    expect(screen.getByText(`Please find the static data at`, {exact: false})).toBeInTheDocument();
-    expect(screen.getByTestId('external-link')).toBeInTheDocument();
+    render(
+      <BannerContent
+        content={
+          "We're working to correct an issue with this dataset. Please find the static data at https://fiscaldata.treasury.gov/static-data/published-reports/debt_to_penny.pdf"
+        }
+      />
+    );
+    expect(screen.getByText(`Please find the static data at`, { exact: false })).toBeInTheDocument();
+    expect(screen.getByText('href=', { exact: false })).toBeInTheDocument();
   });
 
   it('does not close when closable is set to false', () => {
     render(<BannerContent />);
     expect(screen.queryByRole('button', { name: /close/i })).not.toBeInTheDocument();
   });
-
 });
