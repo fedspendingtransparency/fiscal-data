1683cf373d1f4db80462e2e03daf7ba126eaeec8
jhertzler <jordan.hertzler@kc.frb.org>
2024-05-16 08:22:18 -0500

    FDG-9096 Add test

--- cypress/e2e/dataset-detail-page/dataset-detail-page.cy.ts
+++ cypress/e2e/dataset-detail-page/dataset-detail-page.cy.ts
@@ -1,6 +1,27 @@
 describe('Dataset detail page validation', () => {
-  it('navigates to DTS', () => {
-    cy.visit('/datasets/daily-treasury-statement/');
-    cy.contains('Daily Treasury Statement (DTS)');
+  const datasets: { url: string; name: string; dataTables: { name: string; endpoint: string }[] }[] = [
+    {
+      url: '/datasets/daily-treasury-statement/',
+      name: 'Daily Treasury Statement (DTS)',
+      dataTables: [
+        { name: 'Operating Cash Balance', endpoint: '/v1/accounting/dts/operating_cash_balance' },
+        { name: 'Public Debt Transactions', endpoint: '/v1/accounting/dts/public_debt_transactions' },
+        { name: 'Adjustment of Public Debt Transactions to Cash Basis', '/v1/accounting/dts/adjustment_public_debt_transactions_cash_basis' },
+      ],
+    },
+  ];
+
+  it('loops', () => {
+    datasets.forEach(dataset => {
+      cy.visit(dataset.url);
+      cy.contains(dataset.dataTables[0].name).click();
+      dataset.dataTables.forEach(table => {
+        cy.contains(table.name).click();
+        // Endpoint in the API Quick Guide documentation updates for each table
+        cy.contains('/services/api/fiscal_service' + table.endpoint);
+        // cy.get('input').type('opening balance');
+        cy.contains(table.name).click();
+      });
+    });
   });
 });
