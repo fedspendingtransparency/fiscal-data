31fb404a0a3615c09775d5254621e2f107a8a20c
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2024-05-20 08:36:29 -0500

    wip

--- cypress/e2e/dataset-detail-page/dataset-detail-page.cy.ts
+++ cypress/e2e/dataset-detail-page/dataset-detail-page.cy.ts
@@ -18,20 +18,41 @@ describe('Dataset detail page validation', () => {
     },
   ];
 
-  it('loops', () => {
-    datasets.forEach(dataset => {
-      cy.visit(dataset.url);
-      cy.contains(dataset.dataTables[0].name).click();
-      dataset.dataTables.forEach(table => {
-        cy.contains(table.name).click();
-        // Endpoint in the API Quick Guide documentation updates for each table
-        cy.contains('/services/api/fiscal_service' + table.endpoint);
-        cy.get('input[aria-label="filter ' + table.column.name + ' column"]').type(table.column.searchTerm);
-        cy.contains(table.name).click();
-        cy.contains(table.column.searchTerm)
-          .its('length')
-          .should('eq', 10);
-      });
+  const dtsDataset = {
+    url: '/datasets/daily-treasury-statement/',
+    name: 'Daily Treasury Statement (DTS)',
+    dataTables: [
+      {
+        name: 'Operating Cash Balance',
+        endpoint: '/v1/accounting/dts/operating_cash_balance',
+        column: { prettyName: 'Type of Account', name: 'account_type', searchTerm: 'Table II' },
+      },
+      // {
+      //   name: 'Public Debt Transactions',
+      //   endpoint: '/v1/accounting/dts/public_debt_transactions',
+      //   largeTable: true,
+      //   column: { prettyName: 'Type of Account', name: 'account_type', searchTerm: 'Table II' },
+      // },
+      // {
+      //   name: 'Adjustment of Public Debt Transactions to Cash Basis',
+      //   endpoint: '/v1/accounting/dts/adjustment_public_debt_transactions_cash_basis',
+      // },
+    ],
+  };
+
+  it('Loads DTS', () => {
+    const dataset = dtsDataset;
+    cy.visit(dataset.url);
+    cy.contains(dataset.dataTables[0].name).click();
+    dataset.dataTables.forEach(table => {
+      cy.contains(table.name).click();
+      // Endpoint in the API Quick Guide documentation updates for each table
+      cy.contains('/services/api/fiscal_service' + table.endpoint);
+      cy.get('input[aria-label="filter ' + table.column.name + ' column"]').type(table.column.searchTerm);
+      cy.contains(table.column.searchTerm)
+        .its('length')
+        .should('eq', 10);
+      cy.contains(table.name).click();
     });
   });
 });
