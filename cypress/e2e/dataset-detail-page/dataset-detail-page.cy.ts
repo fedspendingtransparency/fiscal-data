b6ce81e9e4908800d16f354f341a06f0eb426b75
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2024-05-21 10:21:09 -0500

    FDG-9069 Updates slgs config

--- cypress/e2e/dataset-detail-page/dataset-detail-page.cy.ts
+++ cypress/e2e/dataset-detail-page/dataset-detail-page.cy.ts
@@ -62,7 +62,7 @@ describe('Dataset detail page validation', () => {
       {
         name: 'Demand Deposit Rate',
         endpoint: '/v1/accounting/od/slgs_demand_deposit_rates',
-        updateDateRange: '1 year',
+        updateDateRange: '1 Year',
         column: { prettyName: 'Fiscal Year', name: 'fiscal_year', searchTerm: '20' },
       },
       {
