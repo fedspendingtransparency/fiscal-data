4de283349de58f5fa08c38e23b2da4f32dd90aed
chasls2 <110061741+chasls2@users.noreply.github.com>
2024-05-28 10:55:42 -0500

    Removed comments

--- src/layouts/explainer/sections/national-deficit/deficit-key-takeaways/deficit-key-takeaways.jsx
+++ src/layouts/explainer/sections/national-deficit/deficit-key-takeaways/deficit-key-takeaways.jsx
@@ -8,7 +8,6 @@ import { deficitExplainerLightSecondary, deficitExplainerPrimary } from '../nati
 import useFetchSurplusCount from '../understanding/deficit-surplus-count-helper';
 
 const DeficitKeyTakeaways = () => {
-  // const [surplus, setSurplus] = useState(0);
   const currentYear = new Date().getFullYear();
   const startYear = currentYear - 50;
   const surplus = useFetchSurplusCount(startYear);
@@ -25,21 +24,6 @@ const DeficitKeyTakeaways = () => {
       </CustomLink>
     );
   });
-  // useEffect(() => {
-  //   const fields = 'fields=current_fytd_net_outly_amt,record_date,record_calendar_month,record_fiscal_year';
-  //   const sort = 'sort=record_date';
-  //   const endpointUrl = `v1/accounting/mts/mts_table_5?${fields}&filter=line_code_nbr:eq:5694,record_calendar_month:eq:09&${sort}`;
-  //   basicFetch(`${apiPrefix}${endpointUrl}`).then(res => {
-  //     if (res.data && res.data.length > 0) {
-  //       let surplusCount = res.data.reduce((count, item) => {
-  //         return parseFloat(item.current_fytd_net_outly_amt) > 0 ? count + 1 : count;
-  //       }, 0);
-  //       //the + 4 is to account for surplus that are not in the data.
-  //       setSurplus(surplusCount + 4);
-  //     }
-  //   });
-  // }, []);
-  // const surplusAmount = surplus;
 
   const deficitKeyTakeaways = [
     {
