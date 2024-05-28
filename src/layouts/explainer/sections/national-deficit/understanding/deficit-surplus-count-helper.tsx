d82af73b9a7d84c94b0ba216ad26cd2772caeb27
chasls2 <110061741+chasls2@users.noreply.github.com>
2024-05-28 14:36:09 -0500

    fetch ERROR GONE

--- src/layouts/explainer/sections/national-deficit/understanding/deficit-surplus-count-helper.tsx
+++ src/layouts/explainer/sections/national-deficit/understanding/deficit-surplus-count-helper.tsx
@@ -9,14 +9,15 @@ const useFetchSurplusCount = (startYear: number): number => {
   const [surplus, setSurplus] = useState<number>(4); // Start with an initial surplus of 4 not included in data
 
   useEffect(() => {
-    const fields = 'fields=current_fytd_net_outly_amt';
-    const endpointUrl = `${apiPrefix}v1/accounting/mts/mts_table_5?${fields}&filter=line_code_nbr:eq:5694,record_fiscal_year:gte:${startYear}`;
-
-    basicFetch(endpointUrl).then(res => {
+    const fields = 'fields=current_fytd_net_outly_amt,record_date,record_calendar_month,record_fiscal_year';
+    const sort = 'sort=record_date';
+    const endpointUrl = `v1/accounting/mts/mts_table_5?${fields}&filter=line_code_nbr:eq:5694,record_calendar_month:eq:09&${sort}`;
+    basicFetch(`${apiPrefix}${endpointUrl}`).then(res => {
       if (res.data) {
         const surplusCount = res.data.reduce((count, item: FiscalData) => {
           return item.current_fytd_net_outly_amt > 0 ? count + 1 : count;
         }, 0);
+
         setSurplus(prev => prev + surplusCount);
       }
     });
