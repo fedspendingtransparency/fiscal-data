341db04b4d41087b79425bf75f8917d57336a364
Nicolle Weidner <nicolle.weidner@kc.frb.org>
2024-05-31 16:21:43 -0500

    FDG-9170 revert sitlayout to use NotFound

--- src/components/siteLayout/siteLayout.jsx
+++ src/components/siteLayout/siteLayout.jsx
@@ -3,7 +3,7 @@ import SiteHeader from '../site-header/site-header';
 import SiteFooter from '../site-footer/site-footer';
 import { ErrorBoundary } from 'react-error-boundary';
 import '../../../src/styles.scss';
-import Unavailable from '../../pages/unavailable';
+import NotFound from '../../pages/404';
 
 export const preProdEnvMsg = 'Loading metadata and data from endpoints in pre-production environment.';
 
@@ -17,7 +17,7 @@ const SiteLayout = ({ children, isPreProd }) => {
   return (
     <div>
       <SiteHeader lowerEnvMsg={lowerEnvMsg} />
-      <ErrorBoundary FallbackComponent={() => <Unavailable fallback="true" />}>{children}</ErrorBoundary>
+      <ErrorBoundary FallbackComponent={() => <NotFound fallback="true" />}>{children}</ErrorBoundary>
       <SiteFooter style={{}} />
     </div>
   );
