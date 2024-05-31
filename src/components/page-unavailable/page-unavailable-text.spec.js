65fca8b6516d8e16d4b06120335d470400529561
Nicolle Weidner <nicolle.weidner@kc.frb.org>
2024-05-30 22:09:44 -0500

    FDG-9170 add unavailable text test file

---
+++ src/components/page-unavailable/page-unavailable-text.spec.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import PageUnavailableText from './page-unavailable-text';
+import notFoundGraphic from '../pageError/page-error-graphic';
+
+describe('Unavailable Text', () => {
+  let component = renderer.create();
+  renderer.act(() => {
+    component = renderer.create(<PageUnavailableText />);
+  });
+
+  let instance = component.root;
+
+  it('includes an h1 header', () => {
+    const header = instance.findByType('h1');
+    expect(header).toBeDefined();
+    expect(header.props.className).toBe('unavailableHeader');
+  });
+
+  it('includes an h1 header', () => {
+    const header = instance.findByType('h1');
+    expect(header).toBeDefined();
+    expect(header.props.children.props.children).toBe('Fiscal Data is unavailable right now.');
+  });
+
+  it('includes an unavailable header', () => {
+    const header = instance.findByType('h2');
+    expect(header).toBeDefined();
+    expect(header.props.children.props.children).toBe('We will be back shortly.');
+  });
+
+  it('includes the not found graphic', () => {
+    const graphic = instance.findByType(notFoundGraphic);
+    expect(graphic).toBeDefined();
+  });
+});
+
+describe('Fallback for Error Boundary', () => {
+  let component = renderer.create();
+  renderer.act(() => {
+    component = renderer.create(<PageUnavailableText fallback="true" />);
+  });
+
+  let instance = component.root;
+
+  it('includes an h1 header', () => {
+    const header = instance.findByType('h1');
+    expect(header).toBeDefined();
+    expect(header.props.className).toBe('unavailableHeader');
+  });
+
+  it('includes fallback header', () => {
+    const h2 = instance.findByType('h2');
+    expect(h2).toBeDefined();
+    expect(h2.props.children.props.children).toBe('This content is currently unavailable.');
+  });
+
+  it('includes the not found graphic', () => {
+    const graphic = instance.findByType(notFoundGraphic);
+    expect(graphic).toBeDefined();
+  });
+});
