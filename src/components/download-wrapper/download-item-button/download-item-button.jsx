98fe8e99c6aa458683564f518749b4f6f3402d3c
Nicolle Weidner <nicolle.weidner@kc.frb.org>
2024-07-05 13:45:05 -0500

    FDG-9310 forcing XML prolog into test xml (test)

--- src/components/download-wrapper/download-item-button/download-item-button.jsx
+++ src/components/download-wrapper/download-item-button/download-item-button.jsx
@@ -80,11 +80,14 @@ const DownloadItemButton = ({
         </a>
       );
     } else if (directXMLDownload && smallTableXMLData.length > 0) {
+      let smallTableXMLData2 = '<?xml version="1.0" encoding="UTF-8"?><root-element><data><data-element><record_date>NICOLLE</record_date><type>Total</type><public_prin_borrowings_amt>21547759724058.27</public_prin_borrowings_amt> <public_prin_repayments_amt>20180954532736.88</public_prin_repayments_amt> <public_interest_accrued_amt>445416941012.02</public_interest_accrued_amt> <public_interest_paid_amt>433213532978.59</public_interest_paid_amt> <public_net_unamortized_amt>-247609733757.52</public_net_unamortized_amt> <public_net_amortization_amt>-235639090828.11</public_net_amortization_amt> <intragov_prin_net_increase_amt>332879277182.76</intragov_prin_net_increase_amt> <intragov_interest_accrued_amt>168210307241.85</intragov_interest_accrued_amt> <intragov_interest_paid_amt>189837393552.90</intragov_interest_paid_amt> <intragov_net_unamortized_amt>-54178312240.36</intragov_net_unamortized_amt> <intragov_net_amortization_amt>1609571417.91</intragov_net_amortization_amt></data-element></data></root-element>';
+      console.log('smallTableXMLData: ', smallTableXMLData);
+      console.log('smallTableXMLData2: ', smallTableXMLData2);
       return (
         <a
           className={`${downloadItemBtn} ${disabled ? linkDisabled : ''}`}
           data-testid="xml-download-button"
-          href={`data:text/xml;charset=utf-8,${encodeURIComponent(smallTableXMLData)}`}
+          href={`data:text/xml;charset=utf-8,${encodeURIComponent(smallTableXMLData2)}`}
           download={downloadName + '.xml'}
         >
           {children}
