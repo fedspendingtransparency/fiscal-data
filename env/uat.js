70b74d635068d187103c57044f1f2af31d1b3dfb
chasls2 <110061741+chasls2@users.noreply.github.com>
2024-07-05 10:09:20 -0500

    Added Top to UAT

--- env/uat.js
+++ env/uat.js
@@ -21,6 +21,19 @@ module.exports = {
       relatedDatasets: ['015-BFS-2014Q1-13', '015-BFS-2014Q1-03', '015-BFS-2014Q1-11'],
       currentDateButton: 'byDay',
     },
+    '015-BFS-2014Q3-052': {
+      slug: '/top-treasury-offset-program/',
+      seoConfig: {
+        pageTitle: 'Treasury Offset Program (TOP) | U.S. Treasury Fiscal Data',
+        description:
+          'This dataset shows how Treasury offsets federal payments, such as tax refunds, to ' +
+          'pay off delinquent debts such as unpaid child support.',
+        keywords: 'Debt, Revenue',
+      },
+      topics: ['debt', 'revenue'],
+      relatedDatasets: ['015-BFS-2020Q4-xx', '015-BFS-2014Q1-03', '015-BFS-2014Q1-13', '015-BFS-2017Q2-003'],
+      currentDateButton: 'byMonth',
+    },
   },
   ADDITIONAL_ENDPOINTS: {
     '160': {
@@ -142,6 +155,94 @@ module.exports = {
         'fytd_redeemed_amt',
         'daily_ending_balance_amt',
       ],
+    },
+    '299': {
+      endpoint: 'v1/debt/treasury_offset_program',
+      dateField: 'record_date',
+      downloadName: 'treasury_offset_program',
+      alwaysSortWith: ['record_date, src_line_nbr'],
+      selectColumn: [],
+      // 'Pivot View' in UI; 'Pivot View (Field)' and 'Pivot View (Name)' on form
+      dataDisplays: [
+        {
+          title: 'By State Code',
+          dimensionField: 'state_cd',
+        },
+        {
+          title: 'By Mixed Stream',
+          dimensionField: 'mixed_stream',
+        },
+        {
+          title: 'By State or Agency',
+          dimensionField: 'tate_or_agency',
+        },
+        {
+          title: 'By Agency Name',
+          dimensionField: 'agency_nm',
+        },
+        {
+          title: ' By Agency Full Code Name',
+          dimensionField: 'agency_full_cd_nm',
+        },
+        {
+          title: 'By Agency ID',
+          dimensionField: 'agency_id',
+        },
+        {
+          title: 'By Agency',
+          dimensionField: 'agency',
+        },
+        {
+          title: 'By Agency Site',
+          dimensionField: 'agency_site',
+        },
+        {
+          title: 'By Agency Site Code',
+          dimensionField: 'agency_site_cd',
+        },
+        {
+          title: 'By Agency Site ID',
+          dimensionField: 'agency_site_id',
+        },
+        {
+          title: 'By Payment Category Description',
+          dimensionField: 'payment_category',
+        },
+        {
+          title: 'By Payment Type Code',
+          dimensionField: 'payment_type_cd',
+        },
+        {
+          title: 'By Payment Agency Name',
+          dimensionField: 'payment_agency_nm',
+        },
+        {
+          title: 'By Payment Type',
+          dimensionField: 'payment_type',
+        },
+        {
+          title: 'By Payment Source',
+          dimensionField: 'payment_source',
+        },
+        {
+          title: 'By Debt Category',
+          dimensionField: 'debt_category',
+        },
+        {
+          title: 'By Payment Agency ID',
+          dimensionField: 'payment_agency_id',
+        },
+        {
+          title: 'By Reversal Requestor',
+          dimensionField: 'reversal_requestor',
+        },
+        {
+          title: 'By Payment Source Code',
+          dimensionField: 'payment_source_cd',
+        },
+      ],
+      // 'Pivot Value' in UI, 'Pivot Value (Field)' on form
+      valueFieldOptions: ['net_offset_amt', 'net_dms_fee_amt', 'offset_amt', 'reversal_amt'],
     },
   },
 };
