78ceea7253078b2d083305d70da8f264b9aac9d5
Nicolle Weidner <nicolle.weidner@kc.frb.org>
2024-07-09 11:59:13 -0500

    FDG-9235 initial stub of reports-section unit tests

--- src/components/published-reports/reports-section/reports-section.spec.js
+++ src/components/published-reports/reports-section/reports-section.spec.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReportsSection from './reports-section';
-import {render} from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 describe('Reports Section component', () => {
   it('renders a Reports and Files header', () => {
     const {getByText} = render(<ReportsSection/>);
     expect(getByText('Reports and Files')).toBeInTheDocument();
-  })
+  });
+
+  it('renders a date picker', () => {
+    const {getByText} = render(<ReportsSection/>);
+    expect(getByText('Published Date')).toBeInTheDocument();
+  });
+
+  it('renders a download table', () => {
+    const {getByTestId} = render(<ReportsSection/>);
+    expect(getByTestId('reportsSectionTable')).toBeInTheDocument();
+  });
 });
