eb7beb4a7edc3f4e40d2fb28b5f9f3e59612e239
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2024-07-11 13:26:54 -0500

    Remove style props

--- src/layouts/explainer/explainer-components/chart-container/chart-container.jsx
+++ src/layouts/explainer/explainer-components/chart-container/chart-container.jsx
@@ -2,27 +2,15 @@ import { chart, chartContainer, headerContainer, footerContainer, chartSubTitle,
 import React from 'react';
 import { format } from 'date-fns';
 
-const ChartContainer = ({
-  title,
-  subTitle = null,
-  altText,
-  header,
-  footer,
-  date,
-  children,
-  customContainerStyles = {},
-  customHeaderStyles = {},
-  customSpacing = {},
-  customTestId = null,
-}) => {
+const ChartContainer = ({ title, subTitle = null, altText, header, footer, date, children, customHeaderStyles = {}, customTestId = null }) => {
   return (
-    <div className={chartContainer} role="figure" aria-label={altText} style={{ ...customSpacing }}>
+    <div className={chartContainer} role="figure" aria-label={altText}>
       <div className={chartTitle}>{title}</div>
       <div className={`${chartSubTitle} chartContainerSubTitle`}>{subTitle}</div>
       <div className={`${headerContainer} chartContainerHeader`} style={{ ...customHeaderStyles }}>
         {header}
       </div>
-      <div data-testid={customTestId ? customTestId : 'chart'} className={`${chart} chartContainerChart`} style={{ ...customContainerStyles }}>
+      <div data-testid={customTestId ? customTestId : 'chart'} className={`${chart} chartContainerChart`}>
         {children}
       </div>
       <div className={`${footerContainer} chartContainerFooter`}>
