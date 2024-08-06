7f04a1c79b4bb74d0f0135089543369bb5b38077
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2024-08-06 13:48:49 -0500

    FDG-9287 fix failing tests

--- src/components/published-reports/month-picker/month-picker-dropdown/month-picker-dropdown.spec.js
+++ src/components/published-reports/month-picker/month-picker-dropdown/month-picker-dropdown.spec.js
@@ -6,7 +6,15 @@ describe('Month Picker', () => {
   const mockDropdownOptions = ['March', 'April', 'May'];
   const mockYearDropdownOptions = ['2020', '2019', 2018];
   it('Renders provided month options as buttons', () => {
-    const { getByRole } = render(<MonthPickerDropdown monthDropdownOptions={mockDropdownOptions} yearDropdownOptions={mockYearDropdownOptions} />);
+    const { getByRole } = render(
+      <MonthPickerDropdown
+        monthDropdownOptions={mockDropdownOptions}
+        yearDropdownOptions={mockYearDropdownOptions}
+        setSelectedDate={jest.fn()}
+        handleClose={jest.fn()}
+        selectedDate="August 2024"
+      />
+    );
     expect(getByRole('button', { name: mockDropdownOptions[0] })).toBeInTheDocument();
     expect(getByRole('button', { name: mockDropdownOptions[1] })).toBeInTheDocument();
     expect(getByRole('button', { name: mockDropdownOptions[2] })).toBeInTheDocument();
@@ -14,7 +22,13 @@ describe('Month Picker', () => {
 
   it('Renders year button', () => {
     const { getByRole, queryByRole } = render(
-      <MonthPickerDropdown monthDropdownOptions={mockDropdownOptions} yearDropdownOptions={mockYearDropdownOptions} />
+      <MonthPickerDropdown
+        monthDropdownOptions={mockDropdownOptions}
+        yearDropdownOptions={mockYearDropdownOptions}
+        setSelectedDate={jest.fn()}
+        handleClose={jest.fn()}
+        selectedDate="August 2024"
+      />
     );
     const yearButton = getByRole('button', { name: 'Open Year Dropdown' });
     expect(yearButton).toBeInTheDocument();
@@ -34,6 +48,7 @@ describe('Month Picker', () => {
         yearDropdownOptions={mockYearDropdownOptions}
         handleClose={mockHandleCloseFn}
         setSelectedDate={mockSetSelectedDateFn}
+        selectedDate="August 2024"
       />
     );
     act(() => {
@@ -53,6 +68,7 @@ describe('Month Picker', () => {
         yearDropdownOptions={mockYearDropdownOptions}
         handleClose={mockHandleCloseFn}
         setSelectedDate={mockSetSelectedDateFn}
+        selectedDate="August 2024"
       />
     );
     act(() => {
