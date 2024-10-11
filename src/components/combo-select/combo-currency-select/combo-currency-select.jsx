304684d4fb0eafe66435e63fae957908218f5186
Wyatt Chandler <wyatt.chandler@kc.frb.org>
2024-10-11 10:59:35 -0500

    FDG-9638 removed unused import

--- src/components/combo-select/combo-currency-select/combo-currency-select.jsx
+++ src/components/combo-select/combo-currency-select/combo-currency-select.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState } from 'react';
 import { selector_label } from '../../select-control/select-control.module.scss';
 import useOnClickOutside from 'use-onclickoutside';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
