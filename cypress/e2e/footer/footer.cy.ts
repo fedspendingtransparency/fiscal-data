d9962ee17820d959dd4c72889db3210a65dc27c2
chasls2 <110061741+chasls2@users.noreply.github.com>
2024-10-16 13:03:02 -0500

    FDG-9096

--- cypress/e2e/footer/footer.cy.ts
+++ cypress/e2e/footer/footer.cy.ts
@@ -41,8 +41,7 @@ describe('Validate footer links ', () => {
 
   it('Validates Subscribe to Our Mailing list link', () => {
     cy.visit('/');
-    cy.get('[data-testid="accessibility-link"]').click();
-    cy.url().should('include', '/accessibility.html');
+    cy.get('[data-testid="accessibility-link"]').should('have.attr', 'href', 'https://fiscal.treasury.gov/accessibility.html');
   });
   it('Validates Subscribe to Our Mailing list link', () => {
     cy.visit('/');
