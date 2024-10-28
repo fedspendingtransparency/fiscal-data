59eeef4864558cb0bc8211579fcf0fea67da720e
chasls2 <110061741+chasls2@users.noreply.github.com>
2024-10-28 10:19:09 -0500

    FDG-9066 Final update

--- cypress/e2e/dataset-detail-page/ddp-sub-nav.cy.ts
+++ cypress/e2e/dataset-detail-page/ddp-sub-nav.cy.ts
@@ -1,39 +1,44 @@
 describe('Homepage user flow validation', () => {
   it('Validates the DDP page loads', () => {
-    cy.visit('/datasets/').wait(4500);
+    cy.visit('/datasets/monthly-treasury-statement/summary-of-receipts-outlays-and-the-deficit-surplus-of-the-u-s-government').wait(3000);
 
-    cy.findByLabelText('Enter search terms')
-      .should('be.visible')
-      .type('MTS');
-    cy.findByText('Monthly Treasury Statement (MTS)')
-      .should('be.visible')
-      .click();
-    cy.url().should('include', '/monthly-treasury-statement/');
+    cy.findByLabelText('Jump to Introduction section').should('be.visible');
   });
 
   it('Validates Introduction jumps to the introduction part of the page', () => {
-    cy.visit('/datasets/monthly-treasury-statement/summary-of-receipts-outlays-and-the-deficit-surplus-of-the-u-s-government/').wait(4500);
+    cy.visit('/datasets/monthly-treasury-statement/summary-of-receipts-outlays-and-the-deficit-surplus-of-the-u-s-government/').wait(3000);
     cy.findByLabelText('Jump to Introduction section').click();
     cy.url().should('include', '#introduction');
+    cy.focused()
+      .should('contain', 'Introduction')
+      .type('{enter}');
+    cy.url().should('include', '#introduction');
   });
-  it('Validates Preview & Download jumps to the introduction part of the page', () => {
-    cy.visit('/datasets/monthly-treasury-statement/summary-of-receipts-outlays-and-the-deficit-surplus-of-the-u-s-government/').wait(4500);
-    cy.findByLabelText('Jump to Introduction section').click();
+  it('Validates Preview & Download jumps to the data Table part of the page', () => {
+    cy.visit('/datasets/monthly-treasury-statement/summary-of-receipts-outlays-and-the-deficit-surplus-of-the-u-s-government/').wait(3000);
+    cy.findByLabelText('Jump to Preview & Download section').click();
+    cy.url().should('include', '#data-table');
+    cy.focused()
+      .should('contain', 'Preview & Download')
+      .type('{enter}');
     cy.url().should('include', '#data-table');
   });
-  it('Validates Dataset Properties jumps to the introduction part of the page', () => {
-    cy.visit('/datasets/monthly-treasury-statement/summary-of-receipts-outlays-and-the-deficit-surplus-of-the-u-s-government/').wait(4500);
-    cy.findByLabelText('Jump to Introduction section').click();
+  it('Validates Dataset Properties jumps to the Data Sets part of the page', () => {
+    cy.visit('/datasets/monthly-treasury-statement/summary-of-receipts-outlays-and-the-deficit-surplus-of-the-u-s-government/').wait(3000);
+    cy.findByLabelText('Jump to Dataset Properties section').click();
+    cy.url().should('include', '#dataset-properties');
+    cy.focused()
+      .should('contain', 'Dataset Properties')
+      .type('{enter}');
     cy.url().should('include', '#dataset-properties');
   });
-  it('Validates Api Quick Guide jumps to the introduction part of the page', () => {
-    cy.visit('/datasets/monthly-treasury-statement/summary-of-receipts-outlays-and-the-deficit-surplus-of-the-u-s-government/').wait(4500);
-    cy.findByLabelText('Jump to Introduction section').click();
+  it('Validates Api Quick Guide jumps to the API Quick Guide part of the page', () => {
+    cy.visit('/datasets/monthly-treasury-statement/summary-of-receipts-outlays-and-the-deficit-surplus-of-the-u-s-government/').wait(3000);
+    cy.findByLabelText('Jump to API Quick Guide section').click();
     cy.url().should('include', '#api-quick-guide');
-  });
-  it('Validates Preview & Download  jumps to the introduction part of the page', () => {
-    cy.visit('/datasets/monthly-treasury-statement/summary-of-receipts-outlays-and-the-deficit-surplus-of-the-u-s-government/').wait(4500);
-    cy.findByLabelText('Jump to Introduction section').click();
-    cy.url().should('include', '/monthly-treasury-statement/');
+    cy.focused()
+      .should('contain', 'API Quick Guide')
+      .type('{enter}');
+    cy.url().should('include', '#api-quick-guide');
   });
 });
