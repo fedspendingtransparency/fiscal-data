f8f617a4376294266bca97a3785e32c509922006
Nicolle Weidner <nicolle.weidner@kc.frb.org>
2024-11-07 13:51:39 -0600

    FDG-9741 add variable typing

--- src/components/data-preview/data-preview.tsx
+++ src/components/data-preview/data-preview.tsx
@@ -2,12 +2,12 @@ import React, { FunctionComponent } from 'react';
 import DatasetSectionContainer from '../dataset-section-container/dataset-section-container';
 
 type DataPreviewProp = {
-  placeholder;
+  placeholder: string;
 };
 
-const DataPreview: FunctionComponent<DataPreviewProp> = ({ placeholder }) => {
+const DataPreview: FunctionComponent<DataPreviewProp> = ({ placeholder: string }) => {
   return (
-    <DatasetSectionContainer>
+    <DatasetSectionContainer id="data-preview" title="">
       <p>Data Preview Placeholder</p>
     </DatasetSectionContainer>
   );
