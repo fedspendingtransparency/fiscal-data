ba3079da581f764e465f09acca917ec5915f5991
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2024-11-13 16:26:19 -0600

    FDG-9747 Create filter components

--- src/components/data-preview/data-preview.tsx
+++ src/components/data-preview/data-preview.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import DatasetSectionContainer from '../dataset-section-container/dataset-section-container';
+import Filters from './filters/filters';
 
 type DataPreviewProp = {
   placeholder: string;
@@ -8,6 +9,7 @@ type DataPreviewProp = {
 const DataPreview: FunctionComponent<DataPreviewProp> = ({ placeholder: string }) => {
   return (
     <DatasetSectionContainer id="data-preview" title="">
+      <Filters />
       <p>Data Preview Placeholder</p>
     </DatasetSectionContainer>
   );
---
+++ src/components/data-preview/filters/filters.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react';
+import DataPreviewTableFilters from './data-preview-table-filters/data-preview-table-filters';
+
+const Filters: FunctionComponent = () => {
+  return (
+    <>
+      <DataPreviewTableFilters />
+    </>
+  );
+};
+
+export default Filters;
