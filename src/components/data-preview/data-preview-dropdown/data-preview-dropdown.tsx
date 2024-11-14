7b01607a802964f15d1c6db007ca44ac620c6965
chasls2 <110061741+chasls2@users.noreply.github.com>
2024-11-14 13:29:46 -0600

    FDG-9742 Create Data Preview Header

---
+++ src/components/data-preview/data-preview-dropdown/data-preview-dropdown.tsx
@@ -0,0 +1,25 @@
+import React, { FunctionComponent, useState } from 'react';
+import { faDatabase } from '@fortawesome/free-solid-svg-icons';
+import { dataPreview, dataPreviewHeader, dataPreviewTitle } from './data-preview-dropdown.module.scss';
+import DropdownLabelButton from './../../dropdown-label-button/dropdown-label-button';
+import DropdownContainer from '../../dropdown-container/dropdown-container';
+
+type DataPreviewProp = {
+  tableName: string;
+};
+
+const DataPreviewDropdown: FunctionComponent<DataPreviewProp> = ({ tableName }) => {
+  const [active, setActive] = useState(true);
+
+  const dropdownButton = (
+    <DropdownLabelButton label={'Data Table'} icon={faDatabase} selectedOption={tableName} active={active} setActive={setActive} />
+  );
+
+  return (
+    <DropdownContainer dropdownButton={dropdownButton} setActive={setActive} active={active}>
+      {/* DROP DOWN CONTAINER GOES HERE*/}
+    </DropdownContainer>
+  );
+};
+
+export default DataPreviewDropdown;
