72a521263656c3be2fe3ba6f1b2a3f1a5d6a7531
Nicolle Weidner <nicolle.weidner@kc.frb.org>
2024-11-19 14:11:15 -0600

    FDG-9759 remove commented type

--- src/components/data-table/data-table.tsx
+++ src/components/data-table/data-table.tsx
@@ -32,45 +32,6 @@ import {
 import { useSetRecoilState } from 'recoil';
 import { IDataTableProps } from '../../models/IDataTableProps';
 
-// type DataTableProps = {
-//   // defaultSelectedColumns will be null unless the dataset has default columns specified in the dataset config
-//   rawData;
-//   nonRawDataColumns?;
-//   defaultSelectedColumns: string[];
-//   setTableColumnSortData;
-//   hasPublishedReports: boolean;
-//   publishedReports;
-//   hideCellLinks: boolean;
-//   resetFilters: boolean;
-//   shouldPage: boolean;
-//   showPaginationControls: boolean;
-//   setSelectColumnPanel;
-//   selectColumnPanel;
-//   setResetFilters: (value: boolean) => void;
-//   tableName: string;
-//   hideColumns?: string[];
-//   pagingProps;
-//   manualPagination: boolean;
-//   rowsShowing: { begin: number; end: number };
-//   columnConfig?;
-//   detailColumnConfig?;
-//   detailView?;
-//   detailViewAPI?;
-//   setDetailViewState?: (val: { value?: string; secondary?: string }) => void;
-//   detailViewState?: { value?: string; secondary?: string };
-//   allowColumnWrap?: string[];
-//   aria;
-//   pivotSelected;
-//   setSummaryValues?;
-//   customFormatting?;
-//   sorting: SortingState;
-//   setSorting: (value: SortingState) => void;
-//   allActiveFilters: string[];
-//   setAllActiveFilters: (value: string[]) => void;
-//   setTableSorting?: (value: SortingState) => void;
-//   disableDateRangeFilter: boolean;
-// };
-
 const DataTable: FunctionComponent<IDataTableProps> = ({
   rawData,
   nonRawDataColumns,
