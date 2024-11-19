00796b16128e02a346aa97eacbec65459be91caa
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2024-11-19 14:31:12 -0600

    FDG-9747 merge

--- src/components/data-preview/data-preview-filter-section/data-preview-filter-section.tsx
+++ src/components/data-preview/data-preview-filter-section/data-preview-filter-section.tsx
@@ -1,16 +1,6 @@
-import { FunctionComponent, ReactElement, useEffect, useState } from 'react';
-import determineDateRange, {
-  generateAnalyticsEvent,
-  generateFormattedDate,
-  prepAvailableDates,
-} from '../../filter-download-container/range-presets/helpers/helper';
-import { addDays, differenceInYears, subQuarters } from 'date-fns';
-import { monthNames } from '../../../utils/api-utils';
-import { fitDateRangeToTable } from '../../filter-download-container/range-presets/range-presets';
+import { FunctionComponent, ReactElement } from 'react';
 import React from 'react';
-import DownloadWrapper from '../../download-wrapper/download-wrapper';
 import DataPreviewDownload from './data-preview-download/data-preview-download';
-import DateRangeFilter from './date-range-filter/date-range-filter';
 import { pxToNumber } from '../../../helpers/styles-helper/styles-helper';
 import { filterAndDownloadContainer, filterContainer } from './data-preview-filter-section.module.scss';
 import DataPreviewTableFilters from './data-preview-table-filters/data-preview-table-filters';
@@ -20,12 +10,13 @@ import { withWindowSize } from 'react-fns';
 
 type DataPreviewFilterSectionProps = {
   width?: number;
-  children: ReactElement | string;  dateRange;
+  children: ReactElement | string;
+  dateRange;
   isFiltered;
   selectedTable;
   dataset;
-  allTablesSelected;
-  isCustomDateRange;
+  allTablesSelected: boolean;
+  isCustomDateRange: boolean;
   selectedUserFilter;
   tableColumnSortData;
   filteredDateRange;
@@ -53,7 +44,25 @@ const DataPreviewFilterSection: FunctionComponent<DataPreviewFilterSectionProps>
           <DataPreviewTableFilters />
           <ColumnFilter />
         </div>
-        {width >= pxToNumber(breakpointLg) && <DataPreviewDownload
+        {width >= pxToNumber(breakpointLg) && (
+          <DataPreviewDownload
+            width={width}
+            dateRange={dateRange}
+            isFiltered={isFiltered}
+            selectedTable={selectedTable}
+            dataset={dataset}
+            allTablesSelected={allTablesSelected}
+            isCustomDateRange={isCustomDateRange}
+            selectedUserFilter={selectedUserFilter}
+            tableColumnSortData={tableColumnSortData}
+            filteredDateRange={filteredDateRange}
+            selectedDetailViewFilter={selectedDetailViewFilter}
+          />
+        )}
+      </div>
+      {children}
+      {width < pxToNumber(breakpointLg) && (
+        <DataPreviewDownload
           width={width}
           dateRange={dateRange}
           isFiltered={isFiltered}
@@ -65,23 +74,8 @@ const DataPreviewFilterSection: FunctionComponent<DataPreviewFilterSectionProps>
           tableColumnSortData={tableColumnSortData}
           filteredDateRange={filteredDateRange}
           selectedDetailViewFilter={selectedDetailViewFilter}
-        />}
-      </div>
-      {children}
-      {width < pxToNumber(breakpointLg) &&       <DataPreviewDownload
-        width={width}
-        dateRange={dateRange}
-        isFiltered={isFiltered}
-        selectedTable={selectedTable}
-        dataset={dataset}
-        allTablesSelected={allTablesSelected}
-        isCustomDateRange={isCustomDateRange}
-        selectedUserFilter={selectedUserFilter}
-        tableColumnSortData={tableColumnSortData}
-        filteredDateRange={filteredDateRange}
-        selectedDetailViewFilter={selectedDetailViewFilter}
-      />}
-
+        />
+      )}
     </>
   );
 };
