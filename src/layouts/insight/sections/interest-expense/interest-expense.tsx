43038b1ee47233f1ab4dac094c2088c2308ad24c
Wyatt Chandler <wyatt.chandler@kc.frb.org>
2024-11-20 14:11:23 -0600

    FDG-9796 added DS&M copy

--- src/layouts/insight/sections/interest-expense/interest-expense.tsx
+++ src/layouts/insight/sections/interest-expense/interest-expense.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { InterestExpenseChart } from './interest-expense-chart/interest-expense-chart';
 import { BodyCopy } from './body-copy/body-copy';
+import CustomLink from '../../../../components/links/custom-link/custom-link';
 
 export const interestExpenseDataSources = (
   <>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
-    id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-    quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur
-    aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-    quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-    voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-    Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-    voluptas nulla pariatur?
+    Visit the{' '}
+    <CustomLink url={'/datasets/interest-expense-debt-outstanding/interest-expense-on-the-public-debt-outstanding'}>
+      Interest Expense on the Debt Outstanding
+    </CustomLink>{' '}
+    and{' '}
+    <CustomLink url={'/datasets/interest-expense-debt-outstanding/interest-expense-on-the-public-debt-outstanding'}>
+      Average Interest Rates on U.S. Treasury Securities
+    </CustomLink>{' '}
+    datasets to explore and download this data. The interest expense reflects the sum of all interest paid on the debt for each fiscal year or through
+    the latest month of the latest fiscal year. The average interest rate is the average of all interest rates on outstanding securities for the last
+    month of each fiscal year or the most recent month with available data. Because this rate is an average of rates paid on various security types,
+    the interest expense cannot be determined by multiplying the average interest rate by the outstanding debt.
   </>
 );
 
