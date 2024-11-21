5be8bf034ec0a14cf46136e6195cf9c340527a88
Nicolle Weidner <nicolle.weidner@kc.frb.org>
2024-11-21 13:58:22 -0600

    FDG-9795 initial add of definition to body

--- src/layouts/insight/insight.tsx
+++ src/layouts/insight/insight.tsx
@@ -17,6 +17,7 @@ import { insightsDataSources, insightsSections } from './sections/sections';
 import { withWindowSize } from 'react-fns';
 import { pxToNumber } from '../../helpers/styles-helper/styles-helper';
 import { breakpointLg } from '../../variables.module.scss';
+import GlossaryProvider from '../../components/glossary/glossary-context/glossary-context';
 
 interface IInsightSection {
   component: ReactElement;
@@ -27,35 +28,37 @@ const InsightPageLayout = ({ pageContext, width }) => {
   const { pageName, seoConfig, heroImage } = pageContext;
 
   return (
-    <SiteLayout isPreProd={false}>
-      <PageHelmet pageTitle={seoConfig.pageTitle} description={seoConfig.description} keywords={seoConfig.keywords} />
-      <div className={insightsContainer}>
-        <h1 className={sectionHeading}>{heroImage.heading}</h1>
-        {width < pxToNumber(breakpointLg) && (
-          <SocialShare copy={insightSocialShareMap[pageName]} pageName="" headerLevel="h2" displayStyle="responsive" />
-        )}
-        <span className={lastUpdated}>Last Updated: Month DD, YYYY</span>
-        <div className={contentContainer}>
-          <div className={mainContent}>
-            {insightsSections[pageName]?.map((section: IInsightSection) => (
-              <React.Fragment key={section.index}>
-                <section className={sectionContainer}>{section.component}</section>
-              </React.Fragment>
-            ))}
-            <div>
-              <h2>Data Sources and Methodologies:</h2> {insightsDataSources[pageName]}
+    <GlossaryProvider>
+      <SiteLayout isPreProd={false}>
+        <PageHelmet pageTitle={seoConfig.pageTitle} description={seoConfig.description} keywords={seoConfig.keywords} />
+        <div className={insightsContainer}>
+          <h1 className={sectionHeading}>{heroImage.heading}</h1>
+          {width < pxToNumber(breakpointLg) && (
+            <SocialShare copy={insightSocialShareMap[pageName]} pageName="" headerLevel="h2" displayStyle="responsive" />
+          )}
+          <span className={lastUpdated}>Last Updated: Month DD, YYYY</span>
+          <div className={contentContainer}>
+            <div className={mainContent}>
+              {insightsSections[pageName]?.map((section: IInsightSection) => (
+                <React.Fragment key={section.index}>
+                  <section className={sectionContainer}>{section.component}</section>
+                </React.Fragment>
+              ))}
+              <div>
+                <h2>Data Sources and Methodologies:</h2> {insightsDataSources[pageName]}
+              </div>
+            </div>
+            <div className={relatedContent}>
+              {width >= pxToNumber(breakpointLg) && (
+                <SocialShare copy={insightSocialShareMap[pageName]} pageName="" headerLevel="h2" displayStyle="responsive" />
+              )}
+              <CitationList header="Explore More" citations={exploreMoreCitationsMap[pageName]} />
+              <CitationList header="Discover Datasets" citations={discoverDatasetsCitationsMap[pageName]} />
             </div>
           </div>
-          <div className={relatedContent}>
-            {width >= pxToNumber(breakpointLg) && (
-              <SocialShare copy={insightSocialShareMap[pageName]} pageName="" headerLevel="h2" displayStyle="responsive" />
-            )}
-            <CitationList header="Explore More" citations={exploreMoreCitationsMap[pageName]} />
-            <CitationList header="Discover Datasets" citations={discoverDatasetsCitationsMap[pageName]} />
-          </div>
         </div>
-      </div>
-    </SiteLayout>
+      </SiteLayout>
+    </GlossaryProvider>
   );
 };
 
