d52342910451ffffd6cc3e93a3e4a6e4c4c2fa10
Nicolle Weidner <nicolle.weidner@kc.frb.org>
2024-11-25 14:23:07 -0600

    FDG-9100 add null/Nan/Undefined and section tests

--- cypress/e2e/explainers/deficit.cy.ts
+++ cypress/e2e/explainers/deficit.cy.ts
@@ -1,5 +1,46 @@
 describe('Deficit Explainer Page', () => {
-  it('ensure no tiles page tiles contain NaN, null, or undefined values', () => {
-    cy.visit('/');
+  beforeEach(() => {
+    cy.visit('/americas-finance-guide/national-deficit/');
   });
+
+  it('Navigate to the deficit explainer, ensure pageensure page does not contain NaN, null, or undefined values', () => {
+    cy.findAllByText('null').should('not.exist');
+    cy.findAllByText('NaN').should('not.exist');
+    cy.findAllByText('undefined').should('not.exist');
+
+    // also an option to get everything in the body
+    // cy.get('body').then(body => {
+    //   cy.contains('null').should('not.exist');
+    //   cy.contains('NaN').should('not.exist');
+    //   cy.contains('undefined').should('not.exist');
+    // });
+  });
+
+  describe('Validate that the sub nav takes the user to the correct section on the page', () => {
+    it('Validate that the sub nav takes the user Overview section', () => {
+      cy.findByRole('link', { name: 'Overview' }).click();
+      cy.url().should('include', 'americas-finance-guide/');
+    });
+
+    it('Validate that the sub nav takes the user Revenue section', () => {
+      cy.findByRole('link', { name: 'Revenue' }).click();
+      cy.url().should('include', 'americas-finance-guide/government-revenue');
+    });
+
+    it('Validate that the sub nav takes the user Spending section', () => {
+      cy.findByRole('link', { name: 'Spending' }).click();
+      cy.url().should('include', 'americas-finance-guide/federal-spending');
+    });
+
+    it('Validate that the sub nav takes the user Debt section', () => {
+      cy.findByRole('link', { name: 'Debt' }).click();
+      cy.url().should('include', 'americas-finance-guide/national-debt');
+    });
+  });
+
+  it('Validate all links on the page navigate to the correct destinations', () => {});
+  it('Validate all glossary terms on page', () => {});
+  it('Validate that the related datasets section contains the correct datasets', () => {});
+
+  describe('Validate charts', () => {});
 });
