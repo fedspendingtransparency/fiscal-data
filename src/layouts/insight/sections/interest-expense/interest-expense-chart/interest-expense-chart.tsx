85f45816c7613b027d44c5884b5c3aa0efb5ff9f
Wyatt Chandler <wyatt.chandler@kc.frb.org>
2024-11-25 15:14:03 -0600

    FDG-9794 all axis values are now dynamic

--- src/layouts/insight/sections/interest-expense/interest-expense-chart/interest-expense-chart.tsx
+++ src/layouts/insight/sections/interest-expense/interest-expense-chart/interest-expense-chart.tsx
@@ -7,6 +7,8 @@ import { getShortForm } from '../../../../../utils/rounding-utils';
 import ChartDataHeader from '../../../../explainer/explainer-components/chart-data-header/chart-data-header';
 import { useWindowSize } from '../../../../../hooks/windowResize';
 import { useGetInterestExpenseData } from '../useGetInterestExpenseData';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const breakpoint = {
   desktop: 1015,
@@ -15,7 +17,7 @@ const breakpoint = {
 
 export const InterestExpenseChart = () => {
   const [width, height] = useWindowSize();
-  const { chartData, currentFY } = useGetInterestExpenseData();
+  const { chartData, currentFY, chartXAxisValues, chartLoading } = useGetInterestExpenseData();
   const [fiscalYear, setFiscalYear] = useState(2025);
   const [curExpenseAmount, setCurExpenseAmount] = useState(890000000000);
   const [curRate, setCurRate] = useState(3.8);
@@ -29,7 +31,6 @@ export const InterestExpenseChart = () => {
   }>({ year: 2025, expense: 890000000000, rate: 3.8 });
 
   const defaultRateAxis: number[] = [0, 1, 2, 3, 4];
-  const defaultExpenseAxis: number[] = [0, 300000000000, 600000000000, 900000000000, 1200000000000];
 
   const resetDataHeader = () => {
     if (latestData) {
@@ -37,6 +38,28 @@ export const InterestExpenseChart = () => {
       setCurExpenseAmount(latestData.expense);
       setCurRate(latestData.rate);
     }
+  };
+
+  const generateExpenseValueTicks = (): number[] => {
+    const expenseValues = chartData.map(element => element.expense);
+    const max = Math.max(...expenseValues);
+    const top = Math.round(max / 300000000000) * 300000000000;
+    const ticks = [];
+    for (let i = 0; i <= top; i += 300000000000) {
+      ticks.push(i);
+    }
+    return ticks;
+  };
+
+  const generateInterestRateTicks = (): number[] => {
+    const rateValues = chartData.map(element => element.rate);
+    const max = Math.max(...rateValues);
+    const top = Math.ceil(max);
+    const ticks = [];
+    for (let i = 0; i <= top; i++) {
+      ticks.push(i);
+    }
+    return ticks;
   };
 
   useEffect(() => {
@@ -49,64 +72,93 @@ export const InterestExpenseChart = () => {
 
   return (
     <div>
-      <div>
-        <ChartDataHeader
-          dateField={fiscalYear === latestData.year ? 'FYTD' : 'Fiscal Year'}
-          fiscalYear={fiscalYear}
-          right={{ label: 'Interest Expense', value: `$${getShortForm(curExpenseAmount.toString())}` }}
-          left={{ label: 'Avg. Interest Rate', value: `${curRate}%` }}
-        />
-      </div>
-      <div data-testid="chartParent">
-        <Legend />
-        <div
-          role="presentation"
-          onBlur={() => {
-            setChartFocus(false);
-            resetDataHeader();
-          }}
-          onFocus={() => setChartFocus(true)}
-          onMouseOver={() => setChartHover(true)}
-          onMouseLeave={() => setChartHover(false)}
-        >
-          <ResponsiveContainer height={360} width={'99%'}>
-            <ComposedChart data={chartData} margin={{ top: 12, bottom: -8, left: 3, right: -18 }} accessibilityLayer onMouseLeave={resetDataHeader}>
-              <CartesianGrid vertical={false} stroke="#d9d9d9" />
-              <XAxis dataKey={'year'} ticks={[2010, 2013, 2016, 2019, 2022, 2024]} />
-              <YAxis
-                dataKey={'expense'}
-                tickFormatter={value => (value === 0 ? '$0' : `$${getShortForm(value)}`)}
-                axisLine={false}
-                tickLine={false}
-                ticks={defaultExpenseAxis}
-                tick={{ fill: interestExpensePrimary }}
-              />
-              <YAxis
-                yAxisId={1}
-                dataKey={'rate'}
-                orientation={'right'}
-                axisLine={false}
-                tickLine={false}
-                type="number"
-                tickCount={5}
-                ticks={defaultRateAxis}
-                tickFormatter={value => `${value.toFixed(1)}%`}
-              />
-              <Tooltip
-                cursor={{
-                  stroke: '#00796B20',
-                  strokeWidth: isMobile ? 16 : 32,
-                }}
-                content={<CustomTooltip setYear={setFiscalYear} setExpense={setCurExpenseAmount} setRate={setCurRate} />}
-                isAnimationActive={false}
-                active={chartFocus || chartHover}
-              />
-              <Bar dataKey={'expense'} barSize={isMobile ? 12 : 20} fill={interestExpensePrimary} />
-              <Line dataKey={'rate'} yAxisId={1} stroke={'#666666'} type={'monotone'} strokeWidth={1} activeDot={false} />
-            </ComposedChart>
-          </ResponsiveContainer>
+      {chartLoading ? (
+        <div>
+          <FontAwesomeIcon icon={faSpinner} spin pulse /> Loading...
         </div>
-      </div>
+      ) : (
+        <>
+          <div>
+            <ChartDataHeader
+              dateField={fiscalYear === latestData.year ? 'FYTD' : 'Fiscal Year'}
+              fiscalYear={fiscalYear}
+              right={{ label: 'Interest Expense', value: `$${getShortForm(curExpenseAmount.toString())}` }}
+              left={{ label: 'Avg. Interest Rate', value: `${curRate}%` }}
+            />
+          </div>
+          <div data-testid="chartParent">
+            <Legend />
+            <div
+              role="presentation"
+              onBlur={() => {
+                setChartFocus(false);
+                resetDataHeader();
+              }}
+              onFocus={() => setChartFocus(true)}
+              onMouseOver={() => setChartHover(true)}
+              onMouseLeave={() => setChartHover(false)}
+            >
+              <ResponsiveContainer height={360} width={'99%'}>
+                <ComposedChart
+                  data={chartData}
+                  margin={{ top: 12, bottom: -8, left: 3, right: -18 }}
+                  accessibilityLayer
+                  onMouseLeave={resetDataHeader}
+                >
+                  <CartesianGrid vertical={false} stroke="#d9d9d9" />
+                  <XAxis dataKey={'year'} ticks={chartXAxisValues} />
+                  <YAxis
+                    dataKey={'expense'}
+                    type={'number'}
+                    tickFormatter={value => {
+                      if (value === 0) {
+                        return '$0';
+                      } else {
+                        return `$${getShortForm(value)}`;
+                      }
+                    }}
+                    axisLine={false}
+                    tickLine={false}
+                    tickCount={5}
+                    ticks={generateExpenseValueTicks()}
+                    tick={{ fill: interestExpensePrimary }}
+                  />
+                  <YAxis
+                    yAxisId={1}
+                    dataKey={'rate'}
+                    orientation={'right'}
+                    axisLine={false}
+                    tickLine={false}
+                    type="number"
+                    tickCount={5}
+                    ticks={generateInterestRateTicks()}
+                    tickFormatter={value => `${value.toFixed(1)}%`}
+                  />
+                  <Tooltip
+                    cursor={{
+                      stroke: '#00796B20',
+                      strokeWidth: isMobile ? 16 : 32,
+                    }}
+                    content={<CustomTooltip setYear={setFiscalYear} setExpense={setCurExpenseAmount} setRate={setCurRate} />}
+                    isAnimationActive={false}
+                    active={chartFocus || chartHover}
+                  />
+                  <Bar dataKey={'expense'} barSize={isMobile ? 12 : 20} fill={interestExpensePrimary} isAnimationActive={false} />
+                  <Line
+                    dataKey={'rate'}
+                    yAxisId={1}
+                    stroke={'#666666'}
+                    type={'monotone'}
+                    strokeWidth={1}
+                    activeDot={false}
+                    isAnimationActive={false}
+                  />
+                </ComposedChart>
+              </ResponsiveContainer>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/layouts/insight/sections/interest-expense/useGetInterestExpenseData.ts
+++ src/layouts/insight/sections/interest-expense/useGetInterestExpenseData.ts
@@ -13,6 +13,8 @@ export const useGetInterestExpenseData = () => {
   const [startFY, setStartFY] = useState(null);
   const [currentFY, setCurrentFY] = useState(null);
   const [chartData, setChartData] = useState(null);
+  const [chartLoading, setChartLoading] = useState<boolean>(true);
+  const [chartXAxisValues, setChartXAxisValues] = useState<number[]>(null);
 
   useEffect(() => {
     const gatherData = async () => {
@@ -34,46 +36,64 @@ export const useGetInterestExpenseData = () => {
       // Expense Amount Chart Data
       await basicFetch(
         `${apiPrefix}v2/accounting/od/interest_expense?sort=-record_date&filter=record_fiscal_year:gte:${start}&page[size]=10000`
-      ).then(res => {
-        const groupedExpenseDataByFY = groupByProperty(res.data, 'record_fiscal_year');
+      ).then(res1 => {
+        const groupedExpenseDataByFY = groupByProperty(res1.data, 'record_fiscal_year');
         for (const year in groupedExpenseDataByFY) {
           const yearData = groupedExpenseDataByFY[year];
           if (year !== current) {
             const sum = yearData
               .filter(element => element.record_calendar_month === '09')
               .reduce((a, { fytd_expense_amt }) => a + parseInt(fytd_expense_amt), 0);
-            chartData.push({ year: year, expense: sum });
+            chartData.push({ year: parseInt(year), expense: sum });
           } else {
             const currentFYSum = yearData
               .filter(element => element.record_calendar_month === interestExpMonth && element.record_fiscal_year === current)
               .reduce((a, { fytd_expense_amt }) => a + parseInt(fytd_expense_amt), 0);
-            chartData.push({ year: year, expense: currentFYSum });
+            chartData.push({ year: parseInt(year), expense: currentFYSum });
           }
         }
         // Interest rate chart data
         basicFetch(
           `${apiPrefix}v2/accounting/od/avg_interest_rates?sort=-record_date&filter=security_desc:eq:Total%20Interest-bearing%20Debt,record_fiscal_year:gte:${start}&page[size]=300`
-        ).then(res => {
-          const groupedInterestRateByFY = groupByProperty(res.data, 'record_fiscal_year');
+        ).then(res2 => {
+          const groupedInterestRateByFY = groupByProperty(res2.data, 'record_fiscal_year');
           for (const year in groupedInterestRateByFY) {
             const yearData = groupedInterestRateByFY[year];
             if (year !== current) {
               const rate = yearData.filter(element => element.record_calendar_month === '09');
-              const index = chartData.findIndex(element => element.year === rate[0].record_fiscal_year);
+              const index = chartData.findIndex(element => element.year === parseInt(rate[0].record_fiscal_year));
               chartData[index].rate = parseFloat(rate[0].avg_interest_rate_amt);
             } else {
               const currentRate = yearData.filter(
                 element => element.record_calendar_month === interestExpMonth && element.record_fiscal_year === current
               );
-              const index = chartData.findIndex(element => element.year === currentRate[0].record_fiscal_year);
+              const index = chartData.findIndex(element => element.year === parseInt(currentRate[0].record_fiscal_year));
               chartData[index].rate = parseFloat(currentRate[0].avg_interest_rate_amt);
             }
           }
-          setChartData(chartData);
+          if (res1.data[0].record_calendar_month === res2.data[0].record_calendar_month) {
+            setChartData(chartData);
+            // Chart axis values
+            // Generate X Axis Values
+            const years = [];
+            chartData.forEach((element, index) => {
+              if (index === 0) {
+                years.push(element.year);
+              } else {
+                if (index % 3 === 0) {
+                  years.push(element.year);
+                } else if (element.year === parseInt(current) && !years.includes(element.year)) {
+                  years.push(element.year);
+                }
+              }
+            });
+            setChartXAxisValues(years);
+            setChartLoading(false);
+          }
         });
       });
     };
     gatherData();
   }, []);
-  return { startFY, currentFY, chartData };
+  return { startFY, currentFY, chartData, chartXAxisValues, chartLoading };
 };
