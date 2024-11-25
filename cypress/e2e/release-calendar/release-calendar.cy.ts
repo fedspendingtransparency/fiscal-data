574b7902d0956519601986ad38c0b3256435089e
chasls2 <110061741+chasls2@users.noreply.github.com>
2024-11-25 09:47:36 -0600

    FDG-9100 Cypress fix

---
+++ cypress/e2e/release-calendar/release-calendar.cy.ts
@@ -0,0 +1,12 @@
+// describe('Release calendar interaction flow', () => {
+//   beforeEach(() => {
+//     cy.visit('/release-calendar/').wait(3000);
+//   });
+//   it('Navigate to the release calendar page', () => {
+//     cy.visit('/');
+
+//     cy.findByRole('link', { name: 'Dataset Search' }).then($link => {
+//       cy.log($link.prop('outerHTML'));
+//     });
+//   });
+// });
