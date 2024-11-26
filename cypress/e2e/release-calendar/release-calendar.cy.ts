ad97837f96ea8325b0b462f2097a5561be68449c
chasls2 <110061741+chasls2@users.noreply.github.com>
2024-11-26 08:53:07 -0600

    FDG-9097

--- cypress/e2e/release-calendar/release-calendar.cy.ts
+++ cypress/e2e/release-calendar/release-calendar.cy.ts
@@ -1,39 +1,31 @@
 describe('Navigate to the release calendar page', () => {
-
   it('Navigate to the release calendar page', () => {
-   
     cy.visit('/');
-    cy.findByLabelText('Page links for Resources').click(); 
+    cy.findByLabelText('Page links for Resources').click();
     cy.contains('Release Calendar').click();
     cy.url().should('include', '/release-calendar/');
-    });
   });
-
+});
 
 describe('Release calendar interaction flow', () => {
   beforeEach(() => {
     cy.visit('/release-calendar/');
   });
   it('Verifies sorting by name', () => {
-   
     cy.findAllByText('Date').click();
     cy.findAllByText('Name').click();
     cy.findByLabelText('Change Sort By: from Name').should('exist');
     cy.findAllByText('#').should('exist');
     cy.findAllByText('A').should('exist');
-    });
+  });
   it('Verifies clicking on calender entry goes to correct page', () => {
-      cy.findAllByText('Date').click();
-      cy.findAllByText('Name').click();
+    cy.findAllByText('Date').click();
+    cy.findAllByText('Name').click();
 
-      cy.findAllByText('120 Day Delinquent Debt Referral Compliance Report').click();
-      cy.url().should('include', '/120-day-delinquent-debt-referral-compliance-report');
-    });
+    cy.findAllByText('120 Day Delinquent Debt Referral Compliance Report').click();
+    cy.url().should('include', '/120-day-delinquent-debt-referral-compliance-report');
+  });
   it('Verifies sorting by date', () => {
-      cy.findByLabelText('Change Sort By: from Date').should('exist');
-
+    cy.findByLabelText('Change Sort By: from Date').should('exist');
   });
-
-  });
-
-
+});
