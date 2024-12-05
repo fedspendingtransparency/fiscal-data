12cfd1c4f81381871bdc760a9ea763903227b77b
Nicolle Weidner <nicolle.weidner@kc.frb.org>
2024-12-05 14:11:01 -0600

    FDG-9090 wip

--- cypress/e2e/dataset-detail-page/dataset-properties.cy.ts
+++ cypress/e2e/dataset-detail-page/dataset-properties.cy.ts
@@ -1,3 +1,13 @@
 describe('Dataset Properties', () => {
-  it('Navigate to the dataset page, click between the data dictionary, data tables, metadata, and notes & known limitations tabs and verify that content loads for each tab', () => {});
+  beforeEach((), => {
+    cy.visit('/datasets/daily-treasury-statement/');
+  });
+
+  it('Navigate to the dataset page, click between the data dictionary, data tables, metadata, and notes & known limitations tabs and verify that content loads for each tab', () => {
+    const propertyTabs = cy.findAllByRole('tab');
+
+    propertyTabs.forEach(tab => {
+
+    });
+  });
 });
