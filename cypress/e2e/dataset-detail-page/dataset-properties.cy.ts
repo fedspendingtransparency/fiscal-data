c13c5a654fe18220acefbf1392f070138f1608fb
Nicolle Weidner <nicolle.weidner@kc.frb.org>
2024-12-05 10:43:11 -0600

    FDG-9090 add initial file

---
+++ cypress/e2e/dataset-detail-page/dataset-properties.cy.ts
@@ -0,0 +1,3 @@
+describe('Dataset Properties', () => {
+  it('Navigate to the dataset page, click between the data dictionary, data tables, metadata, and notes & known limitations tabs and verify that content loads for each tab', () => {});
+});
