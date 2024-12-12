fd7dc937a2b567082f5a9b0e7fed167deb91db46
chasls2 <110061741+chasls2@users.noreply.github.com>
2024-12-12 11:51:52 -0600

    FDG-9744

---
+++ src/components/data-preview/data-preview-dropdown-search/data-preview-dropdown-dialog-search.tsx
@@ -0,0 +1,43 @@
+import React, { FunctionComponent, useState } from 'react';
+import { dataTableSearchContainer } from './data-preview-dropdown-dialog-search.spec';
+import ComboSelectDropdown from '../../combo-select/combo-currency-select/combo-select-dropdown/combo-select-dropdown';
+interface ButtonData {
+  label: string;
+  subtitle?: string;
+  onClick: () => void;
+  type: 'dataTable' | 'searchFilter';
+}
+
+interface Props {
+  options: ButtonData[];
+  searchBarLabel: string;
+  selectedTable: any;
+  setSelectedTable: any;
+  active: boolean;
+}
+
+const DataPreviewDropdownDialogSearch: FunctionComponent<Props> = ({ selectedTable, setSelectedTable, options, searchBarLabel, active }) => {
+  const [actives, setActive] = useState(active);
+  const [searchBarActive, setSearchBarActive] = useState(false);
+  const handleSearchChange = (option: ButtonData) => {
+    setSelectedTable(option);
+  };
+
+  return (
+    <div className={dataTableSearchContainer}>
+      <ComboSelectDropdown
+        active={actives}
+        setDropdownActive={setActive}
+        selectedOption={selectedTable}
+        updateSelection={handleSearchChange}
+        searchBarLabel={searchBarLabel}
+        options={options}
+        optionLabelKey="tableName"
+        searchBarActive={searchBarActive}
+        setSearchBarActive={setSearchBarActive}
+      />
+    </div>
+  );
+};
+
+export default DataPreviewDropdownDialogSearch;
