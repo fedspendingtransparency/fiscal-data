4d9ed797e6cd7e3021b550c6073bd40bc849276f
Wyatt Chandler <wyatt.chandler@kc.frb.org>
2024-12-16 12:34:03 -0600

    FDG-9758 initial styling & mobile implementation

--- src/components/download-dialog/download-dialog.tsx
+++ src/components/download-dialog/download-dialog.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 
-export const DownloadDialog = () => {
+import { container, item, border } from './download-dialog.module.scss';
+
+export const DownloadDialog: FunctionComponent = () => {
   return (
-    <div>
-      <div>
+    <div className={container}>
+      <div className={item} role={'button'}>
         <span>CSV</span>
         <span>84 KB</span>
       </div>
-      <div>
+      <div className={item} role={'button'}>
         <span>JSON</span>
         <span>84 KB</span>
       </div>
-      <div>
+      <div className={item} role={'button'}>
         <span>XML</span>
         <span>84 KB</span>
       </div>
-      <div />
-      <div>
-        <span>CSV</span>
-        <span>84 KB</span>
+      <div className={border} />
+      <div className={item} role={'button'}>
+        <span>Data Dictionary</span>
+        <span>24 KB</span>
       </div>
     </div>
   );
