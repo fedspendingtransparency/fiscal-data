09f1b495c9e5f97812548506d6326f248132f5e5
chasls2 <110061741+chasls2@users.noreply.github.com>
2025-01-02 15:05:33 -0600

    FDG-9109

--- cypress/e2e/about-us/about-us-sub-nav.cy.ts
+++ cypress/e2e/about-us/about-us-sub-nav.cy.ts
@@ -12,10 +12,37 @@ describe('About us interaction flow', () => {
       .click();
     cy.url().should('include', '/#why-datasets-go-further-than-others');
   });
+  it('Verifies keyboard interaction about us sub nav interaction flow FAQ', () => {
+    cy.findByTitle('I’m new to using APIs...')
+      .should('exist')
+      .focus()
+      .type('{enter}');
+    cy.url().should('include', '/#new-to-apis');
+  });
   it('Verifies about us sub nav interaction flow About Fiscal Data', () => {
     cy.findByTitle('Background')
       .should('exist')
       .click();
     cy.url().should('include', '/#background');
+  });
+  it('Verifies keyboard interaction about us sub nav interaction flow About Fiscal Data', () => {
+    cy.findByTitle('Mission')
+      .should('exist')
+      .focus()
+      .type('{enter}');
+    cy.url().should('include', '/#mission');
+  });
+  it('Verifies about us sub nav interaction flow Contact Us', () => {
+    cy.findByTitle('Contact Us')
+      .should('exist')
+      .click();
+    cy.url().should('include', '/#contact-us');
+  });
+  it('Verifies keyboard interaction about us sub nav interaction flow About Contact Us', () => {
+    cy.findByTitle('Contact Us')
+      .should('exist')
+      .focus()
+      .type('{enter}');
+    cy.url().should('include', '/#contact-us');
   });
 });
