ffa87242d9102a88cfc99fdb202e558163c758fb
Nicolle Weidner <nicolle.weidner@kc.frb.org>
2025-01-23 11:39:36 -0600

    FDG-9111 add internal and external link tests

--- cypress/e2e/resources/api-documentation.cy.ts
+++ cypress/e2e/resources/api-documentation.cy.ts
@@ -1,6 +1,6 @@
 describe('API Documentation Page', () => {
   beforeEach(() => {
-    cy.visit('/api-documentation');
+    cy.visit('/api-documentation/');
   });
 
   it('Navigate to the api documentation, ensure page does not contain NaN, null, or undefined values', () => {
@@ -9,5 +9,63 @@ describe('API Documentation Page', () => {
     cy.findAllByText('undefined').should('not.exist');
   });
 
-  it('Validate all links', () => {});
+  it('Validate all internal links on the page navigate to the correct destinations', () => {
+    const links = [
+      {
+        name: 'our website',
+        url: '/',
+      },
+      {
+        name: "Your Guide to America's Finances",
+        url: 'americas-finance-guide/',
+      },
+      {
+        name: 'Search and filter',
+        url: 'datasets/',
+      },
+      // {
+      //   name: 'Datasets',
+      //   url: 'datasets/',
+      // },
+      {
+        name: 'full documentation on Aggregation and Sums',
+        url: 'api-documentation/#aggregation-sums',
+      },
+      {
+        name: 'Endpoints by Dataset',
+        url: 'api-documentation/#list-of-endpoints-table',
+      },
+      // {
+      //   name: 'Datasets',
+      //   url: 'datasets/',
+      // },
+      {
+        name: 'Format',
+        url: 'api-documentation/#parameters-format',
+      },
+    ];
+
+    links.forEach(link => {
+      cy.findByRole('link', { name: link.name }).click();
+      cy.url().should('include', link.url);
+      cy.visit('/api-documentation/');
+    });
+  });
+
+  // it('Validate all external links on the page navigate to the correct destinations', () => {
+  //   const links = [
+  //     {
+  //       name: 'data registry',
+  //       url: 'https://fiscal.treasury.gov/data-registry/index.html',
+  //     },
+  //     // {
+  //     //   name: 'USAspending',
+  //     //   url: 'https://www.usaspending.gov/',
+  //     // },
+  //   ];
+  //
+  //   links.forEach(link => {
+  //     cy.findByRole('link', { name: link.name }).should('have.attr', 'href', link.url);
+  //   });
+  // });
 });
