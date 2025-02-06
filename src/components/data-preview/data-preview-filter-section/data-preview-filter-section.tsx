432dd6eacf4fce5ccaac9a0c1bd0d234810e1847
Noah_Simcoe <noah.simcoe@kc.frb.org>
2025-02-06 09:51:44 -0600

    FDG-9971 - WIP, added div

--- src/components/data-preview/data-preview-filter-section/data-preview-filter-section.tsx
+++ src/components/data-preview/data-preview-filter-section/data-preview-filter-section.tsx
@@ -2,7 +2,7 @@ import { FunctionComponent, ReactElement } from 'react';
 import React from 'react';
 import DataPreviewDownload from './data-preview-download/data-preview-download';
 import { pxToNumber } from '../../../helpers/styles-helper/styles-helper';
-import { filterAndDownloadContainer, filterContainer } from './data-preview-filter-section.module.scss';
+import { filterAndDownloadContainer, filterContainer, tableToggle } from './data-preview-filter-section.module.scss';
 import DataPreviewTableFilters from './data-preview-table-filters/data-preview-table-filters';
 import ColumnFilter from './column-filter/column-filter';
 import { breakpointLg } from '../data-preview.module.scss';
@@ -45,7 +45,9 @@ const DataPreviewFilterSection: FunctionComponent<DataPreviewFilterSectionProps>
           <DataPreviewTableFilters />
           <ColumnFilter allTablesSelected={allTablesSelected} />
         </div>
-        <ChartTableToggle></ChartTableToggle>
+        <div className={tableToggle}>
+          <ChartTableToggle></ChartTableToggle>
+        </div>
         {width >= pxToNumber(breakpointLg) && (
           <DataPreviewDownload
             width={width}
