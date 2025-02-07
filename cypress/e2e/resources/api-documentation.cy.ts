db1eb18a5eaf55a5ad7eedc8777f9a06e051a59a
daron-barnes <daron.barnes@kc.frb.org>
2025-02-07 17:05:49 -0600

    FDG-9112 initial, in progress

--- cypress/e2e/resources/api-documentation.cy.ts
+++ cypress/e2e/resources/api-documentation.cy.ts
@@ -70,4 +70,170 @@ describe('API Documentation Page', () => {
       cy.wrap(link).should('have.attr', 'href', 'https://www.usaspending.gov/');
     });
   });
+
+  it('Validate all sub nav links navigate to the correct destinations', () => {
+    const links = [
+      {
+        text: 'Getting Started',
+        url: 'api-documentation/#getting-started',
+      },
+      {
+        text: 'What is an API?',
+        url: 'api-documentation/#what-is-an-api',
+      },
+      {
+        text: 'What is a dataset?',
+        url: 'api-documentation/#what-is-a-dataset',
+      },
+      {
+        text: 'API Endpoint URL structure',
+        url: 'api-documentation/#api-endpoint-url-structure',
+      },
+      {
+        text: 'How to Access our API',
+        url: 'api-documentation/#how-to-access-our-api',
+      },
+      {
+        text: 'License & Authorization',
+        url: 'api-documentation/#license-and-authorization',
+      },
+      {
+        text: 'Change Log',
+        url: 'api-documentation/#change-log',
+      },
+      {
+        text: 'Endpoints',
+        url: 'api-documentation/#endpoints',
+      },
+      {
+        text: 'List of Endpoints',
+        url: 'api-documentation/#list-of-endpoints',
+      },
+      {
+        text: 'Fiscal Service Data Registry',
+        url: 'api-documentation/#data-registry',
+      },
+      {
+        text: 'Methods',
+        url: 'api-documentation/#methods',
+      },
+      {
+        text: 'Parameters',
+        url: 'api-documentation/#parameters',
+      },
+      {
+        text: 'Data Types',
+        url: 'api-documentation/#data-types',
+      },
+      {
+        text: 'Filters',
+        url: 'api-documentation/#filters',
+      },
+      {
+        text: 'Sorting',
+        url: 'api-documentation/#parameters-sorting',
+      },
+      {
+        text: 'Format',
+        url: 'api-documentation/#parameters-format',
+      },
+      {
+        text: 'Pagination',
+        url: 'api-documentation/#parameters-pagination',
+      },
+      {
+        text: 'Responses & Response Objects',
+        url: 'api-documentation/#responses-response-objects',
+      },
+      {
+        text: 'Response Codes',
+        url: 'api-documentation/#responses-response-codes',
+      },
+      {
+        text: 'Meta Object',
+        url: 'api-documentation/#responses-meta-object',
+      },
+      {
+        text: 'Links Object',
+        url: 'api-documentation/#responses-links-object',
+      },
+      {
+        text: 'Data Object',
+        url: 'api-documentation/#responses-data-object',
+      },
+      {
+        text: 'Error Object',
+        url: 'api-documentation/#responses-error-object',
+      },
+      {
+        text: 'Pagination Header',
+        url: 'api-documentation/#responses-pagination-header',
+      },
+      {
+        text: 'Aggregation & Sums',
+        url: 'api-documentation/#aggregation-sums',
+      },
+      {
+        text: 'Examples and Code Snippets',
+        url: 'api-documentation/#examples-code-snippets',
+      },
+      {
+        text: 'Filters',
+        url: 'api-documentation/#examples-filters',
+      },
+      {
+        text: 'Sorting',
+        url: 'api-documentation/#examples-sorting',
+      },
+      {
+        text: 'Format',
+        url: 'api-documentation/#examples-format',
+      },
+      {
+        text: 'Pagination',
+        url: 'api-documentation/#examples-pagination',
+      },
+      {
+        text: 'Aggregation',
+        url: 'api-documentation/#examples-aggregation',
+      },
+      {
+        text: 'Pivoting',
+        url: 'api-documentation/#examples-pivoting',
+      },
+      {
+        text: 'Multi-dimension Datasets',
+        url: 'api-documentation/#examples-multi-dimension-datasets',
+      },
+    ];
+
+    const problems = [
+      {
+        text: 'Fields by Endpoint',
+        url: 'api-documentation/#fields-by-endpoint',
+        level: 3,
+      },
+      {
+        text: 'Fields by Endpoint',
+        url: 'api-documentation/#fields-fields-by-endpoint',
+        level: 4,
+      },
+      {
+        text: 'Fields',
+        url: 'api-documentation/#fields',
+      },
+      {
+        text: 'Fields',
+        url: 'api-documentation/#examples-fields',
+      },
+    ];
+
+    links.forEach(link => {
+      let sel = '[class^="api-module--link"]';
+      sel = link.level ? `${sel}[class*="Level${link.level}"]` : sel;
+
+      cy.findByText(link.text, { selector: sel }).click();
+      cy.url().should('include', link.url);
+    });
+  });
 });
