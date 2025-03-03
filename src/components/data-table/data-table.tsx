ac91eceaa5d77614c7faa963058c53c4042b44e2
Stevens <110061741+chasls2@users.noreply.github.com>
2025-03-03 10:47:48 -0600

    FDG-10209

--- src/components/data-table/data-table.tsx
+++ src/components/data-table/data-table.tsx
@@ -242,8 +242,10 @@ const DataTable: FunctionComponent<IDataTableProps> = ({
         downloadData = downloadData.map(entry => {
           const dataWithTextQualifiers = [];
           Object.values(entry).forEach(val => {
-            dataWithTextQualifiers.push(val?.includes(',') ? `"${val}"` : val);
+            const stringValue = String(val ?? '');
+            dataWithTextQualifiers.push(stringValue.includes(',') ? `"${stringValue}"` : stringValue);
           });
+
           return dataWithTextQualifiers;
         });
         downloadData.unshift(downloadHeaders);
