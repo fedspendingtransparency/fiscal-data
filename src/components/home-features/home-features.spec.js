6634ec3f48270db0817d003b5d038be092eb891b
j1nts01 <noah.simcoe@kc.frb.org>
2025-03-04 15:28:22 -0600

    FDG 10201 - more test rewrites

--- src/components/home-features/home-features.spec.js
+++ src/components/home-features/home-features.spec.js
@@ -17,18 +17,14 @@ describe('HomeFeatures component', () => {
   });
 
   it('has aria-hidden=true and alt text on each of those six images', () => {
-    const imagesArray = instance.findAllByType('img');
-    const featuresArr = features.slice();
-    // Loops through all of the images found in the virtual dom
-    imagesArray.forEach(img => {
-      expect(img.props['aria-hidden']).toBe('true');
-      const imgAlt = img.props.alt;
-      // Compares the alt text on the virtual dom images to the alt text supplied
-      // from the features const
-      const featuresIdx = featuresArr.map(d => d.alt).indexOf(imgAlt);
+    const { getAllByRole } = render(<HomeFeatures />);
+    const images = getAllByRole('img', { hidden: true });
+    const featuresArr = [...features];
+    images.forEach(img => {
+      expect(img.getAttribute('aria-hidden')).toBe('true');
+      const imgAlt = img.getAttribute('alt');
+      const featuresIdx = featuresArr.findIndex(d => d.alt === imgAlt);
       expect(featuresIdx).toBeGreaterThanOrEqual(0);
-      // Helps to ensure a 1 to 1 relationship between the dom images and the supplied
-      // features const.
       featuresArr.splice(featuresIdx, 1);
     });
   });
