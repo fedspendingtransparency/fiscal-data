9ea6148e309a217e0a331e17166131f05c44d3c8
Stevens <110061741+chasls2@users.noreply.github.com>
2025-03-04 21:07:42 -0600

    FDG-10196 small fix

--- src/components/chart-with-table/chart-table-view.spec.js
+++ src/components/chart-with-table/chart-table-view.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import ChartTableView from './interest-expense-table';
+import ChartTableView from './chart-table-view';
 import '@testing-library/jest-dom';
 
 const mockDtgTable = jest.fn();
