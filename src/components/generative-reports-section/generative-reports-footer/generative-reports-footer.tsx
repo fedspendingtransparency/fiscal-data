70fafa10fb3a406988806970d31732237399686f
Barnes <J1DZB01@rb.win.frb.org>
2025-03-11 08:47:16 -0500

    FDG-10170 cleanup

--- src/components/generative-reports-section/generative-reports-footer/generative-reports-footer.tsx
+++ src/components/generative-reports-section/generative-reports-footer/generative-reports-footer.tsx
@@ -1,16 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import { generativeReportsFooter, footerMessageLabel, footerEmphasis } from './generative-reports-footer.module.scss';
 const GenerativeReportsFooter: FunctionComponent = ({ message }) => {
-  const defaultMessage = (
-    <>
-      <span className={footerMessageLabel}>Note: </span>
-      <span>Daily Treasury Statement reports dated </span>
-      <span className={footerEmphasis}>before </span>
-      <span>FY 1998 are grouped by fiscal year. Once inside the desired year, scroll to the specific month and day.</span>
-    </>
-  );
-
-  return <div className={generativeReportsFooter}>{message || defaultMessage}</div>;
+  return <div className={generativeReportsFooter}>{message || ''}</div>;
 };
 
 export default GenerativeReportsFooter;
