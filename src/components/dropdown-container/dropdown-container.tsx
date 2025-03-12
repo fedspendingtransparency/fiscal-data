074c1b3ccf418101f1d8f53c04da8e54b841f857
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2025-03-12 13:20:32 -0500

    FDG-10252 Update blur function

--- src/components/dropdown-container/dropdown-container.tsx
+++ src/components/dropdown-container/dropdown-container.tsx
@@ -36,7 +36,7 @@ const DropdownContainer: FunctionComponent<IDropdownContainer> = ({
     if (event) {
       const parent = dropdownRef.current;
       const related = event?.relatedTarget as HTMLElement;
-      if ((!parent?.outerText?.includes(related?.outerText) || !related?.outerText) && related?.id !== 'gatsby-focus-wrapper') {
+      if (!parent?.outerText?.includes(related?.outerText) && related?.id !== 'gatsby-focus-wrapper') {
         setActive(false);
       }
     }
