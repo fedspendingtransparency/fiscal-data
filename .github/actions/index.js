0c24b1257caf16068d168dca9bf7b43d88ec407a
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2025-03-14 16:51:03 -0500

    test

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -1,13 +1,14 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios');
+const summaryData = require('../../coverage/coverage-summary.json');
 // const { XMLData } = require('../../coverage/clover.xml');
 
 try {
   // Fetch the value of the input 'who-to-greet' specified in action.yml
   const nameToGreet = core.getInput('who-to-greet');
   const summaryData = require('../../coverage/coverage-summary.json');
-  console.log(`Hello ${nameToGreet}!`);
+  console.log('total: ', summaryData.total.lines.total, 'covered', summaryData.total.lines.total);
 
   // Record the time of greeting as an output
   const time = new Date().toTimeString();
