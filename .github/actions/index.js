5033486470fc96a3d015dc38bebe60d621ef7810
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2025-03-17 08:10:38 -0500

    test

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -1,18 +1,20 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const axios = require('axios');
-const summaryData = require('../../coverage/coverage-summary.json');
-// const { XMLData } = require('../../coverage/clover.xml');
 
 try {
-  // Fetch the value of the input 'who-to-greet' specified in action.yml
-  const nameToGreet = core.getInput('who-to-greet');
   const summaryData = require('../../coverage/coverage-summary.json');
   console.log('total: ', summaryData.total.lines.total, 'covered', summaryData.total.lines.total);
+  const coverage = summaryData.total.lines.covered / summaryData.total.lines.total;
 
-  // Record the time of greeting as an output
-  const time = new Date().toTimeString();
-  core.setOutput('time', time);
+  const token = core.getInput('GITHUB_TOKEN');
+  const context = github.context;
+  const pr_number = context.payload.pull_request.number;
+  const oktokit = github.getOctokit(token);
+  const new_comment = oktokit.rest.issues.createComment({
+    ...context.repo,
+    issue_number: pr_number,
+    body: `Coverage: ${coverage}`,
+  });
 } catch (error) {
   // Handle errors and indicate failure
   core.setFailed(error.message);
