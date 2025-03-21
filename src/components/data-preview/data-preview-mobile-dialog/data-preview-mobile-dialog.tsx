50ddf4892438990deb72a7c79fdd3c163425bcc8
Noah_Simcoe <noah.simcoe@kc.frb.org>
2025-03-21 14:56:22 -0500

    FDG-9975 wip

--- src/components/data-preview/data-preview-mobile-dialog/data-preview-mobile-dialog.tsx
+++ src/components/data-preview/data-preview-mobile-dialog/data-preview-mobile-dialog.tsx
@@ -1,14 +1,23 @@
 import {
-  linkContainer,
+  applyCancelContainer,
+  button,
+  checkIcon,
   mainContainer,
   navContainer,
-  navigableContent,
 } from '../../data-preview/data-preview-mobile-dialog/data-preview-mobile-dialog.module.scss';
 import React from 'react';
+import { Link } from 'gatsby';
+import SearchBar from '../../../components/search-bar/search-bar';
+import { faCaretLeft, faCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const DataPreviewMobileDialog = () => {
   const shouldTocShow = true;
-  const shouldContentShow = false;
+
+  const onSearchBarChange = event => {
+    // const val = event && event.target ? event.target.value : '';
+    // filterHandler(val);
+  };
 
   return (
     <div className={mainContainer}>
@@ -17,17 +26,43 @@ const DataPreviewMobileDialog = () => {
         {shouldTocShow}
         {shouldTocShow && (
           <div key={1}>
-            <h1>test test</h1>
-            <div
-              role="presentation"
-              // onMouseEnter={() => handleMouseEnter(1)}
-              // onMouseLeave={handleMouseLeave}
-              className={`${linkContainer}`}
-            ></div>
+            <h3>
+              <FontAwesomeIcon icon={faCaretLeft} />
+              Data Preview
+            </h3>
+            {/*below elements need to be left-aligned*/}
+            <div>
+              <h3>Filters</h3>
+              <p>Search filters</p>
+              <div>
+                <SearchBar onChange={onSearchBarChange} filter={'test'} />
+              </div>
+            </div>
+            {/*below div is for filter options*/}
+            <div>
+              <h3>Filter #1 test</h3>
+              <h3>Filter #2 test</h3>
+            </div>
+            {/*below div is for apply/cancel block - add box shadow above*/}
+            <div className={applyCancelContainer}>
+              <Link to="/api-documentation/" className={button} data-testid={'button-link'}>
+                <FontAwesomeIcon icon={faCheck} className={checkIcon} />
+                Apply
+              </Link>
+              <h4>
+                <u>Cancel</u>
+              </h4>
+            </div>
+            {/*<div*/}
+            {/*  role="presentation"*/}
+            {/*  // onMouseEnter={() => handleMouseEnter(1)}*/}
+            {/*  // onMouseLeave={handleMouseLeave}*/}
+            {/*  className={`${linkContainer}`}*/}
+            {/*></div>*/}
           </div>
         )}
       </div>
-      <div className={`${navigableContent} ${shouldContentShow ? '' : 'hidden'}`}>TEST TEXT</div>
+      {/*<div className={`${navigableContent} ${shouldContentShow ? '' : 'hidden'}`}>TEST TEXT</div>*/}
       {/*<TOCButton handleToggle={handleInteraction} state={tocIsOpen} />*/}
     </div>
   );
