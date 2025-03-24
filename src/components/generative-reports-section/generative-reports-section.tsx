5345d1eedc85f7c74358f9155d1f082b62e0d0c0
chasls2 <110061741+chasls2@users.noreply.github.com>
2025-03-24 11:11:28 -0500

    FDG-10172 Added date picket

--- src/components/generative-reports-section/generative-reports-section.tsx
+++ src/components/generative-reports-section/generative-reports-section.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FunctionComponent } from 'react';
 import DatasetSectionContainer from '../dataset-section-container/dataset-section-container';
 import { IDatasetApi } from '../../models/IDatasetApi';
 import GenerativeReportsEmptyTable from './generative-reports-empty-table/generative-reports-empty-table';
 import { filtersContainer } from '../published-reports/reports-section/reports-section.module.scss';
 import GenerativeReportsAccountFilter from './generative-reports-account-filter/generative-reports-account-filter';
+import ReportDatePicker from '../published-reports/report-date-picker/report-date-picker';
 
 export const title = 'Reports and Files';
 export const notice = 'Banner Notice';
@@ -13,12 +14,26 @@ const GenerativeReportsSection: FunctionComponent<{ apisProp: IDatasetApi[]; use
   apisProp,
   useDefaultReportTable,
 }) => {
+  const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+
+  const earliestDate = new Date(apisProp[0]?.earliestDate);
+  const latestDate = new Date(apisProp[0].latestDate);
+
   return (
     <>
       {useDefaultReportTable && (
         <div>
           <DatasetSectionContainer title={title} id={'generative-reports-and-files'}>
             <div className={filtersContainer}>
+              <ReportDatePicker
+                isDailyReport={false}
+                selectedDate={selectedDate}
+                setSelectedDate={setSelectedDate}
+                latestReportDate={latestDate}
+                earliestReportDate={earliestDate}
+                allReportDates={[]}
+                allReportYears={[]}
+              />
               <GenerativeReportsAccountFilter apiData={apisProp} />
             </div>
             <GenerativeReportsEmptyTable />
