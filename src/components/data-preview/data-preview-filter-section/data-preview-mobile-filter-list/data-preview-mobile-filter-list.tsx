58caec55ec3d6e4615d381e66e911c699f2d08fb
Noah_Simcoe <noah.simcoe@kc.frb.org>
2025-04-01 10:27:00 -0500

    FDG-9977 styling

--- src/components/data-preview/data-preview-filter-section/data-preview-mobile-filter-list/data-preview-mobile-filter-list.tsx
+++ src/components/data-preview/data-preview-filter-section/data-preview-mobile-filter-list/data-preview-mobile-filter-list.tsx
@@ -1,4 +1,10 @@
 import React, { FunctionComponent } from 'react';
+import {
+  buttonContainer,
+  buttonSleeve,
+  optionName,
+  optionSecondary,
+} from '../../../data-preview/data-preview-filter-section/data-preview-mobile-filter-list/data-preview-mobile-filter-list.module.scss';
 
 const placeholderFilters = [
   { name: 'Record Date', secondary: 'Last 5 years' },
@@ -15,16 +21,18 @@ export interface IMobileFilterList {
 
 const DataPreviewMobileFilterList: FunctionComponent<IMobileFilterList> = ({ filterOptions = placeholderFilters }) => {
   return (
-    <>
+    <div data-testid="test" className={buttonContainer}>
       {filterOptions.map(filterOption => {
         return (
-          <button>
-            <span>{filterOption.name}</span>
-            <span>{filterOption.secondary}</span>
-          </button>
+          <div className={buttonSleeve}>
+            <button>
+              <span className={optionName}>{filterOption.name}</span>
+              <span className={optionSecondary}>{filterOption.secondary}</span>
+            </button>
+          </div>
         );
       })}
-    </>
+    </div>
   );
 };
 
