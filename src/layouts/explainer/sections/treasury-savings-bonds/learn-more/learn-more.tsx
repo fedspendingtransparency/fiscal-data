2a7466949757b16dc2cabcdb202223501adc1b32
daron-barnes <Daron.Barnes@kc.frb.org>
2025-04-07 10:49:28 -0500

    FDG-10171

--- src/layouts/explainer/sections/treasury-savings-bonds/learn-more/learn-more.tsx
+++ src/layouts/explainer/sections/treasury-savings-bonds/learn-more/learn-more.tsx
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import CustomLink from '../../../../../components/links/custom-link/custom-link';
 import Footnote from '../../../../../components/footnote/footnote';
 import { getSaleBondsFootNotes } from './learn-more-helper';
+<<<<<<< HEAD
+=======
+import AnchorText from '../../../../../components/anchor-text/anchor-text';
+import { analyticsEventHandler } from '../../../explainer-helpers/explainer-helpers';
+>>>>>>> ef661e57d (FDG-10171)
 
 const LearnMore: React.FC = () => {
   const [lastAnchorClicked, setLastAnchorClicked] = useState<string | null>(null);
@@ -21,13 +26,30 @@ const LearnMore: React.FC = () => {
     }
   };
 
+  const links = {
+    treasury: (
+      <CustomLink
+        url="https://www.treasurydirect.gov/savings-bonds/buy-a-bond/"
+        onClick={() => analyticsEventHandler('TreasuryDirect', 'Savings Bonds Citation Click')}
+      >
+        TreasuryDirect
+      </CustomLink>
+    ),
+    hunt: (
+      <CustomLink
+        url="https://treasurydirect.gov/savings-bonds/treasury-hunt/"
+        onClick={() => analyticsEventHandler('Treasury Hunt', 'Savings Bonds Citation Click')}
+      >
+        Treasury Hunt
+      </CustomLink>
+    ),
+  };
+
   return (
     <>
       <p>
-        Today, individuals can buy Series I and Series EE bonds online through{' '}
-        <CustomLink url="https://www.treasurydirect.gov/savings-bonds/buy-a-bond/">TreasuryDirect</CustomLink>. TreasuryDirect also offers a feature
-        called <CustomLink url="https://treasurydirect.gov/savings-bonds/treasury-hunt/">Treasury Hunt</CustomLink>, which allows users to search to
-        see if there are unredeemed bonds in their name.
+        Today, individuals can buy Series I and Series EE bonds online through {links['treasury']}. TreasuryDirect also offers a feature called{' '}
+        {links['hunt']}, which allows users to search to see if there are unredeemed bonds in their name.
       </p>
       <Footnote footnotes={getSaleBondsFootNotes()} width="100%" onBackToContentClick={handleBackToContentClick} />
     </>
