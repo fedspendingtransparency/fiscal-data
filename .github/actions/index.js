2a812d0aae9c29875d8998964161527ce6ee76ff
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2025-04-11 14:48:42 -0500

    wip

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -19,7 +19,7 @@ const github = require('@actions/github');
       issue_number: pr_number,
       issue_url: `https://api.github.com/repos/fedspendingtransparency/fiscal-data/issues/${pr_number}`,
     });
-    console.log(context.payload.pull_request._links.comments);
+    console.log(context.payload);
 
     oktokit.rest.issues.createComment({
       ...context.repo,
