c4931baeeeae66e7453905d9d8f6a8133920de16
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2025-04-11 10:00:18 -0500

    wip

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -4,6 +4,7 @@ const github = require('@actions/github');
 try {
   const summaryData = require('../../coverage/coverage-summary.json');
   const coverage = summaryData.total.lines.pct;
+  const icon = coverage < 90 ? '' : ':white-check-mark:';
   const token = core.getInput('GITHUB_TOKEN');
   const context = github.context;
   const pr_number = context.payload.pull_request.number;
@@ -11,7 +12,7 @@ try {
   oktokit.rest.issues.createComment({
     ...context.repo,
     issue_number: pr_number,
-    body: `Coverage: ${coverage}`,
+    body: `Coverage: ${coverage}% ${icon}`,
   });
 } catch (error) {
   // Handle errors and indicate failure
