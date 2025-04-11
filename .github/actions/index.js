a04ebcc19a6090526cba125c7514387afc0ed7c7
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2025-04-11 10:43:05 -0500

    wip

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -11,7 +11,7 @@ try {
   const context = github.context;
   const pr_number = context.payload.pull_request.number;
   const oktokit = github.getOctokit(token);
-  oktokit.rest.gists.listComments();
+  await oktokit.rest.gists.listComments();
 
   oktokit.rest.issues.createComment({
     ...context.repo,
