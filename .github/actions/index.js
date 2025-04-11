df5bd546d780a6adde8426ea66e3f7b7ecfea5af
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2025-04-11 13:13:52 -0500

    wip

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -12,18 +12,8 @@ try {
   const pr_number = context.payload.pull_request.number;
   const issue_number = context.payload.issue.number;
   const oktokit = github.getOctokit(token);
-  // await oktokit.rest.gists.listComments();
-  // await oktokit.request(`PATCH /repos/fedspendingtransparency/fiscal-data/pull/${pr_number}`, {
-  //   body: `Total Line Coverage: ${coverage}% ${icon}`,
-  // });
-  await oktokit.request(`GET /repos/{owner}/{repo}/issues/{issue_number}`, {
-    owner: 'fedspendingtransparency',
-    repo: 'fiscal-data',
-    issue_number: issue_number,
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28',
-    },
-  });
+
+  oktokit.rest.issues.getComment({ ...context.repo, issue_number: pr_number });
 
   oktokit.rest.issues.createComment({
     ...context.repo,
