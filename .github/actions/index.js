7eaea7488c06651e3461d43f3e51851bc38c99a7
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2025-04-11 13:50:32 -0500

    wip

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -2,27 +2,29 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  // const summaryData = require('../../coverage/coverage-summary.json');
-  // const coverage = summaryData.total.lines.pct; ??
-  const coverage = 90.35;
-  const icon = coverage < 90 ? '❌' : `✅`;
-  const token = core.getInput('GITHUB_TOKEN');
-  const context = github.context;
-  const pr_number = context.payload.pull_request.number;
-  // const issue_number = context.payload.issue.number;
-  const oktokit = github.getOctokit(token);
+(async () => {
+  try {
+    // const summaryData = require('../../coverage/coverage-summary.json');
+    // const coverage = summaryData.total.lines.pct; ??
+    const coverage = 90.35;
+    const icon = coverage < 90 ? '❌' : `✅`;
+    const token = core.getInput('GITHUB_TOKEN');
+    const context = github.context;
+    const pr_number = context.payload.pull_request.number;
+    // const issue_number = context.payload.issue.number;
+    const oktokit = github.getOctokit(token);
 
-  const comment_id = await oktokit.rest.issues.getComment({ ...context.repo, issue_number: pr_number });
-  console.log(comment_id);
+    const comment_id = await oktokit.rest.issues.getComment({ ...context.repo, issue_number: pr_number });
+    console.log(comment_id);
 
-  oktokit.rest.issues.createComment({
-    ...context.repo,
-    issue_number: pr_number,
-    body: `Total Line Coverage: ${coverage}% ${icon}`,
-    comment_id: `${pr_number}_total_coverage`,
-  });
-} catch (error) {
-  // Handle errors and indicate failure
-  core.setFailed(error.message);
-}
+    oktokit.rest.issues.createComment({
+      ...context.repo,
+      issue_number: pr_number,
+      body: `Total Line Coverage: ${coverage}% ${icon}`,
+      comment_id: `${pr_number}_total_coverage`,
+    });
+  } catch (error) {
+    // Handle errors and indicate failure
+    core.setFailed(error.message);
+  }
+})();
