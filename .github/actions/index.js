53e18ef4e7fd70a67efdb6973a6bcd4766a4c279
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2025-04-11 10:16:51 -0500

    coverage icons!

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -4,7 +4,7 @@ const github = require('@actions/github');
 try {
   const summaryData = require('../../coverage/coverage-summary.json');
   const coverage = summaryData.total.lines.pct;
-  const icon = coverage < 90 ? '' : `✅`;
+  const icon = coverage < 90 ? '❌' : `✅`;
   const token = core.getInput('GITHUB_TOKEN');
   const context = github.context;
   const pr_number = context.payload.pull_request.number;
