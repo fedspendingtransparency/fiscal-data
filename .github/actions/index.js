5718b4a4783189e848bc2804d11b5e577bb678f5
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2025-04-11 08:25:57 -0500

    wip

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -4,11 +4,12 @@ const github = require('@actions/github');
 try {
   const summaryData = require('../../coverage/coverage-summary.json');
   const coverage = summaryData.total.lines.covered / summaryData.total.lines.total;
+  const coverageFormatted = coverage.toPrecision(2);
   const token = core.getInput('GITHUB_TOKEN');
   const context = github.context;
   const pr_number = context.payload.pull_request.number;
   const oktokit = github.getOctokit(token);
-  const new_comment = oktokit.rest.issues.createComment({
+  oktokit.rest.issues.createComment({
     ...context.repo,
     issue_number: pr_number,
     body: `Coverage: ${coverage}`,
