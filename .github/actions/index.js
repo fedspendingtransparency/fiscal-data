1cabd390f4044e992c99d43b75a9d50d7190f71e
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2025-04-11 15:55:51 -0500

    wip

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -4,16 +4,12 @@ const github = require('@actions/github');
 
 (async () => {
   try {
-    // const summaryData = require('../../coverage/coverage-summary.json');
-    // const coverage = summaryData.total.lines.pct; ??
     const coverage = 90.35;
     const icon = coverage < 90 ? '❌' : `✅`;
     const token = core.getInput('GITHUB_TOKEN');
     const context = github.context;
     const pr_number = context.payload.pull_request.number;
-    // const issue_number = context.payload.issue.number;
     const oktokit = github.getOctokit(token);
-    // const current_issue = await oktokit.rest.issues.
     const pr_id = context.payload.pull_request.id;
     const pr_comments_response = await oktokit.rest.issues.listComments({
       ...context.repo,
@@ -21,8 +17,8 @@ const github = require('@actions/github');
       id: pr_id,
     });
     const pr_comments = pr_comments_response.data;
-    if (pr_comments.length > 0) {
-      const coverage_comment = pr_comments.find(comment => comment.user.login === 'github-actions[bot]');
+    const coverage_comment = pr_comments?.find(comment => comment.user.login === 'github-actions[bot]');
+    if (pr_comments.length > 0 && coverage_comment) {
       const last_comment = pr_comments[pr_comments.length - 1];
       console.log(coverage_comment);
       console.log(pr_comments);
@@ -30,7 +26,7 @@ const github = require('@actions/github');
         ...context.repo,
         issue_number: pr_number,
         comment_id: coverage_comment.id,
-        body: `Total !! Line !! Coverage: ${coverage}% ${icon}`,
+        body: `Total Line Coverage: ${coverage}% ${icon}`,
       });
     } else {
       oktokit.rest.issues.createComment({
