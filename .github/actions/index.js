6f844c3710e762a02dbda2673ace2004defd3674
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2025-04-11 15:29:34 -0500

    wip

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -19,24 +19,25 @@ const github = require('@actions/github');
       ...context.repo,
       issue_number: pr_number,
       id: pr_id,
-    });
-    const last_comment = pr_comments.data[pr_comments.data.length - 1];
+    }).data;
     // const comment_id = last_comment.id
-    console.log(last_comment);
-    console.log('**********************************************************');
-    console.log(context);
-    //
-    // oktokit.rest.issues.createComment({
-    //   ...context.repo,
-    //   issue_number: pr_number,
-    //   body: `Total Line Coverage: ${coverage}% ${icon}`,
-    // });
-    oktokit.rest.issues.updateComment({
-      ...context.repo,
-      issue_number: pr_number,
-      comment_id: last_comment.id,
-      body: `Total !! Line !! Coverage: ${coverage}% ${icon}`,
-    });
+    if (pr_comments.length > 0) {
+      const coverage_comment = pr_comments.find(comment => comment.body.includes('Total !! Line !! Coverage:'))[0];
+      const last_comment = pr_comments[pr_comments.data.length - 1];
+      console.log(coverage_comment);
+      oktokit.rest.issues.updateComment({
+        ...context.repo,
+        issue_number: pr_number,
+        comment_id: last_comment.id,
+        body: `Total !! Line !! Coverage: ${coverage}% ${icon}`,
+      });
+    } else {
+      oktokit.rest.issues.createComment({
+        ...context.repo,
+        issue_number: pr_number,
+        body: `Total Line Coverage: ${coverage}% ${icon}`,
+      });
+    }
   } catch (error) {
     // Handle errors and indicate failure
     core.setFailed(error.message);
