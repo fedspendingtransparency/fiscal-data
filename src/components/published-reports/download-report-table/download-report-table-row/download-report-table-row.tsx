ec354fea60dd7e28e524fc93fe73f9bcef71c4c7
Noah_Simcoe <noah.simcoe@kc.frb.org>
2025-04-21 07:44:38 -0500

    FDG-10395 resetting state if needed

--- src/components/published-reports/download-report-table/download-report-table-row/download-report-table-row.tsx
+++ src/components/published-reports/download-report-table/download-report-table-row/download-report-table-row.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect, useState } from 'react';
+import React, {FunctionComponent, useEffect, useState} from 'react';
 import {
   center,
   downloadButton,
@@ -15,15 +15,15 @@ import {
   fileDescription,
   startName,
 } from './download-report-table-row.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleCheck, faCloudArrowDown } from '@fortawesome/free-solid-svg-icons';
-import { getFileDisplay, getFileTypeImage, getGeneratedReportFileDisplay } from '../../util/util';
-import { IPublishedReportDataJson } from '../../../../models/IPublishedReportDataJson';
-import { getDateLabelForReport, getGeneratedFileSize } from '../../../../helpers/dataset-detail/report-helpers';
-import { getFileSize } from '../../download-report/download-helpers';
-import { PDFDownloadLink } from '@react-pdf/renderer/lib/react-pdf.browser';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCircleCheck, faCloudArrowDown} from '@fortawesome/free-solid-svg-icons';
+import {getFileDisplay, getFileTypeImage, getGeneratedReportFileDisplay} from '../../util/util';
+import {IPublishedReportDataJson} from '../../../../models/IPublishedReportDataJson';
+import {getDateLabelForReport, getGeneratedFileSize} from '../../../../helpers/dataset-detail/report-helpers';
+import {getFileSize} from '../../download-report/download-helpers';
+import {PDFDownloadLink} from '@react-pdf/renderer/lib/react-pdf.browser';
 import ReportGenerator from '../../report-generator/report-generator';
-import { DocumentProps, pdf } from '@react-pdf/renderer';
+import {DocumentProps, pdf} from '@react-pdf/renderer';
 
 interface IGeneratedReport {
   name: string;
@@ -110,6 +110,8 @@ const DownloadReportTableRow: FunctionComponent<{
         try {
           const blob = await pdf(instance).toBlob();
           getGeneratedFileSize(blob, setFileSize);
+          const { setApiErrorMessage } = generatedReport;
+          setApiErrorMessage(false);
         } catch (error) {
           const { setApiErrorMessage } = generatedReport;
           setApiErrorMessage(true);
