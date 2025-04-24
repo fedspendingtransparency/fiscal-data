48dc43db0851c3edb9c4275adcb30e7fb6a75ad7
j1nts01 <noah.simcoe@kc.frb.org>
2025-04-24 11:34:29 -0500

    FDG-10454 date picker hidden for special cases

--- src/transform/metadata-transform.js
+++ src/transform/metadata-transform.js
@@ -111,6 +111,7 @@ const transformMapper = (datasetIdMap, endpointConfigIdMap, topics, filters, rel
       dataset.relatedDatasets = mappedDataset && mappedDataset.relatedDatasets ? mappedDataset.relatedDatasets : [];
       dataset.currentDateButton = mappedDataset && mappedDataset.currentDateButton ? mappedDataset.currentDateButton : null;
       dataset.hideRawDataTable = mappedDataset && mappedDataset.hideRawDataTable ? mappedDataset.hideRawDataTable : null;
+      dataset.hideReportDatePicker = mappedDataset && mappedDataset.hideReportDatePicker ? mappedDataset.hideReportDatePicker : null;
       dataset.detailView = mappedDataset && mappedDataset.detailView ? mappedDataset.detailView : null;
       dataset.customNoChartMessage = mappedDataset && mappedDataset.customNoChartMessage ? mappedDataset.customNoChartMessage : null;
       dataset.datePreset = mappedDataset && mappedDataset.datePreset ? mappedDataset.datePreset : null;
