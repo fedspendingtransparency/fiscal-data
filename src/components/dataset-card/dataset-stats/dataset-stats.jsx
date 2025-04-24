00cfe236e52f003fc7d097e141916321587070bd
chasls2 <110061741+chasls2@users.noreply.github.com>
2025-04-24 10:42:30 -0500

    FDG-10451

--- src/components/dataset-card/dataset-stats/dataset-stats.jsx
+++ src/components/dataset-card/dataset-stats/dataset-stats.jsx
@@ -14,49 +14,54 @@ export default function DatasetStats({ dataset }) {
   const lastUpdated = dataset && dataset.techSpecs && dataset.techSpecs.lastUpdated ? dataset.techSpecs.lastUpdated : 'no date available';
   const latestDateParts = dataset && dataset.techSpecs && dataset.techSpecs.latestDate ? latestDate.split('/') : ['', '', ''];
   const useFutureIcon = isAfter(new Date(latestDateParts[2] - 0, latestDateParts[0] - 1, latestDateParts[1] - 0, 0, 0, 0), new Date());
+  const hideRawDataTable = dataset.hideRawDataTable;
 
   return (
     <ul className={list}>
-      <li data-testid="dateRange-li" aria-label={'Date Range: ' + dateRange}>
-        <div className={statItem}>
-          <div>
-            {dateRange && useFutureIcon ? (
-              <img
-                src={futureDateIcon}
-                className={futureDateIconStyle}
-                data-testid={'futureDateIcon'}
-                alt={'future date icon'}
-                aria-hidden={'true'}
-              />
-            ) : (
-              <FontAwesomeIcon icon={faCalendarWeek} size="1x" className={icon} data-testid="calendar-week-icon" />
-            )}
-          </div>
-          <div>
-            <div className={statHeaderText}> Date Range </div>
-            <div className={stateSubHeaderText}> {dateRange} </div>
-          </div>
-        </div>
-      </li>
-      <li data-testid={'lastUpdated'} aria-label={'last updated' + lastUpdated}>
-        <div className={statItem}>
-          <FontAwesomeIcon icon={faPen} size="1x" className={icon} data-testid={'pen-icon'} />
-          <div>
-            <div className={statHeaderText}> Last Updated </div>
-            <div className={stateSubHeaderText}> {lastUpdated} </div>
-          </div>
-        </div>
-      </li>
+      {!hideRawDataTable && (
+        <>
+          <li data-testid="dateRange-li" aria-label={'Date Range: ' + dateRange}>
+            <div className={statItem}>
+              <div>
+                {dateRange && useFutureIcon ? (
+                  <img
+                    src={futureDateIcon}
+                    className={futureDateIconStyle}
+                    data-testid={'futureDateIcon'}
+                    alt={'future date icon'}
+                    aria-hidden={'true'}
+                  />
+                ) : (
+                  <FontAwesomeIcon icon={faCalendarWeek} size="1x" className={icon} data-testid="calendar-week-icon" />
+                )}
+              </div>
+              <div>
+                <div className={statHeaderText}> Date Range</div>
+                <div className={stateSubHeaderText}> {dateRange} </div>
+              </div>
+            </div>
+          </li>
+          <li data-testid={'lastUpdated'} aria-label={'last updated' + lastUpdated}>
+            <div className={statItem}>
+              <FontAwesomeIcon icon={faPen} size="1x" className={icon} data-testid={'pen-icon'} />
+              <div>
+                <div className={statHeaderText}> Last Updated</div>
+                <div className={stateSubHeaderText}> {lastUpdated} </div>
+              </div>
+            </div>
+          </li>
+        </>
+      )}
       <li data-testid="updateFrequency-li" aria-label={frequency}>
         <div className={statItem}>
           <FontAwesomeIcon icon={faRepeat} size="1x" className={icon} data-testid="repeat-icon" />
           <div>
-            <div className={statHeaderText}> Release Frequency </div>
+            <div className={statHeaderText}> Release Frequency</div>
             <div className={stateSubHeaderText}> {frequency} </div>
           </div>
         </div>
       </li>
-      {dataset.apis && (
+      {dataset.apis && !hideRawDataTable && (
         <li data-testid="numTables-li" aria-label={'number of data tables: ' + dataset.apis.length}>
           <div className={statItem}>
             <FontAwesomeIcon icon={faDatabase} size="1x" className={icon} data-testid="database-icon" />
