65653bd0f5f4967e5a1a783e4ef13dd14703b8fb
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2025-04-28 08:39:36 -0500

    FDG-10458 Add dataset to dev for testing

--- env/dev.js
+++ env/dev.js
@@ -5,7 +5,22 @@ module.exports = {
   DATA_DOWNLOAD_BASE_URL: 'https://dev.fiscaldata.treasury.gov',
   WEB_SOCKET_BASE_URL: 'wss://downloads.dev.fiscaldata.treasury.gov/main',
   EXPERIMENTAL_WHITELIST: ['experimental-page', 'not-found-md', 'apiNKL', 'aboutUsMDX', 'publishedReportsSection', 'chartingConfigurationTool'],
-  ADDITIONAL_DATASETS: {},
+  ADDITIONAL_DATASETS: {
+    '015-BFS-2025Q2-002': {
+      slug: '/monthly-treasury-disbursements/',
+      hideRawDataTable: true,
+      seoConfig: {
+        pageTitle: 'Monthly Treasury Disbursements',
+        description: '?',
+        keywords: '',
+      },
+      topics: [],
+    },
+  },
   USE_MOCK_RELEASE_CALENDAR_DATA_ON_API_FAIL: true,
-  ADDITIONAL_ENDPOINTS: {},
+  ADDITIONAL_ENDPOINTS: {
+    322: {
+      endpoint: '',
+    },
+  },
 };
