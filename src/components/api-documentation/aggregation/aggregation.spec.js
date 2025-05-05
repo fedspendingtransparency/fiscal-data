1ae07d6b7cd3f42e6bf839c147944ffb28e7a11c
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2025-05-05 14:38:30 -0500

    FDG-10438 wip

--- src/components/api-documentation/aggregation/aggregation.spec.js
+++ src/components/api-documentation/aggregation/aggregation.spec.js
@@ -1,22 +1,18 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import SectionContent from '../section-content/section-content';
 import Aggregation from './aggregation';
+import { render } from '@testing-library/react';
 
 describe('Filters', () => {
-  let component = renderer.create();
-  renderer.act(() => {
-    component = renderer.create(<Aggregation />);
-  });
-  const instance = component.root;
-
   it('has SectionContent as a part of its layout', () => {
-    expect(instance.findAllByType(SectionContent).length).toBeGreaterThan(0);
+    const { getAllByTestId } = render(<Aggregation />);
+    expect(getAllByTestId('section-content').length).toBeGreaterThan(0);
   });
 
   it('creates the Aggregation section with the desired id, heading tag and title', () => {
     const title = 'Aggregation and Sums';
-    const heading = instance.findByProps({ id: 'aggregation-sums' }).findByType('h2');
-    expect(heading.children[0]).toBe(title);
+    const { getByRole } = render(<Aggregation />);
+
+    const heading = getByRole('heading', { level: 2, name: title });
+    expect(heading).toBeInTheDocument();
   });
 });
