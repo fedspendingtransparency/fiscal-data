0cf0e075c2f45b8fa47a7c957174a1132b5271a1
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2025-05-05 11:39:06 -0500

    FDG-10438 masthead

--- src/layouts/topics/masthead/masthead.spec.js
+++ src/layouts/topics/masthead/masthead.spec.js
@@ -1,34 +1,31 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import MastHead from './masthead';
-import BreadCrumbs from '../../../components/breadcrumbs/breadcrumbs';
+import { render, within } from '@testing-library/react';
 
 describe('Topics Masthead', () => {
   const title = 'Dummy Topic';
-  let component;
-  let instance;
-
-  beforeEach(() => {
-    component = renderer.create(<MastHead title={title} />);
-    instance = component.root;
-  });
 
   it('contains the topic title within its breadcrumbs', () => {
-    const breadCrumbProps = instance.findByType(BreadCrumbs).props.links;
-    expect(breadCrumbProps[0].name).toStrictEqual(title);
+    const { getByTestId } = render(<MastHead title={title} />);
+    const breadCrumbs = getByTestId('breadcrumbs');
+    expect(within(breadCrumbs).getByText(title)).toBeInTheDocument();
   });
 
   it('displays the topic title within its header element', () => {
-    const header = instance.findByProps({ 'data-test-id': 'pageTitle' });
-    expect(header.type).toBe('h1');
-    expect(header.props.children).toStrictEqual(title);
+    const { getByRole } = render(<MastHead title={title} />);
+    const header = getByRole('heading', { name: title, level: 1 });
+    expect(header).toBeInTheDocument();
   });
 
   it('contains a tagLine', () => {
-    expect(instance.findByProps({ 'data-test-id': 'tagLine' }).props.children).toBeDefined();
+    const { getByTestId } = render(<MastHead title={title} />);
+
+    expect(getByTestId('tagLine')).toBeInTheDocument();
   });
 
   it('contains a Related Topics section', () => {
-    expect(instance.findByProps({ 'data-test-id': 'relatedTopics' }).props.children).toBeDefined();
+    const { getByTestId } = render(<MastHead title={title} />);
+
+    expect(getByTestId('relatedTopics')).toBeInTheDocument();
   });
 });
