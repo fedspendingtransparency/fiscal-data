3fee2c1a58a75dc2a5ea212de86c74fab65c4e74
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2025-05-05 14:55:49 -0500

    FDG-10438 wip

--- src/components/api-documentation/getting-started/getting-started.spec.js
+++ src/components/api-documentation/getting-started/getting-started.spec.js
@@ -1,58 +1,60 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import GettingStarted from './getting-started';
-import SectionContent from '../section-content/section-content';
+import { render } from '@testing-library/react';
 
 describe('Getting Started', () => {
-  let component = renderer.create();
-  renderer.act(() => {
-    component = renderer.create(<GettingStarted />);
-  });
-  const instance = component.root;
-
   it('has SectionContent as a part of its layout', () => {
-    expect(instance.findAllByType(SectionContent).length).toBeGreaterThan(0);
+    const { getAllByTestId } = render(<GettingStarted />);
+
+    expect(getAllByTestId('section-content').length).toBeGreaterThan(0);
   });
 
   it('creates the Getting Started section with the desired id, heading tag and title', () => {
     const title = 'Getting Started';
-    const heading = instance.findByProps({ id: 'getting-started' }).findByType('h2');
-    expect(heading.children[0]).toBe(title);
+    const { getByRole } = render(<GettingStarted />);
+    const heading = getByRole('heading', { level: 2, name: title });
+    expect(heading).toBeInTheDocument();
   });
 
   it('creates the "What is an API?" section with the desired id, heading tag and title', () => {
     const title = 'What is an API?';
-    const heading = instance.findByProps({ id: 'what-is-an-api' }).findByType('h3');
-    expect(heading.children[0]).toBe(title);
+    const { getByRole } = render(<GettingStarted />);
+    const heading = getByRole('heading', { level: 3, name: title });
+    expect(heading).toBeInTheDocument();
   });
 
   it('creates the "What is a Dataset?" section with the desired id, heading tag and title', () => {
     const title = 'What is a Dataset?';
-    const heading = instance.findByProps({ id: 'what-is-a-dataset' }).findByType('h3');
-    expect(heading.children[0]).toBe(title);
+    const { getByRole } = render(<GettingStarted />);
+    const heading = getByRole('heading', { level: 3, name: title });
+    expect(heading).toBeInTheDocument();
   });
 
   it('creates the API Endpoint section with the desired id, heading tag and title', () => {
     const title = 'API Endpoint URL Structure';
-    const heading = instance.findByProps({ id: 'api-endpoint-url-structure' }).findByType('h3');
-    expect(heading.children[0]).toBe(title);
+    const { getByRole } = render(<GettingStarted />);
+    const heading = getByRole('heading', { level: 3, name: title });
+    expect(heading).toBeInTheDocument();
   });
 
   it('creates the How to Access our API section with the desired id, heading tag and title', () => {
     const title = 'How to Access our API';
-    const heading = instance.findByProps({ id: 'how-to-access-our-api' }).findByType('h3');
-    expect(heading.children[0]).toBe(title);
+    const { getByRole } = render(<GettingStarted />);
+    const heading = getByRole('heading', { level: 3, name: title });
+    expect(heading).toBeInTheDocument();
   });
 
   it('creates the License and Authorization section with the desired id, heading tag and title', () => {
     const title = 'License and Authorization';
-    const heading = instance.findByProps({ id: 'license-and-authorization' }).findByType('h3');
-    expect(heading.children[0]).toBe(title);
+    const { getByRole } = render(<GettingStarted />);
+    const heading = getByRole('heading', { level: 3, name: title });
+    expect(heading).toBeInTheDocument();
   });
 
   it('creates the API Versioning section with the desired id, heading tag and title', () => {
     const title = 'API Versioning';
-    const heading = instance.findByProps({ id: 'api-versioning' }).findByType('h3');
-    expect(heading.children[0]).toBe(title);
+    const { getByRole } = render(<GettingStarted />);
+    const heading = getByRole('heading', { level: 3, name: title });
+    expect(heading).toBeInTheDocument();
   });
 });
--- src/components/datasets/filters/filterPublisherWrapper/filterPublisher.spec.js
+++ src/components/datasets/filters/filterPublisherWrapper/filterPublisher.spec.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import FilterPublisher from './filterPublisher';
-import renderer from 'react-test-renderer';
+import { render, within } from '@testing-library/react';
+
 describe('Filter Dataset Publisher', () => {
-  let component = renderer.create();
-  renderer.act(() => {
-    component = renderer.create(
+  it('renders an element with the test id "publisher-filter-wrapper', () => {
+    const { getByTestId } = render(
       <FilterPublisher
         filterList={[
           { groupId: 'publisher', id: 'FA' },
@@ -15,24 +15,31 @@ describe('Filter Dataset Publisher', () => {
         Test Filter Publisher Wrapper
       </FilterPublisher>
     );
-  });
-  const instance = component.root;
-
-  it('renders an element with the test id "publisher-filter-wrapper', () => {
-    const publisherFilterWrapper = instance.findByProps({ 'data-testid': 'publisher-filter-wrapper' });
-    expect(publisherFilterWrapper).toBeDefined();
+    const publisherFilterWrapper = getByTestId('publisher-filter-wrapper');
+    expect(publisherFilterWrapper).toBeInTheDocument();
   });
 
   it('contains an outer container that calculates and displays a the number of publishers with the heading "Treasury Department"', () => {
-    const outerLabel = instance.findByProps({ 'data-testid': 'publisher-outer-container-label' });
-    expect(outerLabel).toBeDefined();
-    const publisherCount = outerLabel.findByProps({ 'data-testid': 'publisher-count' });
-    expect(publisherCount).toBeDefined();
-    expect(publisherCount.children[1]).toEqual('2 Publishers');
-    const outerContainer = instance.findByProps({ 'data-testid': 'publisher-outer-container' });
-    expect(outerContainer).toBeDefined();
-    const treasuryLabel = outerContainer.findByProps({ 'data-testid': 'treasury-label' });
-    expect(treasuryLabel.children[0]).toEqual('Treasury Department');
+    const { getByTestId } = render(
+      <FilterPublisher
+        filterList={[
+          { groupId: 'publisher', id: 'FA' },
+          { groupId: 'non-publisher', id: 'NP' },
+          { groupId: 'publisher', id: 'RSS' },
+        ]}
+      >
+        Test Filter Publisher Wrapper
+      </FilterPublisher>
+    );
+    const outerLabel = getByTestId('publisher-outer-container-label');
+    expect(outerLabel).toBeInTheDocument();
+    const publisherCount = within(outerLabel).getByTestId('publisher-count');
+    expect(publisherCount).toBeInTheDocument();
+    expect(within(publisherCount).getByText('2 Publishers')).toBeInTheDocument();
+    const outerContainer = getByTestId('publisher-outer-container');
+    expect(outerContainer).toBeInTheDocument();
+    const treasuryLabel = within(outerContainer).getByTestId('treasury-label');
+    expect(within(treasuryLabel).getByText('Treasury Department')).toBeInTheDocument();
   });
 
   it('contains an inner container that also calculates and displays a the number of publishers with the heading "Bureau of Fiscal Service"', () => {
@@ -48,6 +55,17 @@ describe('Filter Dataset Publisher', () => {
   });
 
   it('contains a child inside wrapper', () => {
-    expect(instance.props.children).toEqual('Test Filter Publisher Wrapper');
+    const { getByText } = render(
+      <FilterPublisher
+        filterList={[
+          { groupId: 'publisher', id: 'FA' },
+          { groupId: 'non-publisher', id: 'NP' },
+          { groupId: 'publisher', id: 'RSS' },
+        ]}
+      >
+        Test Filter Publisher Wrapper
+      </FilterPublisher>
+    );
+    expect(getByText('Test Filter Publisher Wrapper')).toBeInTheDocument();
   });
 });
