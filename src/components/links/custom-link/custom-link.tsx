c964a3ccb1e6a4bca00edc343498252f73234566
chasls2 <110061741+chasls2@users.noreply.github.com>
2025-05-08 08:41:11 -0500

    FDG-10490

--- src/components/links/custom-link/custom-link.tsx
+++ src/components/links/custom-link/custom-link.tsx
@@ -42,7 +42,7 @@ const CustomLink: FunctionComponent<CustomLinkProps> = ({
   const urlSplit = thisurl.split('/');
   const pageName = urlSplit[urlSplit.length - 2];
   const explainerPageName = analyticsEventMap[pageName];
-
+  const PROD_ROOT = 'https://fiscaldata.treasury.gov';
   const { getGAEvent } = useGAEventTracking(null, explainerPageName);
 
   const onClickEventHandler = () => {
@@ -60,21 +60,28 @@ const CustomLink: FunctionComponent<CustomLinkProps> = ({
   };
 
   useEffect(() => {
-    const curPath = url || href;
+    let curPath = url || href;
     if (!curPath) return;
 
     if (curPath !== urlOrHref) {
       setUrlOrHref(curPath);
     }
-    const externalUrl: RegExp = /^external:/;
-    if (!ext && externalUrl.test(curPath)) {
-      setExt(true);
-      setUrlOrHref(curPath.replace(externalUrl, ''));
+
+    if (curPath.startsWith(PROD_ROOT)) {
+      curPath = curPath.replace(PROD_ROOT, '') || '/';
     }
+
+    const externalPrefix = /^external:/;
+    if (externalPrefix.test(curPath)) {
+      curPath = curPath.replace(externalPrefix, '');
+      setExt(true);
+    }
+
+    if (curPath !== urlOrHref) setUrlOrHref(curPath);
   }, [ext, url, href]);
 
   const isAbsolute = urlOrHref.startsWith('http');
-  const isSameSite = isAbsolute && urlOrHref.startsWith('https://fiscaldata.treasury.gov/');
+  const isSameSite = !isAbsolute || (typeof window !== 'undefined' && isAbsolute && new URL(urlOrHref).hostname === window.location.hostname);
 
   const treatAsExternal = (ext ?? false) || (isAbsolute && !isSameSite && ['http', 'tel'].some(p => urlOrHref.startsWith(p)));
 
