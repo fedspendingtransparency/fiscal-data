f51bd8c62da32f40aaf2e2925fba13e6c6a42b46
Noah_Simcoe <noah.simcoe@kc.frb.org>
2025-06-10 12:31:06 -0500

    FDG-9790 wip

--- src/components/data-preview/data-preview-filter-section/data-preview-table-filters/column-filter-options/date-column-filter/custom-date-filter/custom-date-filter.spec.js
+++ src/components/data-preview/data-preview-filter-section/data-preview-table-filters/column-filter-options/date-column-filter/custom-date-filter/custom-date-filter.spec.js
@@ -1,10 +1,14 @@
 import { render, within } from '@testing-library/react';
 import React from 'react';
 import CustomDateFilter from './custom-date-filter';
+import userEvent from '@testing-library/user-event';
 
 describe('Date column filter', () => {
   const mockPickerDateRange1 = { from: new Date('3/17/2024'), to: new Date('3/17/2025'), latestDate: '3-17-2020', earliestDate: '3-17-2025' };
   const mockPickerDateRange2 = { from: new Date('3/17/2025'), to: new Date('3/16/2025'), latestDate: '3-17-2020', earliestDate: '3-17-2025' };
+  const mockColumnConfig1 = { defaultEndDate: new Date('3/17/2025'), defaultStartDate: new Date('3/17/2024') };
+  const mockColumnConfig2 = { defaultEndDate: new Date('3/16/2025'), defaultStartDate: new Date('3/17/2025') };
+
   it('renders start and end date pickers', () => {
     const { getByRole } = render(<CustomDateFilter pickerDateRange={mockPickerDateRange1} />);
     const startDatePicker = getByRole('button', { name: 'Select Start Date' });
@@ -14,7 +18,7 @@ describe('Date column filter', () => {
   });
 
   it('defaults date pickers to pickerDateRange value', () => {
-    const { getByRole } = render(<CustomDateFilter pickerDateRange={mockPickerDateRange1} />);
+    const { getByRole } = render(<CustomDateFilter pickerDateRange={mockPickerDateRange1} columnConfig={mockColumnConfig1} />);
     const startDatePicker = getByRole('button', { name: 'Select Start Date' });
     const endDatePicker = getByRole('button', { name: 'Select End Date' });
     expect(within(startDatePicker).getByText('March 17, 2024')).toBeInTheDocument();
@@ -30,10 +34,21 @@ describe('Date column filter', () => {
   });
 
   it('swaps the dates if the From date is greater than the To date', () => {
-    const { getByRole } = render(<CustomDateFilter pickerDateRange={mockPickerDateRange2} />);
+    const { getByRole } = render(<CustomDateFilter pickerDateRange={mockPickerDateRange2} columnConfig={mockColumnConfig2} />);
     const startDateButton = getByRole('button', { name: 'Select Start Date' });
     const endDateButton = getByRole('button', { name: 'Select End Date' });
     within(startDateButton).getByText('March 16, 2025');
     within(endDateButton).getByText('March 17, 2025');
+  });
+
+  it('correctly updates the start date', async () => {
+    const { getByRole } = render(<CustomDateFilter pickerDateRange={mockPickerDateRange2} columnConfig={mockColumnConfig2} />);
+    const startDateButton = getByRole('button', { name: 'Select Start Date' });
+    await userEvent.click(startDateButton);
+    const newDateBox = getByRole('gridcell', { name: 2 });
+    await userEvent.click(newDateBox);
+    const applyButton = getByRole('button', { name: 'Apply Selected Date' });
+    await userEvent.click(applyButton);
+    within(startDateButton).getByText('test');
   });
 });
