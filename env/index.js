28cab87279d15ec7c157ea76a44c4dd1aa431ce2
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2025-06-10 07:30:30 -0500

    FDG-10177 Add lower env feature wrapper

--- env/index.js
+++ env/index.js
@@ -7,6 +7,7 @@
 /*
   Supported entries include:
   EXPERIMENTAL_WHITELIST - [ 'feature-id' ] // array of as-needed feature-ids whose <Experimental /> wrappers should be respected
+  LOWER_ENV_FEATURE_WHITELIST - [ 'feature-id' ] // array of as-needed feature-ids whose <LowerEnvironmentFeature /> wrappers should be respected
   ADDITIONAL_DATASETS - {structured object keyed by Id following form in transform/static-metadata/datasets.json}
   EXCLUDED_ENDPOINT_IDS - {simple array of endpoint IDs to exclude, eg: ['27', '94'];
   ADDITIONAL_ENDPOINTS - {structured object keyed by Id following form of endpointConfig in transform/endpointConfig.js}
