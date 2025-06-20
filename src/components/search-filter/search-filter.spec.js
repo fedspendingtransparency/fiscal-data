b406c5eee7fe7919bd2116f8aed28cb9215e4c46
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2025-06-20 17:00:00 -0500

    FDG-9790 Update string value tests

--- src/components/search-filter/search-filter.spec.js
+++ src/components/search-filter/search-filter.spec.js
@@ -4,22 +4,22 @@ import React from 'react';
 import userEvent from '@testing-library/user-event';
 
 describe('Search Filter', () => {
-  const setFilter = jest.fn();
   it('renders a search bar', () => {
-    const { getByRole } = render(<SearchFilter setFilter={setFilter} filter="" searchLabel="Enter filter term" />);
+    const { getByRole } = render(<SearchFilter columnConfig={{ pendingValue: '' }} searchLabel="Enter filter term" />);
     expect(getByRole('textbox')).toBeInTheDocument();
   });
 
   it('renders the header', () => {
-    const { getByText } = render(<SearchFilter setFilter={setFilter} filter="" searchLabel="Enter filter term" header={'Description'} />);
+    const { getByText } = render(<SearchFilter columnConfig={{ pendingValue: '' }} searchLabel="Enter filter term" header={'Description'} />);
     expect(getByText('Description')).toBeInTheDocument();
   });
 
   it('updates list on search text change', () => {
-    const { getByRole } = render(<SearchFilter setFilter={setFilter} filter="" searchLabel="Enter filter term" header={'Description'} />);
+    const mockConfig = { pendingValue: '' };
+    const { getByRole } = render(<SearchFilter columnConfig={mockConfig} searchLabel="Enter filter term" header={'Description'} />);
     const searchBar = getByRole('textbox');
     userEvent.click(searchBar);
     userEvent.keyboard('search filter text...');
-    expect(setFilter).toHaveBeenCalled();
+    expect(mockConfig.pendingValue).toBe('search filter text...');
   });
 });
