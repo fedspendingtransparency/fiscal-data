7146dcaee22ff02c540c8d79741df1ff0e7cba51
Noah_Simcoe <noah.simcoe@kc.frb.org>
2025-06-26 11:30:50 -0500

    FDG-10531 added values for total slgs and latest date

--- src/layouts/insight/sections/state-and-local-government-series/body-copy/body-copy.tsx
+++ src/layouts/insight/sections/state-and-local-government-series/body-copy/body-copy.tsx
@@ -1,26 +1,42 @@
-import React, { ReactElement } from 'react';
+import React, { useState, ReactElement, useEffect } from 'react';
 import { apiPrefix, basicFetch } from '../../../../../utils/api-utils';
-
-export const getLastDayOfEachMonth = async () => {
-  // eslint-disable-next-line max-len
-  return basicFetch(
-    `${apiPrefix}v1/accounting/od/slgs_securities?fields=record_date,record_calendar_month,record_calendar_day,record_calendar_year&sort=-record_date`
-  );
-};
-
-export const getSlgsTotal = async () => {
-  return getLastDayOfEachMonth().then(res => {
-    const lastUpdated = res.data[0].record_date;
-    return basicFetch(
-      `${apiPrefix}v1/accounting/od/slgs_securities?fields=record_date,outstanding_0_3_mos_cnt,outstanding_0_3_mos_amt,outstanding_3_6_mos_cnt,outstanding_3_6_mos_amt,outstanding_6_mos_to_2_yrs_cnt,outstanding_6_mos_to_2_yrs_amt,outstanding_2_5_yrs_cnt,outstanding_2_5_yrs_amt,outstanding_5_10_yrs_cnt,outstanding_5_10_yrs_amt,outstanding_over_10_yrs_cnt,outstanding_over_10_yrs_amt,record_calendar_month,record_calendar_day,record_calendar_year&filter=record_date:eq:${lastUpdated}&sort=-record_date`
-    ).then(res => {
-      const slgsTotal = res.data[0].outstanding_0_3_mos_amt;
-      return `the test amount is ${slgsTotal}`;
-    });
-  });
-};
+import { format } from 'date-fns';
+import { getDateWithoutTimeZoneAdjust } from '../../../../../utils/date-utils';
 
 export const BodyCopy = (): ReactElement => {
+  const [lastUpdated, setLastUpdated] = useState(null);
+  const [slgsTotal, setSlgsTotal] = useState(null);
+
+  useEffect(() => {
+    basicFetch(
+      `${apiPrefix}v1/accounting/od/slgs_securities?fields=record_date,record_calendar_month,record_calendar_day,record_calendar_year&sort=-record_date`
+    ).then(res => {
+      if (res.data) {
+        const lastUpdatedDate = res.data[0].record_date;
+        const formattedDate = format(getDateWithoutTimeZoneAdjust(res.data[0].record_date), 'MMMM d, yyyy');
+        setLastUpdated(formattedDate);
+
+        basicFetch(
+          `${apiPrefix}v1/accounting/od/slgs_securities?fields=record_date,outstanding_0_3_mos_cnt,outstanding_0_3_mos_amt,outstanding_3_6_mos_cnt,outstanding_3_6_mos_amt,outstanding_6_mos_to_2_yrs_cnt,outstanding_6_mos_to_2_yrs_amt,outstanding_2_5_yrs_cnt,outstanding_2_5_yrs_amt,outstanding_5_10_yrs_cnt,outstanding_5_10_yrs_amt,outstanding_over_10_yrs_cnt,outstanding_over_10_yrs_amt,record_calendar_month,record_calendar_day,record_calendar_year&filter=record_date:eq:${lastUpdatedDate}&sort=-record_date`
+        ).then(res => {
+          if (res.data) {
+            const slgsTotalSum =
+              parseFloat(res.data[0]['outstanding_0_3_mos_amt']) +
+              parseFloat(res.data[0]['outstanding_3_6_mos_amt']) +
+              parseFloat(res.data[0]['outstanding_6_mos_to_2_yrs_amt']) +
+              parseFloat(res.data[0]['outstanding_2_5_yrs_amt']) +
+              parseFloat(res.data[0]['outstanding_5_10_yrs_amt']) +
+              parseFloat(res.data[0]['outstanding_over_10_yrs_amt']);
+            const slgsInBillions = Math.round(slgsTotalSum / 1000000000);
+            setSlgsTotal(slgsInBillions);
+          }
+        });
+      }
+    });
+  }, []);
+
+  console.log(typeof lastUpdated);
+
   return (
     <div>
       State and Local Government Series (SLGS) are non-marketable securities that exist to help state and local governments meet their financing needs
@@ -28,10 +44,8 @@ export const BodyCopy = (): ReactElement => {
       school, there may be a delay between when the funds are acquired through the bond sales and when the city must pay the bills to build the
       school. As a result, state and local governments will invest these funds. The IRS has strict guidelines about how state and local governments
       can invest these funds to ensure they're used properly. SLGS securities are an attractive option because they make it easier to comply with IRS
-      regulations. SLGS also serve the federal government as
-      {/*a means of financing the federal debt, similar to other treasury securities. As of {Month DD, YYYY (latest*/}
-      {/*date for both SLGS and Debt to the Penny data)}, there are {$XXX B (SLGS total)} outstanding SLGS securities,*/}
-      {/*{XX} percent of the total public debt outstanding.*/}
+      regulations. SLGS also serve the federal government as a means of financing the federal debt, similar to other treasury securities. As of{' '}
+      {lastUpdated}, there are ${slgsTotal} B outstanding SLGS securities, XX percent of the total public debt outstanding.
     </div>
   );
 };
