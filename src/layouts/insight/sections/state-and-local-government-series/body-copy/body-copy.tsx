4625ee33973f8a3c4e22b720ef37e78f2caf262b
Noah_Simcoe <noah.simcoe@kc.frb.org>
2025-06-26 13:14:33 -0500

    FDG-10531 api switch, added % metric to body copy

--- src/layouts/insight/sections/state-and-local-government-series/body-copy/body-copy.tsx
+++ src/layouts/insight/sections/state-and-local-government-series/body-copy/body-copy.tsx
@@ -6,15 +6,22 @@ import { getDateWithoutTimeZoneAdjust } from '../../../../../utils/date-utils';
 export const BodyCopy = (): ReactElement => {
   const [lastUpdated, setLastUpdated] = useState(null);
   const [slgsTotal, setSlgsTotal] = useState(null);
+  const [totalPublicDebtOutstanding, setTotalPublicDebtOutstanding] = useState(null);
+  const [percentOfTheTotalPublicDebt, setPercentOfTheTotalPublicDebt] = useState(null);
 
   useEffect(() => {
-    basicFetch(
-      `${apiPrefix}v1/accounting/od/slgs_securities?fields=record_date,record_calendar_month,record_calendar_day,record_calendar_year&sort=-record_date`
-    ).then(res => {
+    basicFetch(`${apiPrefix}v2/accounting/od/debt_to_penny?sort=-record_date`).then(res => {
       if (res.data) {
-        const lastUpdatedDate = res.data[0].record_date;
-        const formattedDate = format(getDateWithoutTimeZoneAdjust(res.data[0].record_date), 'MMMM d, yyyy');
+        const lastUpdatedDate = res.data[1].record_date;
+        const formattedDate = format(getDateWithoutTimeZoneAdjust(lastUpdatedDate), 'MMMM d, yyyy');
         setLastUpdated(formattedDate);
+
+        basicFetch(`${apiPrefix}v2/accounting/od/debt_to_penny?filter=record_date:eq:${lastUpdatedDate}`).then(res => {
+          if (res.data) {
+            const totalDebtHeld = res.data[0]['tot_pub_debt_out_amt'];
+            setTotalPublicDebtOutstanding(totalDebtHeld);
+          }
+        });
 
         basicFetch(
           `${apiPrefix}v1/accounting/od/slgs_securities?fields=record_date,outstanding_0_3_mos_cnt,outstanding_0_3_mos_amt,outstanding_3_6_mos_cnt,outstanding_3_6_mos_amt,outstanding_6_mos_to_2_yrs_cnt,outstanding_6_mos_to_2_yrs_amt,outstanding_2_5_yrs_cnt,outstanding_2_5_yrs_amt,outstanding_5_10_yrs_cnt,outstanding_5_10_yrs_amt,outstanding_over_10_yrs_cnt,outstanding_over_10_yrs_amt,record_calendar_month,record_calendar_day,record_calendar_year&filter=record_date:eq:${lastUpdatedDate}&sort=-record_date`
@@ -27,15 +34,16 @@ export const BodyCopy = (): ReactElement => {
               parseFloat(res.data[0]['outstanding_2_5_yrs_amt']) +
               parseFloat(res.data[0]['outstanding_5_10_yrs_amt']) +
               parseFloat(res.data[0]['outstanding_over_10_yrs_amt']);
-            const slgsInBillions = Math.round(slgsTotalSum / 1000000000);
-            setSlgsTotal(slgsInBillions);
+            setSlgsTotal(slgsTotalSum);
+
+            const num = (slgsTotal / totalPublicDebtOutstanding) * 100;
+            const percentOfTheTotalPublicDebtOutstanding = Math.round(num * 100) / 100;
+            setPercentOfTheTotalPublicDebt(percentOfTheTotalPublicDebtOutstanding);
           }
         });
       }
     });
   }, []);
-
-  console.log(typeof lastUpdated);
 
   return (
     <div>
@@ -45,7 +53,8 @@ export const BodyCopy = (): ReactElement => {
       school. As a result, state and local governments will invest these funds. The IRS has strict guidelines about how state and local governments
       can invest these funds to ensure they're used properly. SLGS securities are an attractive option because they make it easier to comply with IRS
       regulations. SLGS also serve the federal government as a means of financing the federal debt, similar to other treasury securities. As of{' '}
-      {lastUpdated}, there are ${slgsTotal} B outstanding SLGS securities, XX percent of the total public debt outstanding.
+      {lastUpdated}, there are ${Math.round(slgsTotal / 1000000000)} B outstanding SLGS securities, {percentOfTheTotalPublicDebt} percent of the total
+      public debt outstanding.
     </div>
   );
 };
