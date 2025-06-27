87f9f9a6bf0cc14ef3948a0cbd9d79c57384a0af
Noah_Simcoe <noah.simcoe@kc.frb.org>
2025-06-27 11:36:53 -0500

    FDG-10531 removing '<1' text

--- src/layouts/explainer/explainer-test-helper.js
+++ src/layouts/explainer/explainer-test-helper.js
@@ -816,15 +816,21 @@ const mockPreviousYearDate = { data: [{ record_fiscal_year: 2024, record_date: '
 export const mockSavingsBondFetchResponses = () => {
   fetchMock.get(
     `https://www.transparency.treasury.gov/services/api/fiscal_service/v1/accounting/od/securities_sales?filter=security_type_desc:eq:Savings%20Bond&sort=-record_date&page[size]=1`,
-    mockSavingsBondDataNoFilter
+    mockSavingsBondDataNoFilter,
+    { overwriteRoutes: true },
+    { repeat: 1 }
   );
   fetchMock.get(
     `https://www.transparency.treasury.gov/services/api/fiscal_service/v1/accounting/od/securities_sales?filter=security_type_desc:eq:Savings%20Bond,record_fiscal_year:eq:2024`,
-    mockSavingsBondCurrentFY
+    mockSavingsBondCurrentFY,
+    { overwriteRoutes: true },
+    { repeat: 1 }
   );
   fetchMock.get(
     `begin:https://www.transparency.treasury.gov/services/api/fiscal_service/v1/accounting/od/securities_sales?filter=security_type_desc:eq:Savings%20Bond,record_fiscal_year:eq:2023,record_calendar_month:eq:`,
-    mockSavingsBondLastFiscalYearCurrentMonth
+    mockSavingsBondLastFiscalYearCurrentMonth,
+    { overwriteRoutes: true },
+    { repeat: 1 }
   );
   fetchMock.get(
     `begin:https://www.transparency.treasury.gov/services/api/fiscal_service/v1/accounting/od/securities_sales?filter=security_type_desc:eq:Savings%20Bond&page[size]=`,
