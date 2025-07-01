dd4d76cca8a58a77111d856506c4eebb98b515cc
chasls2 <110061741+chasls2@users.noreply.github.com>
2025-07-01 15:34:28 -0500

    FDG-9500

--- src/components/published-reports/filter-reports-section/filter-report-section.spec.js
+++ src/components/published-reports/filter-reports-section/filter-report-section.spec.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import FilterReportsSection from './filter-reports-section';
 import { runTimeFilterDatasetConfig } from '../published-reports-test-helper';
+
+const apiMock = {
+  apiId: 302,
+  dateFiled: 'recordDate',
+  earliestDate: '2024-07-04',
+  latestDate: '2024-07-01',
+  tableName: 'mockTable',
+};
 
 describe('Run Time Filter Report Section', () => {
   it('should render an empty table by default', () => {
@@ -9,5 +17,22 @@ describe('Run Time Filter Report Section', () => {
     const { defaultHeader, defaultMessage } = runTimeFilterDatasetConfig.runTimeReportConfig;
     expect(getByText(defaultHeader)).toBeInTheDocument();
     expect(getByText(defaultMessage)).toBeInTheDocument();
+  });
+  it('show the date picker when apis array supplied', () => {
+    render(<FilterReportsSection reportConfig={runTimeFilterDatasetConfig.runTimeReportConfig} apis={[apiMock]} width={1024} />);
+    expect(screen.getByRole('button', { name: /Published Date/i })).toBeInTheDocument();
+  });
+  it('keep "(none selected)" until user picks an account', () => {
+    render(
+      <FilterReportsSection
+        reportConfig={{ ...runTimeFilterDatasetConfig.runTimeReportConfig, optionValues: ['1234', '5678'] }}
+        apis={[apiMock]}
+        width={1024}
+      />
+    );
+    expect(screen.getByRole('button', { name: /\(None selected\)/i })).toBeInTheDocument();
+    fireEvent.click(screen.getByRole('button', { name: /Account/i }));
+    fireEvent.click(screen.getByText('1234'));
+    expect(screen.getByRole('button', { name: '1234' })).toBeInTheDocument();
   });
 });
