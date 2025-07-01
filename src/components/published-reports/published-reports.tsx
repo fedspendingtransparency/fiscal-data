56e351cac6fa04d5218da7b7cae479986cd6d87d
chasls2 <110061741+chasls2@users.noreply.github.com>
2025-07-01 15:06:48 -0500

    FDG-9500

--- src/components/published-reports/published-reports.tsx
+++ src/components/published-reports/published-reports.tsx
@@ -9,7 +9,6 @@ import LowerEnvironmentFeature from '../lower-environment-feature/lower-environm
 export const sectionTitle = 'Reports and Files';
 
 const PublishedReports: FunctionComponent<{ pageConfig: IDatasetConfig }> = ({ pageConfig }) => {
-  console.log(pageConfig);
   return (
     <>
       {pageConfig.reportGenKey && (
