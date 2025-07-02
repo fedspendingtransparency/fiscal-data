848a37d7b3a20ebb5093af44b3ebe9d1dfa3a448
Noah_Simcoe <noah.simcoe@kc.frb.org>
2025-07-02 14:43:53 -0500

    FDG-10533 initializing

---
+++ src/layouts/insight/sections/state-and-local-government-series/state-and-local-government-series-chart/state-and-local-government-series-chart.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const StateAndLocalGovernmentSeriesChart = () => {
+  return (
+    <>
+      <div>
+        <p>This is a test.</p>
+      </div>
+    </>
+  );
+};
