662f7f534ccdf8c6e9da4c8081c6a966d18064bf
Stevens <110061741+chasls2@users.noreply.github.com>
2025-07-24 16:06:27 -0500

    Revert "FDG-10538 removing prop"
    This reverts commit 6c78b2e1a37226df08e0707993c8686766d913b6.

--- src/components/dtg-table/dtg-table.jsx
+++ src/components/dtg-table/dtg-table.jsx
@@ -328,7 +328,7 @@ export default function DtgTable({
     if (!tableProps.data) {
       setCurrentPage(1);
     }
-    if (!tableProps.chartTable) {
+    if (tableProps.chartTable) {
       updateTable(tableProps.data);
     }
   }, [tableProps.data]);
