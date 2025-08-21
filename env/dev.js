126d7846b3cf2ca02fd272369f95265b3f60740f
j1jsh04 <jordan.hertzler@kc.frb.org>
2025-08-21 14:41:15 -0500

    wip

--- env/dev.js
+++ env/dev.js
@@ -5,6 +5,7 @@ module.exports = {
   DATA_DOWNLOAD_BASE_URL: 'https://dev.fiscaldata.treasury.gov',
   WEB_SOCKET_BASE_URL: 'wss://downloads.dev.fiscaldata.treasury.gov/main',
   EXPERIMENTAL_WHITELIST: ['experimental-page', 'not-found-md', 'apiNKL', 'aboutUsMDX', 'publishedReportsSection', 'chartingConfigurationTool'],
+  LOWER_ENV_FEATURE_WHITELIST: ['reportGeneration'],
   ADDITIONAL_DATASETS: {
     '015-BFS-2025Q2-002': {
       slug: '/monthly-treasury-disbursements/',
