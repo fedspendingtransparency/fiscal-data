fa2eaaef83dd4da897d039acc5fd24f3ec1832a6
chasls2 <110061741+chasls2@users.noreply.github.com>
2025-08-27 15:31:49 -0500

    FDG-10695

--- src/layouts/insight/sections/interest-expense/interest-expense-chart/interest-expense-chart.tsx
+++ src/layouts/insight/sections/interest-expense/interest-expense-chart/interest-expense-chart.tsx
@@ -12,7 +12,6 @@ import { ga4DataLayerPush } from '../../../../../helpers/google-analytics/google
 import ChartTableContainer from '../../../../../components/chart-with-table/chart-table-container/chart-table-container';
 import DtgTable from '../../../../../components/dtg-table/dtg-table';
 import { chartTableBoarder } from './interest-expense-chart.module.scss';
-import { SortingState } from '../../../insight-helper';
 import { useRecoilValue } from 'recoil';
 import { smallTableDownloadDataCSV } from '../../../../../recoil/smallTableDownloadData';
 
@@ -46,7 +45,7 @@ const InterestExpenseChart = () => {
   const [curRate, setCurRate] = useState<number>(0);
   const [chartFocus, setChartFocus] = useState<boolean>(false);
   const [chartHover, setChartHover] = useState<boolean>(false);
-  const [sorting, setSorting] = useState<SortingState>([]);
+  const [sorting, setSorting] = useState([]);
   const [downloadData, setDownloadData] = useState([]);
   const chartTitle = `Interest Expense and Average Interest Rates on the National Debt FY ${startFY} - FYTD ${currentFY}`;
   const tableCSVData = useRecoilValue(smallTableDownloadDataCSV);
