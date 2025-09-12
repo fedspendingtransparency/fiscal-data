b94ae5e402c750fc18072485f7f13e74064345b8
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2025-09-12 12:27:28 -0500

    FDG-9939 refactor custom bar

--- src/layouts/explainer/sections/overview/debt-chart/custom-bar-shape/custom-bar-shape.tsx
+++ src/layouts/explainer/sections/overview/debt-chart/custom-bar-shape/custom-bar-shape.tsx
@@ -1,42 +1,52 @@
 import React from 'react';
 
+const getBarSizes = (width, countDown) => {
+  const splitWidth = (width / countDown).toFixed(1);
+  const totalWidth = splitWidth * 10;
+  const barWidth = 6.3; //(1 / 13.3) * totalWidth;
+  const gapWidth = 1.89; //0.3 * bar;
+  return { barWidth, gapWidth };
+};
+
 const CustomBarShape = props => {
   const { height, width, y, x, fill, payload, dataKey, year } = props;
 
-  const barType = dataKey.includes('debt') ? 'debt' : 'deficit';
+  const debtBar = dataKey.includes('debt');
+  const deficitBar = dataKey.includes('deficit');
+
   const debtVal = props[`debt${year}`];
   const debtRemainder = debtVal % 1;
-  const debtIndexRemainder = (debtVal + (1 - debtRemainder)) % 10;
-  let countDown = payload[dataKey];
+  const deficitInitialIndex = (debtVal + (1 - debtRemainder)) % 10;
 
-  const splitWidth = (width / countDown).toFixed(1);
-  const totalWidth = splitWidth * 10;
-  const bar = 6.3; //(1 / 13.3) * totalWidth;
-  const gapWidth = 1.89; //0.3 * bar;
+  const { barWidth, gapWidth } = getBarSizes(width, payload[dataKey]);
   if (width > 0) {
     let xVal = x;
-    const firstBar = bar * (1 - debtRemainder);
-    const axisGap = debtIndexRemainder % 10 === 0 ? gapWidth : 0;
+    const firstBar = barWidth * (1 - debtRemainder);
+    const axisGap = deficitInitialIndex % 10 === 0 ? gapWidth : 0;
     const allBars = [];
-    if (barType === 'deficit') {
+    let remainingBars = payload[dataKey];
+    if (deficitBar) {
+      // first deficit bar will be split from the final debt bar
       allBars.push({ x: xVal, width: firstBar });
       xVal = xVal + firstBar + axisGap + gapWidth;
-      countDown = countDown - (1 - debtRemainder);
+      remainingBars = remainingBars - (1 - debtRemainder);
+      console.log(payload[dataKey], remainingBars, debtRemainder);
     } else {
+      //first debt bar will start with a gap
       xVal = xVal + gapWidth;
     }
-    let barCount = barType === 'debt' ? 0 : debtIndexRemainder;
-    while (countDown >= 1) {
+    let barCount = debtBar ? 0 : deficitInitialIndex;
+    while (remainingBars >= 1) {
       barCount = barCount + 1;
-      const debtAxisGap = barCount !== 0 && barCount % 10 === 0 ? gapWidth : 0;
-      const deficitAxisGap = barCount % 10 === 0 ? gapWidth : 0;
-      const axisGap = barType === 'debt' ? debtAxisGap : deficitAxisGap;
-      allBars.push({ x: xVal, width: bar });
-      xVal = xVal + bar + gapWidth + axisGap;
-      countDown = countDown - 1;
+      allBars.push({ x: xVal, width: barWidth });
+      xVal = xVal + barWidth + gapWidth;
+      if (barCount !== 0 && barCount % 10 === 0) {
+        //Add twice the gap around the x-axis bars
+        xVal = xVal + gapWidth;
+      }
+      remainingBars = remainingBars - 1;
     }
-    console.log(bar, countDown);
-    allBars.push({ x: xVal, width: bar * countDown });
+    allBars.push({ x: xVal, width: barWidth * remainingBars });
 
     return (
       <>
