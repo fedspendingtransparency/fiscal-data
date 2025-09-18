7b0789a462abb9fd90f702f7c2e9734bd3d163c9
Jordan Dachroeden <jordan.dachroeden@kc.frb.org>
2025-09-18 09:57:33 -0500

    FDG-10796 re add hook

--- src/components/published-reports/download-report-table/download-report-table-row/gen-report-download-button.tsx
+++ src/components/published-reports/download-report-table/download-report-table-row/gen-report-download-button.tsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
-import { renderPDF } from '../../../../workers/pdfWorker';
-import DownloadContents from './button-contents';
+import React, { useEffect, useState } from 'react';
 import { fileDescription } from './download-report-table-row.module.scss';
 import { getGeneratedReportFileDisplay } from '../../util/util';
 import { IGeneratedReport } from './download-report-table-row';
+import { useRenderPDF } from './useRenderPDF';
+import DownloadContents from './button-contents';
 
 const GenReportDownloadButton = ({ generatedReport, fileDisplay, setIsLoading, loadingRef, selectedAccount }) => {
   const [displayName, setDisplayName] = useState(null);
   const [publishedDate, setPublishedDate] = useState(null);
-
-  const workerRef = useRef(null);
-  const [res, setRes] = useState(null);
-  const [refSet, setRefSet] = useState(false);
+  const { url, loading, error } = useRenderPDF(generatedReport);
+  // const workerRef = useRef(null);
+  // const [res, setRes] = useState(null);
+  // const [refSet, setRefSet] = useState(false);
 
   //TODO: test removing the ref from the worker
 
@@ -37,63 +37,20 @@ const GenReportDownloadButton = ({ generatedReport, fileDisplay, setIsLoading, l
     // setFileType(null);
   }, [selectedAccount]);
 
-  useEffect(() => {
-    console.log(workerRef);
-    // if (workerRef.current) {
-    loadingRef.current = true;
-    // setIsLoading(true);
-    workerRef.current = renderPDF;
-    setRefSet(true);
-    // }
-    return () => {
-      console.log('unmounting ********************');
-      // if (workerRef.current) {
-      //   workerRef.current.terminate();
-      //   workerRef.current = null; // Clear the ref
-      // }
-    };
-  }, [generatedReport]);
-
-  useEffect(() => {
-    (async () => {
-      if (refSet) {
-        try {
-          const test = await workerRef.current.renderPDFInWorker({ report: generatedReport });
-          console.log('??', test);
-          setRes(test);
-          // loadingRef.current = false;
-          // setIsLoading(false);
-        } catch (e) {
-          console.log(e);
-        }
-      }
-    })();
-    return () => {
-      console.log('unmounting xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
-    };
-  }, [refSet]);
-
-  useMemo(() => {
-    if (res) {
-      const { value } = res;
-      return value ? () => URL.revokeObjectURL(value) : undefined;
-    }
-  }, [res]);
-
   return (
     <>
       {/*{displayName && (*/}
       <tr className={fileDescription} data-testid="file-download-row">
         <td>
           <div style={{ minHeight: '44px' }}>
-            {res?.url && res?.size && (
+            {url?.url && url?.size && (
               <>
                 <DownloadContents
-                  size={res.size}
+                  size={url.size}
                   publishedDate={publishedDate}
                   fileDisplay={fileDisplay}
                   displayName={displayName}
-                  url={res.url}
+                  url={url.url}
                   download={generatedReport.downloadName}
                 />
                 {setIsLoading(false)}
--- src/components/published-reports/download-report-table/download-report-table-row/useRenderPDF.ts
+++ src/components/published-reports/download-report-table/download-report-table-row/useRenderPDF.ts
@@ -1,37 +1,12 @@
-import { useMemo, useRef, useState } from 'react';
+import { useEffect } from 'react';
 import { renderPDF } from '../../../../workers/pdfWorker';
 import { useAsync } from 'react-use';
 
 export const useRenderPDF = report => {
-  const workerRef = useRef(null);
-  const [res, setRes] = useState({});
-
-  useMemo(async () => {
-    if (!workerRef.current) {
-      workerRef.current = renderPDF;
-      const test = await workerRef.current.renderPDFInWorker({ report });
-      console.log('??', test);
-      setRes(test);
-    }
-    return () => {
-      if (workerRef.current) {
-        workerRef.current.terminate();
-        workerRef.current = null; // Clear the ref
-      }
-    };
-  }, []);
-
-  const { value, loading, error } = useAsync(async () => {
-    console.log('generating pdf*******************');
+  const { value: url, loading, error } = useAsync(async () => {
     return renderPDF({ report });
   }, [report]);
 
-  useMemo(() => {
-    if (res) {
-      const { value } = res;
-
-      return value ? () => URL.revokeObjectURL(value) : undefined;
-    }
-  }, [res]);
-  return res;
+  useEffect(() => (url ? () => URL.revokeObjectURL(url) : undefined), [url]);
+  return { url, loading, error };
 };
