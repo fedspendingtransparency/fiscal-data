d93ce0c1cd25b56938c81bdfa69e5f22058f76a5
chasls2 <110061741+chasls2@users.noreply.github.com>
2025-10-17 08:01:28 -0500

    FDG-10893

--- gatsby-config.js
+++ gatsby-config.js
@@ -90,18 +90,18 @@ module.exports = {
       },
     },
     'gatsby-plugin-dts-css-modules',
-    // {
-    //   resolve: 'gatsby-plugin-eslint',
-    //   options: {
-    //     test: /\.js$|\.jsx$/,
-    //     exclude: /(node_modules|.cache|public)/,
-    //     stages: ['develop'],
-    //     options: {
-    //       emitWarning: true,
-    //       failOnError: false,
-    //     },
-    //   },
-    // },
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        test: /\.js$|\.jsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: ['develop'],
+        options: {
+          emitWarning: true,
+          failOnError: false,
+        },
+      },
+    },
     'gatsby-plugin-material-ui',
     {
       resolve: `gatsby-plugin-manifest`,
