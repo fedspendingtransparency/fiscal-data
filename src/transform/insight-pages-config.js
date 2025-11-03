d1fad6c80ca69ca61da147d0d497c42116cbc159
chasls2 <110061741+chasls2@users.noreply.github.com>
2025-11-03 13:19:57 -0600

    FDG-10990

--- src/transform/insight-pages-config.js
+++ src/transform/insight-pages-config.js
@@ -3,7 +3,7 @@ const insightPagesSource = {
     slug: '/interest-expense-avg-interest-rates/',
     breadCrumbLinkName: 'Interest Expense',
     seoConfig: {
-      pageTitle: 'Interest Expense and Interest Rates | U.S. Treasury Fiscal Data ',
+      pageTitle: 'Interest Expense and Average Interest Rates on the National Debt ',
       description: '',
       keywords: 'interest expense, interest cost, interest payments, U.S. debt interest, interest rates',
     },
