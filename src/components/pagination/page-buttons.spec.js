2c688be0fb709a36d7eedbbff6909f33d7419263
Noah_Simcoe <noah.simcoe@kc.frb.org>
2025-12-03 16:39:26 -0600

    FDG-8056 wip on page button tests

--- src/components/pagination/page-buttons.spec.js
+++ src/components/pagination/page-buttons.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PageButtons from './page-buttons';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import { RecoilRoot } from 'recoil';
 
 describe('PageButtons component', () => {
@@ -85,5 +85,54 @@ describe('PageButtons component', () => {
     expect(firstButton).toBeInTheDocument();
     const lastButton = getByRole('button', { name: `${tableName}-page10` });
     expect(lastButton).toBeInTheDocument();
+  });
+
+  it('correctly changes to a new page when its button-number is clicked', () => {
+    const { getByRole } = render(
+      <RecoilRoot>
+        <PageButtons pageButtonProps={pageButtonProps} />
+      </RecoilRoot>
+    );
+    const secondButton = getByRole('button', { name: `${tableName}-page2` });
+    expect(secondButton).toBeInTheDocument();
+    fireEvent.click(secondButton);
+    expect(secondButton).toHaveClass('active');
+  });
+
+  describe('Page button changes', () => {
+    const pages = [1, 2, 3, 4, 5, 6];
+    const pageButtonProps = {
+      maxPage: 5,
+      tableName: 'test',
+      currentPage: 1,
+      pagesArray: pages,
+      handleJump: () => {},
+    };
+    const { maxPage, tableName } = pageButtonProps;
+
+    it('correctly changes pages when clicking the next button', () => {
+      const { getByRole } = render(
+        <RecoilRoot>
+          <PageButtons pageButtonProps={pageButtonProps} />
+        </RecoilRoot>
+      );
+      const nextButton = getByRole('button', { name: 'Next page' });
+      fireEvent.click(nextButton);
+      const secondButton = getByRole('button', { name: `${tableName}-page2` });
+      expect(secondButton).toHaveClass('active');
+    });
+
+    it('correctly changes pages when clicking the previous button', () => {
+      pageButtonProps.currentPage = 2;
+      const { getByRole } = render(
+        <RecoilRoot>
+          <PageButtons pageButtonProps={pageButtonProps} />
+        </RecoilRoot>
+      );
+      const prevButton = getByRole('button', { name: 'Previous page' });
+      fireEvent.click(prevButton);
+      const firstButton = getByRole('button', { name: `${tableName}-page1` });
+      expect(firstButton).toHaveClass('active');
+    });
   });
 });
