e6f4eac99e08a773952b7bd4b4ee7944eeaf9a00
Noah_Simcoe <noah.simcoe@kc.frb.org>
2025-12-04 13:40:06 -0600

    FDG-8051 init

--- src/components/charts/bar/bar.spec.js
+++ src/components/charts/bar/bar.spec.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import renderer from 'react-test-renderer';
 import BarGraph from './bar';
 import { staggeredData } from '../helpers/helpersData';
-import { ResponsiveBar } from '@nivo/bar';
 import helpers from './helpers/helpers';
 
 class ResizeObserver {
@@ -23,38 +21,23 @@ describe('BarGraph component', () => {
     const { queryByTestId } = render(<BarGraph graphData={staggeredData} graphIndex="year" valueKeys={['value']} />);
     expect(queryByTestId('barGraph')).toBeDefined();
   });
-
-  // TODO - Nivo not rendering internal chart
-  it.skip('sets left and bottom axis attributes each to null by default to prevent unwanted tick marks along the axes', async () => {
-    // let component = renderer.create();
-    // let instance = null;
-    // await renderer.act(async () => {
-    //   component = await renderer.create(<BarGraph graphData={staggeredData} graphIndex="year" valueKeys={['value']} />);
-    //   instance = component.root;
-    // });
-    //
-    // const barGraphCanvas = instance.findByType(ResponsiveBar);
-    // expect(barGraphCanvas.props.axisLeft).toBeNull();
-    // expect(barGraphCanvas.props.axisBottom).toBeNull();
-  });
 });
 
 describe('BarGraph component - Custom bar graph', () => {
   window.ResizeObserver = ResizeObserver;
-  const barGraph = <BarGraph graphData={staggeredData} graphIndex="year" valueKeys={['value']} useCustomBarComponent />;
+  const mouseEnterPropSpy = jest.fn();
+
+  const barGraph = (
+    <BarGraph
+      graphData={staggeredData}
+      graphIndex="year"
+      valueKeys={['value']}
+      useCustomBarComponent
+      mouseEnter={mouseEnterPropSpy}
+    />
+  );
   const mouseEnterSpy = jest.spyOn(helpers, 'mouseEnterEvent');
   const mouseLeaveSpy = jest.spyOn(helpers, 'mouseLeaveEvent');
-
-  // TODO - Nivo not rendering internal chart
-  it.skip('creates a customBarGraph', () => {
-    // let component = renderer.create();
-    // renderer.act(() => {
-    //   component = renderer.create(barGraph);
-    // });
-    // const instance = component.root;
-    // const responsiveBar = instance.findByType(ResponsiveBar);
-    // expect(responsiveBar.props.barComponent).toBeDefined();
-  });
 
   it('triggers mouseEnter and mouseLeave events', () => {
     jest.clearAllMocks();
@@ -62,7 +45,7 @@ describe('BarGraph component - Custom bar graph', () => {
     const container = getByTestId('barGraph');
     fireEvent.mouseEnter(container);
     expect(mouseEnterSpy).toHaveBeenCalledTimes(1);
-
+    expect(mouseEnterPropSpy).toHaveBeenCalled();
     fireEvent.mouseLeave(container);
     expect(mouseLeaveSpy).toHaveBeenCalledTimes(1);
   });
