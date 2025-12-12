33044c0806970c9f068f558894dc27fe7736b6bd
j1nts01 <noah.simcoe@kc.frb.org>
2025-12-12 16:15:51 -0600

    FDG-11109 adding test for coverage bump

--- src/layouts/explainer/sections/national-deficit/national-deficit.spec.js
+++ src/layouts/explainer/sections/national-deficit/national-deficit.spec.js
@@ -1,9 +1,22 @@
 import nationalDeficitSections from './national-deficit';
+import { deficitLearnMoreLinks } from '../../../../layouts/explainer/explainer-helpers/national-deficit/national-deficit-helper';
+import { analyticsEventHandler } from '../../explainer-helpers/explainer-helpers';
+
+jest.mock('../../explainer-helpers/explainer-helpers');
 
 describe('National Deficit explainer page sections', () => {
   it('returns 6 sections with headings and body components', () => {
     expect(nationalDeficitSections.length).toBe(6);
     expect(nationalDeficitSections[0].title).toBeDefined();
     expect(nationalDeficitSections[0].component).toBeDefined();
+  });
+
+  it('fires a citation click when the user clicks the learn more link', () => {
+    expect(deficitLearnMoreLinks.length).toBe(5);
+    deficitLearnMoreLinks.forEach(link => {
+      analyticsEventHandler.mockClear();
+      link.onClick();
+      expect(analyticsEventHandler).toHaveBeenCalledWith(link.title, 'Deficit Citation Click');
+    });
   });
 });
