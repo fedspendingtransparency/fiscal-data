f698d10eaf77dec5370d04be39826eb1505fa62d
LaKeishaW <lakeisha.wren@kc.frb.org>
2026-01-05 15:58:37 -0600

    Added test for addition coverage
    Coverage now at 100%

--- src/components/home-main-content/home-main-content.spec.js
+++ src/components/home-main-content/home-main-content.spec.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Analytics from '../../utils/analytics/analytics'
 import HomeMainContent from './home-main-content';
+
+jest.mock('../../utils/analytics/analytics', () => ({
+  __esModule: true,
+  default: { event: jest.fn() },
+}));
 
 describe('Home Main Content', () => {
   it('should render text for h2', () => {
@@ -13,4 +20,21 @@ describe('Home Main Content', () => {
     const { getByTestId } = render(<HomeMainContent />);
     expect(getByTestId('highlight-cards-parent')).toBeDefined();
   });
+
+  it('fires analytics when Dataset Search link is clicked', async () => {
+    render(<HomeMainContent />);
+
+    const link =
+      screen.queryByRole('link', { name: /dataset search page/i }) ??
+      screen.getByText(/dataset search page/i);
+
+    userEvent.click(link)
+
+    expect(Analytics.event).toHaveBeenCalledWith({
+      category: 'Homepage Navigation',
+      action: 'Citation Click',
+      label: 'Dataset Search',
+    });
+  });
+
 });
