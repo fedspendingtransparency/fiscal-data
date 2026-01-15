bfd0095e80aa3c300ee5afae4dad3199681c6199
Stevens <110061741+chasls2@users.noreply.github.com>
2026-01-14 20:09:26 -0600

    FDG-11023

--- src/layouts/explainer/sections/federal-spending/spending-categories/spending-categories.spec.js
+++ src/layouts/explainer/sections/federal-spending/spending-categories/spending-categories.spec.js
@@ -1,5 +1,5 @@
 import { SpendingCategories } from './spending-categories';
-import { render, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import React from 'react';
 import { fireEvent } from '@testing-library/react';
 import fetchMock from 'fetch-mock';
@@ -19,28 +19,5 @@ describe('Federal Spending Overview', () => {
     expect(accordion).toBeInTheDocument();
     fireEvent.click(accordion);
     expect(getByText('Each year, the Social Security', { exact: false })).toBeInTheDocument();
-  });
-
-  it('loads the evergreen data', async () => {
-    const mockData = {
-      data: [
-        {
-          current_fytd_net_outly_amt: '4515067070149.23',
-          prior_fytd_net_outly_amt: '2237949464925.20',
-          record_calendar_month: '06',
-          record_calendar_year: '2022',
-          record_date: '2022-06-30',
-          record_fiscal_year: '2022',
-        },
-      ],
-    };
-
-    fetchMock.get(`begin:https://www.transparency.treasury.gov/services/api/fiscal_service/v1/`, mockData, { overwriteRoute: true }, { repeat: 1 });
-
-    // const fetchSpy = jest.spyOn(global, 'fetch');
-    const { getByText } = render(<SpendingCategories />);
-
-    // expect(fetchMock.called()).toBe(true);
-    await waitFor(() => getByText('federal spending in FY 2022', { exact: false }));
   });
 });
