544893399c742972b2a56e1aead18c176893889e
Stevens <110061741+chasls2@users.noreply.github.com>
2026-01-22 13:22:28 -0600

    inno-day-chas

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,22 +2,21 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 import React from 'react';
 import Persist from './src/components/persist/persist';
 import { RecoilRoot } from 'recoil';
-import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
-import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 import { queryClient } from './react-query-client';
+import { QueryClientProvider } from '@tanstack/react-query';
 
-const isBrowser = () => typeof window !== 'undefined';
+// const isBrowser = () => typeof window !== 'undefined';
 
-const persister = createSyncStoragePersister({
-  storage: isBrowser() && window.sessionStorage,
-});
+// const persister = createSyncStoragePersister({
+//   storage: isBrowser() && window.sessionStorage,
+// });
 
 export const wrapRootElement = ({ element }) => {
   return (
-    <PersistQueryClientProvider client={queryClient} persistOptions={{ persister: persister, maxAge: 600000 }}>
+    <QueryClientProvider client={queryClient}>
       <RecoilRoot>
         <Persist element={element} />
       </RecoilRoot>
-    </PersistQueryClientProvider>
+    </QueryClientProvider>
   );
 };
