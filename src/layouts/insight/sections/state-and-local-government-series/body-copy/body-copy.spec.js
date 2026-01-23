adba3ce4d0ad027b9976dd92bcb792b9715087f8
Noah_Simcoe <noah.simcoe@kc.frb.org>
2026-01-23 11:15:24 -0600

    FDG-11152 adding coverage for ga events on slgs page

--- src/layouts/insight/sections/state-and-local-government-series/body-copy/body-copy.spec.js
+++ src/layouts/insight/sections/state-and-local-government-series/body-copy/body-copy.spec.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { render, waitFor } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import BodyCopy from '../body-copy/body-copy';
 import { mockSLGSFetchResponses, mockSLGSFetchResponses2, mockSLGSFetchResponses3 } from '../../../../explainer/explainer-test-helper';
-
+import Analytics from '../../../../../utils/analytics/analytics';
 
 describe('State and Local Government Series Body Copy', () => {
   beforeAll(() => {
@@ -46,5 +46,29 @@ describe('State and Local Government Series Body Copy - Calculation Variant #2',
     const { findByText } = render(<BodyCopy />);
     await waitFor(() => expect(fetchSpy).toBeCalledTimes(3));
     expect(await findByText('2%', { exact: false })).toBeInTheDocument();
+  });
+});
+
+describe('State and Local Government Series Body Copy - GA Events', () => {
+  it('fires a GA event when each glossary term is clicked', () => {
+    const analyticsSpy = jest.spyOn(Analytics, 'event');
+    const glossaryList = [
+      { text: 'total public debt outstanding', term: 'Total Public Debt Outstanding' },
+      { text: 'State and Local Government Series (SLGS)', term: 'State and Local Government Series' },
+      { text: 'non-marketable securities', term: 'Non-Marketable Securities' },
+      { text: 'federal debt', term: 'Federal Debt' },
+      { text: 'treasury securities', term: 'Treasury Securities' },
+    ];
+    const { getByText } = render(<BodyCopy />);
+    glossaryList.forEach(glossaryItem => {
+      const glossaryButton = getByText(glossaryItem.text);
+      fireEvent.click(glossaryButton);
+      expect(analyticsSpy).toHaveBeenCalledWith({
+        action: 'Glossary Term Click',
+        category: 'State and Local Government Series',
+        label: glossaryItem.term,
+      });
+      analyticsSpy.mockClear();
+    });
   });
 });
