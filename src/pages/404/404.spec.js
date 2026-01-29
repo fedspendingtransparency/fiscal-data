b9145ab7c4316e5e3477cba47be3c6cfc5bf5ffd
Jordan Dachroeden <jordan.dachroeden@kc.frg.orb>
2026-01-29 14:38:03 -0600

    FDG-10000 404 tests

--- src/pages/404/404.spec.js
+++ src/pages/404/404.spec.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import NotFoundPage from './index';
-import { render, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { RecoilRoot } from 'recoil';
 
 describe('404 page', () => {
-  // it('renders page helmet with expected document title', async () => {
-  //   render(<NotFoundPage />, { wrapper: RecoilRoot });
-  //   await waitFor(() => expect(document.title).toContain('Page Not Found'));
-  // });
-
   it('renders the not found component', () => {
     const { getByTestId } = render(<NotFoundPage />, { wrapper: RecoilRoot });
     const notFound = getByTestId('notFoundWrapper');
@@ -17,11 +12,6 @@ describe('404 page', () => {
 });
 
 describe('fallback page', () => {
-  it('renders page helmet with expected document title', async () => {
-    render(<NotFoundPage pageContext={{ fallback: true }} />, { wrapper: RecoilRoot });
-    await waitFor(() => expect(document.title).toContain('Content Currently Unavailable'));
-  });
-
   it('renders the fallback component', () => {
     const { getByTestId } = render(<NotFoundPage />, { wrapper: RecoilRoot });
     const notFound = getByTestId('notFoundWrapper');
