6f4e4390cc3dca466e4451756dcde6b75ca848cf
Jordan Dachroeden <jordan.dachroeden@kc.frg.orb>
2026-02-10 16:01:56 -0600

    FDG-11255 preconnect

--- src/components/page-helmet/page-helmet.jsx
+++ src/components/page-helmet/page-helmet.jsx
@@ -84,6 +84,7 @@ const PageHelmet = ({
           {DatasetStructuredData(datasetDetails)}
         </script>
       )}
+      <link rel="preconnect" href="https://fonts.googleapis.com">
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
       {/*<link rel="preconnect" href="https://www.googletagmanager.com" />*/}
       {/*/!*Google Tag Manager *!/*/}
