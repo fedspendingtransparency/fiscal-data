f50775fd7ad67993bccb01dd5f64f0556a5fbfa4
Noah_Simcoe <noah.simcoe@kc.frb.org>
2026-02-19 07:46:24 -0600

    FDG-11270.1 wip on date-fns upgrade

--- src/utils/api-utils.js
+++ src/utils/api-utils.js
@@ -145,7 +145,7 @@ export const pagedDatatableRequest = async (
     columnDateFilters.forEach(filter => {
       if (filter.fieldName !== 'record_date') {
         if (filter?.from && filter?.to) {
-          const addDateStr = buildDateFilter({ dateField: filter.fieldName }, filter?.from.format('YYYY-MM-DD'), filter?.to.format('YYYY-MM-DD'));
+          const addDateStr = buildDateFilter({ dateField: filter.fieldName }, filter?.from.format('yyyy-MM-dd'), filter?.to.format('yyyy-MM-dd'));
           additionalDateFilter += `,${addDateStr}`;
         }
       }
