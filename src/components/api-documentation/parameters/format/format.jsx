f80499f526c3664ac96cdcd5dba487e1d7cc8da6
LaKeishaW <lakeisha.wren@kc.frb.org>
2026-03-04 09:15:49 -0600

    h4 changed to dt and spacing issue fixed.

--- src/components/api-documentation/parameters/format/format.jsx
+++ src/components/api-documentation/parameters/format/format.jsx
@@ -7,28 +7,36 @@ const baseApiUrl = GLOBALS.PROD_API_BASE_URL;
 
 const Format = () => (
   <SectionContent title="Format" id="parameters-format" headingLevel={3}>
-    <div>
-      <h4>Parameter:</h4> <code className="inline">format=</code>
-    </div>
-    <div>
-      <h4>Definition:</h4> The format parameter allows a user to define the output method of the response (CSV, JSON, XML).
-    </div>
-    <div>
-      <h4>Accepts:</h4> The <code className="inline">format=</code> parameter accepts <code className="inline">xml</code>,{' '}
-      <code className="inline">json</code>, or <code className="inline">csv</code> as an input.
-    </div>
-    <div>
-      <h4>Required:</h4> No, format is not required to make an API request.
-    </div>
-    <div>
-      <h4>Default:</h4> When no format is specified, the default response format is JSON.
-    </div>
-    <div>
-      <h4>Example:</h4>
-    </div>
-    <p>Return all data from the Debt to the Penny dataset in JSON format.</p>
-    <code className={`${code} ${marginBottom}`}>{baseApiUrl}/v2/accounting/od/debt_to_penny?format=json</code>
+    <dl>
+      <div>
+        <dt>Parameter:</dt>
+        <code className="inline">format=</code>
+      </div>
+      <div>
+        <dt>Definition:</dt>
+        The format parameter allows a user to define the output method of the response (CSV, JSON, XML).
+      </div>
+      <div>
+        <dt>Accepts:</dt>
+        The <code className="inline">format=</code> parameter accepts <code className="inline">xml</code>,{' '}
+        <code className="inline">json</code>, or <code className="inline">csv</code> as an input.
+      </div>
+      <div>
+        <dt>Required:</dt>
+        No, format is not required to make an API request.
+      </div>
+      <div>
+        <dt>Default:</dt>
+        When no format is specified, the default response format is JSON.
+      </div>
+      <div>
+        <dt>Example:</dt>
+      </div>
+      <p>Return all data from the Debt to the Penny dataset in JSON format.</p>
+      <code className={`${code} ${marginBottom}`}>{baseApiUrl}/v2/accounting/od/debt_to_penny?format=json</code>
+    </dl>
   </SectionContent>
+
 );
 
 export default Format;
