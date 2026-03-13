9612f7b20f87d276c4f3ff9bc0ce644c99a70ff8
Stevens <110061741+chasls2@users.noreply.github.com>
2026-03-13 09:25:31 -0500

    FDG-11255-c

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,11 +2,22 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 import React from 'react';
 import Persist from './src/components/persist/persist';
 import { RecoilRoot } from 'recoil';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import { queryClient } from './react-query-client';
+
+/** @type {import('@tanstack/react-query-persist-client').Persister} */
+const noopPersister = {
+  persistClient: async () => {},
+  restoreClient: async () => undefined,
+  removeClient: async () => {},
+};
 
 export const wrapRootElement = ({ element }) => {
   return (
-    <RecoilRoot>
-      <Persist element={element} />
-    </RecoilRoot>
+    <PersistQueryClientProvider client={queryClient} persistOptions={{ persister: noopPersister, maxAge: 600000 }}>
+      <RecoilRoot>
+        <Persist element={element} />
+      </RecoilRoot>
+    </PersistQueryClientProvider>
   );
 };
