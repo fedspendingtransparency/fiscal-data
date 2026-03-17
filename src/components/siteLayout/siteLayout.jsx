0340c496d517b355164507af673d0187f4bc8dfc
Stevens <110061741+chasls2@users.noreply.github.com>
2026-03-17 15:14:18 -0500

    FDG-11255-chas deleted it all

--- src/components/siteLayout/siteLayout.jsx
+++ src/components/siteLayout/siteLayout.jsx
@@ -3,7 +3,7 @@ import SiteHeader from '../site-header/site-header';
 import SiteFooter from '../site-footer/site-footer';
 import { ErrorBoundary } from 'react-error-boundary';
 import '../../../src/styles.scss';
-import NotFound from '../../pages/404';
+
 import RedirectModalRenderer from '../modal/redirect-modal/redirect-modal-renderer/redirect-modal-renderer';
 
 export const preProdEnvMsg = 'Loading metadata and data from endpoints in pre-production environment.';
@@ -18,7 +18,7 @@ const SiteLayout = ({ children, isPreProd }) => {
   return (
     <div data-testid="siteLayout">
       <SiteHeader lowerEnvMsg={lowerEnvMsg} />
-      <ErrorBoundary FallbackComponent={() => <NotFound pageContext={{ fallback: true }} />}>
+      <ErrorBoundary FallbackComponent={() => <></>}>
         <main>{children}</main>
       </ErrorBoundary>
       <SiteFooter style={{}} />
