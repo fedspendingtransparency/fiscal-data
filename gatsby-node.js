764180daa2ed5496706e153fd03ebc395c2b3192
Stevens <110061741+chasls2@users.noreply.github.com>
2026-03-18 14:15:01 -0500

    FDG-11255-chas updated 1

--- gatsby-node.js
+++ gatsby-node.js
@@ -1020,29 +1020,29 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   //   });
   // }
 
-  // result.data.allExplainers.explainers.forEach(explainer => {
-  //   if (ENV_ID !== 'production' || explainer.prodReady) {
-  //     const explainerRelatedDatasets = [];
-  //     explainer.relatedDatasets.forEach(dataset => {
-  //       explainerRelatedDatasets.push(result.data.allDatasets.datasets.find(ds => ds.datasetId === dataset));
-  //     });
-  //     createPage({
-  //       path: explainer.slug,
-  //       matchPath: `${explainer.slug}*`,
-  //       component: path.resolve('./src/layouts/explainer/explainer.tsx'),
-  //       context: {
-  //         pageName: explainer.pageName,
-  //         breadCrumbLinkName: explainer.breadCrumbLinkName,
-  //         seoConfig: explainer.seoConfig,
-  //         heroImage: explainer.heroImage,
-  //         relatedDatasets: explainerRelatedDatasets,
-  //         isAFG: explainer.isAFG,
-  //         cpiDataByYear: cpiYearMap,
-  //         cpi12MonthPercentChange: cpi12MonthPercentChangeMap,
-  //       },
-  //     });
-  //   }
-  // });
+  result.data.allExplainers.explainers.forEach(explainer => {
+    if (ENV_ID !== 'production' || explainer.prodReady) {
+      const explainerRelatedDatasets = [];
+      explainer.relatedDatasets.forEach(dataset => {
+        explainerRelatedDatasets.push(result.data.allDatasets.datasets.find(ds => ds.datasetId === dataset));
+      });
+      createPage({
+        path: explainer.slug,
+        matchPath: `${explainer.slug}*`,
+        component: path.resolve('./src/layouts/explainer/explainer.tsx'),
+        context: {
+          pageName: explainer.pageName,
+          breadCrumbLinkName: explainer.breadCrumbLinkName,
+          seoConfig: explainer.seoConfig,
+          heroImage: explainer.heroImage,
+          relatedDatasets: explainerRelatedDatasets,
+          isAFG: explainer.isAFG,
+          cpiDataByYear: cpiYearMap,
+          cpi12MonthPercentChange: cpi12MonthPercentChangeMap,
+        },
+      });
+    }
+  });
 
   // result.data.allInsights.insights.forEach(insight => {
   //   if (ENV_ID !== 'production' || insight.prodReady) {
