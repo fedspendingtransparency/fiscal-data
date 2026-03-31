fd87b6088bcb5d6e2b0f377ceabb28964c841a6c
Noah_Simcoe <noah.simcoe@kc.frb.org>
2026-03-30 20:05:21 -0500

    FDG-11430 clean up

--- src/layouts/explainer/sections/national-debt/growing-national-debt/debt-trends-over-time/debt-trends-over-time-chart.jsx
+++ src/layouts/explainer/sections/national-debt/growing-national-debt/debt-trends-over-time/debt-trends-over-time-chart.jsx
@@ -49,7 +49,6 @@ export const DebtTrendsOverTimeChart = ({ sectionId, beaGDPData, width }) => {
 
   const processData = () => {
     const { finalGDPData } = beaGDPData;
-    console.log('beaGDPData: ', beaGDPData);
     const debtData = data.contents.payload;
     const lastGDPValue = finalGDPData[finalGDPData.length - 1];
     const debtToGDP = [];
