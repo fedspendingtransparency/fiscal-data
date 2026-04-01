9467ffbb505ab5d64b4a7fd2a2dbf95fcf344c75
LaKeishaW <lakeisha.wren@kc.frb.org>
2026-04-01 11:58:42 -0500

    test

--- src/layouts/explainer/explainer-components/afg-components/afg-topic-card/afg-topic-card.jsx
+++ src/layouts/explainer/explainer-components/afg-components/afg-topic-card/afg-topic-card.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons/faArrowRightLong';
-import { arrow, imageContainer, link, textContainer, topicHeading, topicSection } from './afg-topic-card.module.scss';
+import { arrow, imageContainer, link, textContainer, topicHeading, topicSection, desktopView, mobileView } from './afg-topic-card.module.scss';
 import Analytics from '../../../../../utils/analytics/analytics';
 import useGAEventTracking from '../../../../../hooks/useGAEventTracking';
 import AFGDefictChart from '../../../sections/overview/deficit-chart/deficit-chart';
@@ -59,7 +59,6 @@ const AfgTopicCard = ({
     <>
       <section>
         {id &&  ( <HeaderChip text={explainerAnalyticsLabelMap[id]} color={explainerColorMap[id].primary} /> )}
-        <div className={desktopView}>
           <div className={topicSection}>
             <div className={textContainer}>
               <h5 className={topicHeading}>{heading}</h5>
@@ -73,23 +72,6 @@ const AfgTopicCard = ({
               <div>{chart}</div>
             </div>
           </div>
-        </div>
-
-          <div className={mobileView} >
-          <div className={topicSection}>
-            <div className={textContainer}>
-              <h5 className={topicHeading}>{heading}</h5>
-              <div className={imageContainer}>
-                <div>{chart}</div>
-              </div>
-              <div className={body}>{body}</div>
-              <a href={linkUrl} className={`${link} afgTopicsLink`} onClick={onClickEventHandler} id={pageName}>
-                {linkText}
-                <FontAwesomeIcon icon={faArrowRightLong} title="right arrow" className={arrow} />
-              </a>
-            </div>
-          </div>
-        </div>
       </section>
     </>
   );
