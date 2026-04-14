ef49a5f7b8dbd56206ead8c08b7af142e62a5e74
j1nts01 <noah.simcoe@kc.frb.org>
2026-04-13 19:24:32 -0500

    FDG-11459 removing test for link that got removed

--- src/components/about-us/about-section/about-section.spec.js
+++ src/components/about-us/about-section/about-section.spec.js
@@ -32,9 +32,4 @@ describe('About section', () => {
     const headers = getAllByRole('heading', { level: 3 });
     expect(headers.length).toEqual(h3Headers.length);
   });
-
-  it('provides a link to Fiscal Service About Us', () => {
-    const { getByTestId } = render(<About />);
-    expect(getByTestId('fsLink')).toHaveAttribute('href', `${globalConstants.FISCAL_TREASURY_URL}/about.html`);
-  });
 });
