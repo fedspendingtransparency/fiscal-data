dd1199518c89ee806427073a030855361307afb7
j1nts01 <noah.simcoe@kc.frb.org>
2026-04-14 09:40:38 -0500

    FDG-11212 updating timer count for spending/revenue chart hover

--- src/layouts/explainer/sections/federal-spending/spending-trends/total-spending-chart/total-spending-chart.spec.js
+++ src/layouts/explainer/sections/federal-spending/spending-trends/total-spending-chart/total-spending-chart.spec.js
@@ -183,7 +183,7 @@ describe('Total Spending Chart', () => {
     await waitFor(() => expect(fetchSpy).toBeCalled());
     const spendingLineChart = await findByTestId('spendingLineChart');
     expect(spendingLineChart).toHaveStyle('pointer-events: none');
-    jest.advanceTimersByTime(6000);
+    jest.advanceTimersByTime(6500);
     await waitFor(() => expect(spendingLineChart).toHaveStyle('pointer-events: auto'));
   });
 });
