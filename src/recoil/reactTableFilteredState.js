e732d7206412388d51fc616e909b96225bf0aa84
chasls2 <110061741+chasls2@users.noreply.github.com>
2026-04-23 10:24:25 -0500

    FDG-11511

--- src/recoil/reactTableFilteredState.js
+++ src/recoil/reactTableFilteredState.js
@@ -1,12 +1,6 @@
-// import { atom } from 'recoil';
-//
-// export const reactTableFilteredDateRangeState = atom({
-//   key: 'reactTableFilteredDateRangeState',
-//   default: [],
-// });
 import { create } from 'zustand';
 
 export const reactTableFilteredState = create(set => ({
   dateRange: [],
-  setDateRange: dateRange => set({ dateRange}),
+  setDateRange: dateRange => set({ dateRange }),
 }));
