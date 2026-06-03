24980630867d6be3e426b9ef9a7882ad086cf001
Noah_Simcoe <noah.simcoe@kc.frb.org>
2026-06-03 15:04:56 -0500

    FDG-11192 adding x-axis adjustment for debt trends chart

--- src/layouts/explainer/sections/national-debt/growing-national-debt/debt-trends-over-time/debt-trends-over-time-chart.jsx
+++ src/layouts/explainer/sections/national-debt/growing-national-debt/debt-trends-over-time/debt-trends-over-time-chart.jsx
@@ -242,7 +242,7 @@ export const DebtTrendsOverTimeChart = ({ sectionId, beaGDPData, width }) => {
                       tickSize: 6,
                       tickPadding: 8,
                       tickRotation: 0,
-                      tickValues: 9,
+                      tickValues: width < pxToNumber(breakpointLg) ? [1940, 1970, 2000, 2030] : 9,
                     }}
                     axisLeft={{
                       format: formatPercentage,
