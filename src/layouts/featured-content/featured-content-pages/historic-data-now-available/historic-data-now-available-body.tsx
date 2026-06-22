aab89c27b2fe43923372bdf5b2921611186b7d0f
Noah_Simcoe <noah.simcoe@kc.frb.org>
2026-06-22 08:48:30 -0500

    FDG-11736 adding body copy data/div lines

--- src/layouts/featured-content/featured-content-pages/historic-data-now-available/historic-data-now-available-body.tsx
+++ src/layouts/featured-content/featured-content-pages/historic-data-now-available/historic-data-now-available-body.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from 'react';
 import { featuredContentCitationsMap } from '../../featured-content-helpers';
+import { line } from './historic-data-now-available-body.module.scss';
 
 export const HistoricDataNowAvailableBody = (): ReactElement => {
   const { accountsOfReceiptsAndExpendituresDataset } = featuredContentCitationsMap['historic-data-announcement'];
@@ -14,6 +15,31 @@ export const HistoricDataNowAvailableBody = (): ReactElement => {
         This milestone provides insight into federal receipts and expenditures data from 1793 onward, including Treasury's{' '}
         {accountsOfReceiptsAndExpendituresDataset} (1793–1890) and the Combined Statement of Receipts, Outlays, and Balances (1872–2025). Together,
         these records offer a comprehensive picture of America's finances spanning more than 230 years.{' '}
+      </p>
+      <p>
+        This release offers an unprecedented view of government revenue collection and spending since the nation's founding. It enables analysis of an
+        extensive historical archive of receipts and revenue data. By consolidating and modernizing access to this information, the Bureau of the
+        Fiscal Service reinforces its dedication to transparency, accountability, and open data access. With this new data, users could explore:{' '}
+      </p>
+      <div className={line}></div>
+      <p>
+        U.S. government cash and liquidity management by analyzing all daily, monthly, or annual cash balances, receipts, and outlays to identify
+        seasonal patterns, volatility, and periods of surplus or shortfall.{' '}
+      </p>
+      <div className={line}></div>
+      <p>
+        Spending trends and policy impact by extracting all outlay categories and amounts to compute growth rates, shares of total spending, and
+        long-term trends.{' '}
+      </p>
+      <div className={line}></div>
+      <p>
+        Crisis response and fiscal resilience by identifying periods of significant spikes or changes in receipts and outlays and comparing the
+        magnitude, composition, and duration of fiscal changes.{' '}
+      </p>
+      <div className={line}></div>
+      <p>
+        Released ahead of the nation's 250th anniversary, these datasets provide a valuable resource for understanding the financial foundations that
+        have shaped the United States since 1793.{' '}
       </p>
     </div>
   );
