6d2e091dade0be3db25cd8bb49b128f677da3881
chasls2 <110061741+chasls2@users.noreply.github.com>
2026-06-25 10:29:12 -0500

    fdg11739

--- src/layouts/featured-content/featured-content-landing/featured-content-landing.tsx
+++ src/layouts/featured-content/featured-content-landing/featured-content-landing.tsx
@@ -2,16 +2,7 @@ import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import HomePageTile from '../../../components/topics-section/homepage-tile/homepage-tile';
-import {
-  banner,
-  categoryHeader,
-  content,
-  tileGrid,
-  featuredContentHeader,
-  bannerIcon,
-  featuredContentArticleTitle,
-  container
-} from './featured-content-landing.module.scss';
+import { banner, categoryHeader, content, tileGrid, featuredContentHeader, bannerIcon, container } from './featured-content-landing.module.scss';
 import { featuredContentBanner, featuredContentLanding } from './featured-content-landing-config';
 
 const FeaturedContentLanding = () => {
@@ -61,7 +52,7 @@ const FeaturedContentLanding = () => {
                   images={images}
                   rightTile
                   showLine
-                  headingLevel='h2'
+                  headingLevel="h2"
                   featuredContentArticle
                   analyticsCategory="Featured Content Navigation"
                   hoverAnalyticsCategory="Featured Content Cards"
