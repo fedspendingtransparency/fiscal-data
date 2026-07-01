272be50ae5d7b0b91c6363116cf55fcf1a69c818
Noah_Simcoe <noah.simcoe@kc.frb.org>
2026-07-01 13:55:44 -0500

    FDG-11734 wip to cypress test fixes

--- cypress/e2e/resources/api-documentation.cy.ts
+++ cypress/e2e/resources/api-documentation.cy.ts
@@ -13,6 +13,59 @@ describe('API Documentation Page', () => {
     visitApiDocimentationPage();
   });
 
+  it('Navigate to the api documentation, ensure page does not contain NaN, null, or undefined values', () => {
+    cy.findAllByText('null').should('not.exist');
+    cy.findAllByText('NaN').should('not.exist');
+    cy.findAllByText('undefined').should('not.exist');
+  });
+
+  it('Validate all internal links on the page navigate to the correct destinations', () => {
+    const links = [
+      {
+        name: 'our website',
+        url: '/',
+      },
+      {
+        name: "Your Guide to America's Finances",
+        url: 'americas-finance-guide/',
+      },
+      {
+        name: 'Search and filter',
+        url: 'datasets/',
+      },
+      {
+        name: 'full documentation on Aggregation and Sums',
+        url: 'api-documentation/#aggregation-sums',
+      },
+      {
+        name: 'Endpoints by Dataset',
+        url: 'api-documentation/#list-of-endpoints-table',
+      },
+      {
+        name: 'Format',
+        url: 'api-documentation/#parameters-format',
+      },
+    ];
+
+    links.forEach(link => {
+      cy.findByRole('link', { name: link.name }).click();
+      cy.url().should('include', link.url);
+      cy.visit('/api-documentation/');
+    });
+  });
+
+  it('Validate all external links on the page navigate to the correct destinations', () => {
+    const links = [
+      {
+        name: 'data registry',
+        url: 'https://fiscal.treasury.gov/data-registry/index.html',
+      },
+    ];
+    links.forEach(link => {
+      cy.findByRole('link', { name: link.name }).should('have.attr', 'href', link.url);
+    });
+  });
+
   it('Validate all sub-nav links navigate to the correct destinations', () => {
     const links = [
       {
