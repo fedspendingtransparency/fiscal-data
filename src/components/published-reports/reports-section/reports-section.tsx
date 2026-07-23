9fe5869cbaa1729df7fe7cdcda842db7701b7c68
Noah_Simcoe <noah.simcoe@kc.frb.org>
2026-07-23 11:12:54 -0500

    FDG-11681 Styling changes for download button

--- src/components/published-reports/reports-section/reports-section.tsx
+++ src/components/published-reports/reports-section/reports-section.tsx
@@ -161,7 +161,9 @@ const ReportsSection: FunctionComponent<{ dataset: IDatasetConfig }> = ({ datase
                   aria-label={`Download ${zipFileName}`}
                   className={button}
                 >
-                  Download all ({currentReports.length - 1} {currentReports.length - 1 === 1 ? 'file' : 'files'})
+                  <span>
+                    Download all ({currentReports.length - 1} {currentReports.length - 1 === 1 ? 'file' : 'files'})
+                  </span>
                 </a>
               )}
             </LowerEnvironmentFeature>
