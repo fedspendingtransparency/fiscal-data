e7e336cc85d01315f63bacf2cd0d317c7b3ee2f6
LaKeishaW <lakeisha.wren@kc.frb.org>
2026-07-27 10:54:09 -0500

    added test case

--- src/components/datasets/search-results/search-results-helper.spec.js
+++ src/components/datasets/search-results/search-results-helper.spec.js
@@ -4,22 +4,25 @@ const mockDatasets = [
   {
     name: 'Dataset C',
     searchScore: 0.8,
+    popularity_rank: 3,
     techSpecs: { lastUpdated: '2020-04-01' },
   },
   {
     name: 'Dataset B',
     searchScore: 0.5,
+    popularity_rank: 2,
     techSpecs: { lastUpdated: '2020-01-01' },
   },
   {
     name: 'Dataset A',
     searchScore: 0.2,
+    popularity_rank: 1,
     techSpecs: { lastUpdated: '2020-10-01' },
   },
 ];
 
 describe('search results helper', () => {
-  it('establishes sort options with four values', () => {
+  it('establishes sort options with five values', () => {
     expect(SortOptions.length).toBe(5);
   });
 
@@ -53,5 +56,12 @@ describe('search results helper', () => {
   it('sorts by relevance', () => {
     PerformSort(SortOptions[0], mockDatasets);
     expect(mockDatasets[0].name).toBe('Dataset C');
+  });
+
+  it('sorts by popularity', () => {
+    PerformSort(SortOptions[3], mockDatasets);
+    expect(mockDatasets[0].name).toBe('Dataset A');
+    expect(mockDatasets[1].name).toBe('Dataset B');
+    expect(mockDatasets[2].name).toBe('Dataset C');
   });
 });
