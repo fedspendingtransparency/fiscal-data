972ec01d67bde41f29d1eb89e9dd90b3f0500784
chasls2 <110061741+chasls2@users.noreply.github.com>
2026-07-30 09:18:06 -0500

    FDG-11841

--- src/components/data-preview/data-preview-filter-section/data-preview-download-wrapper/download-button/csv-direct-download/csv-direct-download.spec.js
+++ src/components/data-preview/data-preview-filter-section/data-preview-download-wrapper/download-button/csv-direct-download/csv-direct-download.spec.js
@@ -9,7 +9,7 @@ describe('CSV Direct Download Button', () => {
   it('renders a download link', () => {
     const { getByRole } = render(<CsvDirectDownload filename="filename" downloadData={mockCSVData} handleClick={jest.fn()} chidren={<>CSV</>} />);
     const downloadLink = getByRole('link', { hidden: true });
-    expect(downloadLink).toHaveAttribute('href', 'data:text/csv;charset=utf-8,"header 1","header 2"');
+    expect(downloadLink).toHaveAttribute('href', 'data:text/csv;charset=utf-8, "header 1","header 2"');
   });
 
   it('direct CSV download with timestamp', () => {
