395e98079f0d346efd60d1d0a869d45d38c7380b
chasls2 <110061741+chasls2@users.noreply.github.com>
2026-07-30 14:22:34 -0500

    FDG-11842

--- src/components/markdown-transform/markdown-transform.spec.js
+++ src/components/markdown-transform/markdown-transform.spec.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { MarkdownTransform } from './markdown-transform';
 import rehypeSanitize, { defaultSchema } from './rehype-sanitize';
-import { ReactMarkdown } from 'react-markdown';
 
-const testContent = 'This is a link in markdown: [Link](https://fiscaldata.treasury.gov)';
 jest.mock('react-markdown', () => {
   const mockReact = require('react');
   const mockComponent = jest.fn(({ children }) => mockReact.createElement(mockReact.Fragment, null, children));
   return { __esModule: true, default: mockComponent };
 });
+
+import { ReactMarkdown } from 'react-markdown';
+
+const testContent = 'This is a link in markdown: [Link](https://fiscaldata.treasury.gov)';
 
 describe('Markdown transform', () => {
   it('renders markdown content', () => {
@@ -22,9 +24,15 @@ describe('Markdown transform', () => {
     expect(getByText('Link', { exact: false })).toBeInTheDocument();
   });
 
-  it('chains rehype- sanitized', () => {
+  it('chains rehype-sanitized with the default schema after rehype-raw for banner content', () => {
     ReactMarkdown.mockClear();
     render(<MarkdownTransform content={testContent} isBanner={true} />);
+    const { rehypePlugins } = ReactMarkdown.mock.calls[0][0];
+    expect(rehypePlugins[rehypePlugins.length - 1]).toEqual([rehypeSanitize, defaultSchema]);
+  });
+  it('chains rehype-sanitized with the default scheme after rehype-raw for non-banner content', () => {
+    ReactMarkdown.mockClear();
+    render(<MarkdownTransform content={testContent} />);
     const { rehypePlugins } = ReactMarkdown.mock.calls[0][0];
     expect(rehypePlugins[rehypePlugins.length - 1]).toEqual([rehypeSanitize, defaultSchema]);
   });
