c132507c0b6d674a62f7fbb85cdc55d2820c1aaf
chasls2 <110061741+chasls2@users.noreply.github.com>
2026-07-30 16:02:29 -0500

    FDG-11843

--- src/components/page-helmet/page-helmet.jsx
+++ src/components/page-helmet/page-helmet.jsx
@@ -76,16 +76,13 @@ const PageHelmet = ({
       {/* Version info is placed inside a script comment below because both react jsx
           and gatsby are unfriendly toward rendering <!-- html comments --> into built pages.
       */}
-      <script data-testid="version-info">
-        {`/*
-            TAG: ${latestTag}
-            CURRENT BRANCH: ${currentBranch}
-            COMMIT HASH: ${latestCommit?.hash}
-            COMMIT MESSAGE: ${latestCommit?.message}
-            COMMIT DATE: ${latestCommit?.date}
-            ENV ID: ${ENV_ID}
-          */`}
-      </script>
+      <meta
+        data-testid="version-info"
+        name="build-info"
+        content={
+          `TAG: ${latestTag} | CURRENT BRANCH: ${currentBranch} | COMMIT HASH: ${latestCommit?.hash} | COMMIT MESSAGE: ${latestCommit?.message} | ` +
+          `COMMIT DATE: ${latestCommit?.date} | ENV ID: ${ENV_ID}`}
+      />
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width" />
       <title>{title}</title>
