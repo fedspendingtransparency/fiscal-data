b0aeec3256a3a9c96ceebffffb486060a5521e9b
Noah_Simcoe <noah.simcoe@kc.frb.org>
2026-08-12 16:04:26 -0500

    FDG-11891 wip

--- src/layouts/explainer/sections/national-deficit/deficit-by-year/deficit-trends-bar-chart/deficit-trends-bar-chart.jsx
+++ src/layouts/explainer/sections/national-deficit/deficit-by-year/deficit-trends-bar-chart/deficit-trends-bar-chart.jsx
@@ -279,13 +279,12 @@ export const DeficitTrendsBarChart = () => {
               role="presentation"
               ref={ref}
             >
-              <ResponsiveContainer width="100%" height={desktop ? 388 : 300}>
+              <ResponsiveContainer width="100%" height={388}>
                 <BarChart
                   width={chartConfigs.width}
                   height={chartConfigs.height}
                   data={chartData}
-                  margin={{ top: 15, right: 0, bottom: 15, left: 0 }}
-                  barCategoryGap={1}
+                  margin={{ top: 15, right: 15, bottom: 15, left: 0 }}
                   onMouseMove={onChartMouseMove}
                   onMouseLeave={onBarMouseLeave}
                 >
